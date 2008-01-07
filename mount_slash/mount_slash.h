ff2e75b38a1958f7ea3e6582e027de2eeafd1b07
Paul Nowoczynski <pauln@psc.edu>
2008-01-07 19:09:26 +0000

    Added generic reply struct
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@2436 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- mount_slash/mount_slash.h
+++ mount_slash/mount_slash.h
@@ -232,6 +232,10 @@ struct slashrpc_write_rep {
 	u32	size;
 };
 
+struct slashrpc_generic_rep {
+	u32 rc;
+};
+
 int rpc_svc_init(void);
 int rpc_newreq(int, int, int, int, int, struct pscrpc_request **, void *);
 int rpc_getrep(struct pscrpc_request *, int, void *);
