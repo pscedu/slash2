1586bb30ebf24894332d544508dda16167ac9cf8
Jared Yanovich <yanovich@psc.edu>
2008-01-14 18:03:52 +0000

    slash_appthread.* is not needed
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@2474 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- src/slash_appthread.c
+++
@@ -1,7 +0,0 @@
-/* Must stay sync'd with ZTHRT_*. */
-const char *slashThreadTypeNames[] = {                                                
-      "slash_ctlthr",                                                                 
-      "slash_iothr%d",                                                                
-      "slash_mdsthr%d",                                                               
-      "slash_tcplndthr%d"                                                             
-};                
