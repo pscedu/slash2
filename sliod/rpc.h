e0e7ba3e9fa92e399c2650ac13d38fb05d59ff3d
Jared Yanovich <yanovich@psc.edu>
2008-02-04 22:56:14 +0000

    remove slashd things
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@2610 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- sliod/rpc.h
+++ sliod/rpc.h
@@ -2,11 +2,5 @@
 
 #include <sys/types.h>
 
-#include "cfd.h"
-
 struct slashrpc_export {
-	uid_t		uid;
-	gid_t		gid;
-	u64		cfd;
-	struct cfdtree	cfdtree;
 };
--- sliod/sliod.h
+++ sliod/sliod.h
@@ -2,6 +2,7 @@
 
 #define SLIOTHRT_CTL	0	/* control */
 #define SLIOTHRT_LND	1	/* lustre networking helper */
+#define SLIOTHRT_RPC	2	/* RPC comm */
 
 struct slio_ctlthr {
 };
