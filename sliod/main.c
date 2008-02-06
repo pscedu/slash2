cbf9a75eee9ab1828e1dd8deaf3008248759c450
Jared Yanovich <yanovich@psc.edu>
2008-02-06 20:31:05 +0000

    init portals and add cfd2fid_cache
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@2629 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- sliod/main.c
+++ sliod/main.c
@@ -10,6 +10,8 @@
 #include "psc_rpc/rpc.h"
 
 #include "sliod.h"
+#include "fid.h"
+#include "../slashd/cfd.h"
 
 #define SLIO_THRTBL_SIZE 19
 //#define _PATH_SLIOCONF "/etc/sliod.conf"
@@ -84,8 +86,33 @@ main(int argc, char *argv[])
 		default:
 			usage();
 		}
-	libsl_init(PSC_SERVER);
+        pscrpc_init_portals(PSC_SERVER);
 	slio_init();
 	slioctlthr_main(sfn);
 	exit(0);
+}
+
+int
+cfd2fid_cache(slash_fid_t *fidp, struct pscrpc_export *exp, u64 cfd)
+{
+	struct slashrpc_getfid_req *mq;
+	struct slashrpc_getfid_rep *mp;
+	struct cfdent *c;
+
+	/* Check in cfdtree. */
+	if (cfd2fid(fidp, exp, cfd) == 0)
+		return (0);
+
+	/* Not there, contact slashd and populate it. */
+	if ((rc = rpc_newreq(RPCSVC_MDS, SMDS_VERSION, SRMT_GETFID,
+	    sizeof(*mq), sizeof(*mp), &rq, &mq)) != 0)
+		return (rc);
+	mq->pid = exp->exp_connection->c_peer.pid;
+	mq->nid = exp->exp_connection->c_peer.nid;
+	mq->cfd = cfd;
+	if ((rc = rpc_getrep(rq, sizeof(*mp), &mp)) == 0)
+		if ((c = cfdinsert(&cfd, exp, fidp)) != NULL)
+			*fidp = c->fid;;
+	pscrpc_req_finished(rq);
+	return (rc);				/* XXX preserve errno */
 }
