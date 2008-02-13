ae5e2658504fb473a79d11d4c3bf05b8d8213a0a
Jared Yanovich <yanovich@psc.edu>
2008-02-13 19:09:23 +0000

    guts
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@2680 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- newfs_slmd/newfs_slmd.c
+++ newfs_slmd/newfs_slmd.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 
 #include "slconfig.h"
+#include "pathnames.h"
 
 const char *progname;
 
@@ -18,7 +19,9 @@ int
 main(int argc, char *argv[])
 {
 	const char *cfgfn;
-	int c;
+	char fn[PATH_MAX];
+	sl_resm_t *r;
+	int rc, c;
 
 	progname = argv[0];
 	cfgfn = _PATH_SLASHCONF;
@@ -34,5 +37,42 @@ main(int argc, char *argv[])
 	if (argc)
 		usage();
 	slashGetConfig(cfgfn);
+
+	r = libsl_resm_lookup();
+	if (!r)
+		psc_fatalx("resource not found for this node");
+
+	rc = snprintf(fn, sizeof(fn), "%s", r->resm_res->res_fsroot);
+	if (rc == -1)
+		psc_fatal("snprintf");
+	if (rc >= (int)sizeof(fn))
+		psc_fatalx("name too long");
+	if (mkdir(fn, 0755) == -1)
+		psc_fatal("mkdir %s", fn);
+
+	rc = snprintf(fn, sizeof(fn), "%s/%s", r->resm_res->res_fsroot,
+	    _PATH_OBJROOT);
+	if (rc == -1)
+		psc_fatal("snprintf");
+	if (rc >= (int)sizeof(fn))
+		psc_fatalx("name too long");
+	if (mkdir(fn, 0755) == -1)
+		psc_fatal("mkdir %s", fn);
+
+	rc = snprintf(fn, sizeof(fn), "%s/%s", r->resm_res->res_fsroot,
+	    _PATH_NS);
+	if (rc == -1)
+		psc_fatal("snprintf");
+	if (rc >= (int)sizeof(fn))
+		psc_fatalx("name too long");
+	if (mkdir(fn, 0755) == -1)
+		psc_fatal("mkdir %s", fn);
+
 	exit(0);
+}
+
+int
+lnet_localnids_get(__unusedx lnet_nid_t *nids, __unusedx size_t max)
+{
+	return (0);
 }
