7ad9f2d57828ccb46f88ceeeac634cf683cff14d
Paul Nowoczynski <pauln@psc.edu>
2008-06-20 22:00:36 +0000

    add some comments and #define SLASH_BMAP_SHIFT
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@3506 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- include/fidcache.h
+++ include/fidcache.h
@@ -21,16 +21,18 @@
 #include "inode.h"
 #include "offtree.h"
 
+/* Hand computed */
 #define MDS_FID_CACHE_DEFSZ 1024   /* Number of fcmh's to allocate by default */
 #define MDS_FID_CACHE_MAXSZ 131072 /* Max fcmh's */
-
 
 #define SLASH_BMAP_SIZE  134217728
 #define SLASH_BMAP_WIDTH 8
 #define SLASH_BMAP_DEPTH 5
+#define SLASH_BMAP_SHIFT 11
+/* End hand computed */
+
 #define SLASH_BMAP_BLKSZ (SLASH_BMAP_SIZE / power((size_t)SLASH_BMAP_WIDTH, \
 						  (size_t)(SLASH_BMAP_DEPTH-1)))
-
 #define SLASH_BMAP_BLKMASK ~(SLASH_BMAP_BLKSZ-1)
 
 #define SLASH_MAXBLKS_PER_REQ (LNET_MTU / SLASH_BMAP_BLKSZ)
