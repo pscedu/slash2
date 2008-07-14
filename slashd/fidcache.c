0faf432eac32ce46da07be95bf84ab89c7653903
Paul Nowoczynski <pauln@psc.edu>
2008-07-14 19:45:19 +0000

    init bmap waitq
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@3677 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- slashd/fidcache.c
+++ slashd/fidcache.c
@@ -36,13 +36,14 @@ fcmh_clean_check(struct fidcache_memb_handle *f)
 /**
  * bmap_cache_memb_init - initialize a bmap structure and create its offtree.
  * @b: the bmap struct
- * @f: the bmap's owner (
+ * @f: the bmap's owner
  */
 void
 bmap_cache_memb_init(struct bmap_cache_memb *b, struct fidcache_memb_handle *f)
 {
 	memset(b, 0, sizeof(*b));
 	atomic_set(&b->bcm_refcnt, 0);
+	psc_waitq_init(&b->bcm_waitq);
 	b->bcm_oftr = offtree_create(SLASH_BMAP_SIZE, SLASH_BMAP_BLKSZ,
 				     SLASH_BMAP_WIDTH, SLASH_BMAP_DEPTH,
 				     f, sl_buffer_alloc, sl_oftm_addref, 
