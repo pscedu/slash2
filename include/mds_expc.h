94c5ccb437ca6a45a2013f07883a39b2014398be
Paul Nowoczynski <pauln@psc.edu>
2008-07-15 23:35:20 +0000

    reshape and cleanup some mds data structures
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@3689 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- include/mds_expc.h
+++ include/mds_expc.h
@@ -23,20 +23,27 @@ struct mexpbcm {
         struct bmap_cache_memb *mexpbcm_bmap;
 	struct pscrpc_export   *mexpbcm_export;
 	struct psclist_head     mexpbcm_lentry; /* ref for client cb */
-        SPLAY_ENTRY(mexpbcm)    mexpbcm_tentry;				
+        SPLAY_ENTRY(mexpbcm)    mexpbcm_exp_tentry;
+        SPLAY_ENTRY(mexpbcm)    mexpbcm_bmap_tentry;
 };
 
 enum mexpbcm_modes {
-	MEXPBCM_WR  = (1<<0),  /* otherwise READ */
-        MEXPBCM_DIO = (1<<2)
+	MEXPBCM_WR  = (1<<0), /* otherwise read */
+        MEXPBCM_DIO = (1<<1)  /* otherwise caching mode */ 
 };
 
+static inline int
+mexpbmap_cache_cmp(const void *x, const void *y)
+{
+        return (bmap_cache_cmp(((struct mexpbcm *)x)->mexpbcm_bmap,
+                               ((struct mexpbcm *)y)->mexpbcm_bmap));
+}
+
 SPLAY_HEAD(exp_bmaptree, mexpbcm);
-SPLAY_PROTOTYPE(exp_bmaptree, mexpbcm, mexpbcm_tentry, bmap_cache_cmp);
+SPLAY_PROTOTYPE(exp_bmaptree, mexpbcm, mexpbcm_exp_tentry, mexpbmap_cache_cmp);
 /*
  * mexpfcm (mds_export_fidcache_memb) - this structure interacts with the mds fid cache and the clients cache by mediation within the export.  It tracks which bmaps the client has cached, the operation (lamport) clocks of the fcm and the client.  mexpfcm is tracked by the export's fidcache and the fcm's bmap_lessees tree.
-
-\ * mecm_fcm_opcnt is used to detect changes in the fcm who keeps his update count in fcm_slfinfo.slf_opcnt.  At no time may mecm_fcm_opcnt be higher than the fcm (unless the fcm counter has flipped).  mecm_loc_opcnt and mecm_rem_opcnt serve a similar purpose but the update detection is between the client and the export.  So if client A updates the ctime or mode, exp(clientA) will inc mecm_fcm->fcm_slfinfo.slf_opcnt and desync the opcnt between the fcm and the other exports prompting the other exports to update their client's caches.  It may be the case that incrementing  mecm_fcm->fcm_slfinfo.slf_opcnt will cause rpc's to be sent to the set of clients using that fcm.
+ * mecm_fcm_opcnt is used to detect changes in the fcm who keeps his update count in fcm_slfinfo.slf_opcnt.  At no time may mecm_fcm_opcnt be higher than the fcm (unless the fcm counter has flipped).  mecm_loc_opcnt and mecm_rem_opcnt serve a similar purpose but the update detection is between the client and the export.  So if client A updates the ctime or mode, exp(clientA) will inc mecm_fcm->fcm_slfinfo.slf_opcnt and desync the opcnt between the fcm and the other exports prompting the other exports to update their client's caches.  It may be the case that incrementing  mecm_fcm->fcm_slfinfo.slf_opcnt will cause rpc's to be sent to the set of clients using that fcm.
  * 
  * mexpfcm is in the middle of the exp fc chain and corresponds with the GFC fcm tier.
  */
@@ -49,24 +56,36 @@ struct mexpfcm {
         struct exp_bmaptree   mecm_bmaps;      /* my tree of bmap pointers */
         struct pscrpc_export *mecm_export;     /* backpointer to our export */
         SPLAY_ENTRY(mexpfcm)  mecm_exp_tentry; /* export tree entry */
-        SPLAY_ENTRY(mexpfcm)  mecm_fcm_tentry; /* fcm tree entry    */
 };
 
+static inline int
+mexpfcm_cache_cmp(const void *x, const void *y)
+{
+	slfid_t a, b;
+	
+	a = ((struct fidcache_memb_handle *)x)->fcmh_memb->fcm_fg->fg_fid;
+	b = ((struct fidcache_memb_handle *)y)->fcmh_memb->fcm_fg->fg_fid;
+
+        if (a > b)
+                return 1;
+        else if (a < b)
+                return -1;
+        return 0;
+}
+
 SPLAY_HEAD(exp_fidtree, mexpfcm);
-SPLAY_PROTOTYPE(exp_fidtree, mexpfcm, mecm_exp_tentry, bmap_cache_cmp);
+SPLAY_PROTOTYPE(exp_fidtree, mexpfcm, mecm_exp_tentry, mexpfcm_cache_cmp);
 
-
-struct mexpfc_cli {
-        struct exp_fidtree    mc_fids;   /* reference all fcm's via the
-					  * export */
-        struct psclist_head   mc_lentry; /* sched export for revocations */
-	list_cache_t          mc_pndg_invals; /* pending invalidations   */
+struct mexp_cli {
+        struct exp_fidtree    mc_fids;        /* track all fids on export    */
+        struct psclist_head   mc_lentry;      /* sched export for revocation */
+	list_cache_t          mc_pndg_invals; /* pending invalidations       */
 };
 
 /* This data structure will be used to handle ion failover and 
  *   the reassignment of bmaps to other ions.
  */
-struct mexpfc_ion {
+struct mexp_ion {
 	struct dynarray       mi_bmaps;  /* array of struct mexpbcm  */
 	struct dynarray       mi_bmaps_deref; /* dereferencing bmaps */
 	struct psclist_head   mi_lentry; /* chain ion's              */
@@ -76,21 +95,21 @@ struct mexpfc_ion {
 };
 
 /*
- * mexpfc (mds_export_fidcache_handle) - stored in the export's private data, bmec_set is an array of bmap_cache_memb pointers.  This structure is traversed if the export is closed or if the file object is truncated.
+ * mexp (mds_export_private) - stored in the export's private data, bmec_set is an array of bmap_cache_memb pointers.  This structure is traversed if the export is closed or if the file object is truncated.
  *
  * mexpfc is the upper most level of the exp fidcache.
  */
-struct mexpfch {
-	u64                   mec_magic;  /* detect misapplied reconnects? */
-        struct pscrpc_export *mec_export; /* backpointer to our export */
-        psc_spinlock_t        mec_lock;	
-	int                   mec_type;   /* the export flavor (cli, ion) */
-	union mec_types {
-		struct mexpfc_cli *mectype_cli; 
-		struct mexpfc_ion *mectype_ion; 
+struct mexp {
+	u64                   mexp_magic;  /* detect misapplied reconnects? */
+        struct pscrpc_export *mexp_export; /* backpointer to our export */
+        psc_spinlock_t        mexp_lock;	
+	int                   mexp_type;   /* the export flavor (cli, ion) */
+	union mexp_types {
+		struct mexp_cli *mexptype_cli; 
+		struct mexp_ion *mexpype_ion; 
 	};
-#define mec_cli mec_types.mectype_cli
-#define mec_ion mec_types.mectype_ion
+#define mexp_cli mexp_types.mexptype_cli
+#define mexp_ion mexp_types.mexptype_ion
 };
 
 enum mds_exp_types {
@@ -98,29 +117,8 @@ enum mds_exp_types {
 	MDS_EXP_CLI = (1<<1)
 };
 
-/* Tree definition for the fcm to enable tracking of leased bmaps */
-SPLAY_HEAD(bmap_lessees, mexpfcm);
-SPLAY_PROTOTYPE(bmap_lessees, mexpfcm, mecm_fcm_tentry, bmap_cache_cmp);
-
-struct fcmh_info_mds {
-	struct bmap_lessees fcimds_lessees;  /* mds only, client leases array 
-					      * of bmap_mds_export_cache     */
-};
-
-/* 
- * struct bmexpcr is the leaf entity of the bmap_exports tree.
- * XXX GET RID of THIS!!! move the splay into mexpbcm.
- */
-struct bmexpcr {
-        struct mexpbcm       *bmexpcr_ref;
-        SPLAY_ENTRY(bmexpcr)  bmexpcr_tentry;
-};
-
-/* Tree of bmexpcr's held by bmap_mds_info.
- */
-SPLAY_HEAD(bmap_exports, bmexpcr);
-SPLAY_PROTOTYPE(bmap_exports, bmexpcr, bmexpcr_tentry, bmap_cache_cmp);
-
+SPLAY_HEAD(bmap_exports, mexpbcm);
+SPLAY_PROTOTYPE(bmap_exports, mexpbcm, mexpbcm_bmap_tentry,mexpbmap_cache_cmp);
 /*
  * bmap_mds_info - associate the fcache block to its respective export bmap caches.
  * Notes: both read and write clients are stored to bmdsi_exports, the ref counts are used to determine the number of both and hence the caching mode used at the clients.   bmdsi_wr_ion is a shortcut pointer used only when the bmap has client writers - all writers (and readers) are directed to this ion once a client has invoked write mode on the bmap.
@@ -129,7 +127,7 @@ struct bmap_mds_info {
         atomic_t              bmdsi_rd_ref;  /* count our cli read refs     */
         atomic_t              bmdsi_wr_ref;  /* count our cli write refs    */
 	struct mexpfch       *bmdsi_wr_ion;  /* ion export assigned to bmap */
-        struct bmap_exports   bmdsi_exports; /* tree of our client exports  */
+        struct bmap_exports   bmdsi_exports; /* tree of mexpbcm's           */
 };
 
 enum bmap_mds_modes {
