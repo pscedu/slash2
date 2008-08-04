465b1658fae2129f0b5fdc487759195b7bdb5efa
Paul Nowoczynski <pauln@psc.edu>
2008-08-04 23:25:34 +0000

    add private pointer to cfdent.
    support generic interface for server specific routines dealing with 
    freeing and creating cfdents.
    
    
    
    
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@3824 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- slashd/cfd.h
+++ slashd/cfd.h
@@ -13,7 +13,8 @@ struct pscrpc_export;
 struct cfdent {
 	slfid_t			fid;
 	u64			cfd;
-	SPLAY_ENTRY(cfdent)	entry;
+	void                   *pri;
+	SPLAY_ENTRY(cfdent)	entry;				
 };
 
 SPLAY_HEAD(cfdtree, cfdent);
@@ -24,6 +25,22 @@ int	cfdcmp(const void *, const void *);
 void	cfdnew(u64 *, struct pscrpc_export *, slfid_t);
 int	cfd2fid(struct pscrpc_export *, u64, slfid_t *);
 int	cfdfree(struct pscrpc_export *, u64);
+
+/* Server specific cfd ops.  Primarily used to operate on the cfdent's
+ *  'pri' structure.  All calls must be made with the exp lock held.
+ */
+struct cfd_svrops {
+	int (*cfd_new)(struct cfdent *c);
+	int (*cfd_free)(struct cfdent *c);
+	int (*cfd_insert)(struct cfdent *c);
+};
+
+#define CFD_SVROP(cfd, exp, OP) {			\
+		LOCK_ENSURE(&(exp)->exp_lock);		\
+		if (cfdOps && cfdOps->cfd_OP##)		\
+			(*cfdOps->cfd_OP##)(cfd, exp);	\
+	}
+
 
 SPLAY_PROTOTYPE(cfdtree, cfdent, entry, cfdcmp);
 
