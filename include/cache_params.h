10dd9e577c79116ff1b839ab99732d32e4850206
Paul Nowoczynski <pauln@psc.edu>
2008-08-04 23:21:09 +0000

    support different size caches depending on the system.
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@3818 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- include/cache_params.h
+++ include/cache_params.h
@@ -1,9 +1,25 @@
 #ifndef __CACPARAMS_H__
 #define __CACPARAMS_H__
 
+#define FIDC_CLI_HASH_SZ 1023
+#define FIDC_ION_HASH_SZ 4095
+#define FIDC_MDS_HASH_SZ 32767
+
+enum fid_cache_users {
+	FIDC_USER_CLI = 0,
+	FIDC_USER_ION = 1,
+	FIDC_USER_MDS = 2
+};
+
 /* Hand computed */
-#define MDS_FID_CACHE_DEFSZ 1024   /* Number of fcmh's to allocate by default */
-#define MDS_FID_CACHE_MAXSZ 131072 /* Max fcmh's */
+#define FIDC_MDS_DEFSZ 32768   /* Number of fcmh's to allocate by default */
+#define FIDC_MDS_MAXSZ 1048576 /* Max fcmh's */
+
+#define FIDC_CLI_DEFSZ 1024   /* Number of fcmh's to allocate by default */
+#define FIDC_CLI_MAXSZ 131072 /* Max fcmh's */
+
+#define FIDC_ION_DEFSZ 4096   /* Number of fcmh's to allocate by default */
+#define FIDC_ION_MAXSZ 524288 /* Max fcmh's */
 
 #define SLASH_BMAP_SIZE  134217728
 #define SLASH_BMAP_WIDTH 8
