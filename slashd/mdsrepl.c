31219508ab80a4ddd6601dcf23fe7908f15a9c77
Paul Nowoczynski <pauln@psc.edu>
2008-08-07 19:24:01 +0000

    Routines for managing mds replication
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@3862 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

---
+++ slashd/mdsrepl.c
@@ -0,0 +1,71 @@
+#include "inode.h"
+#include "fid.h"
+
+
+__static int 
+mds_repl_crc_check(sl_inodeh_t *i)
+{
+	psc_crc_t crc;
+	size_t sz = (sizeof(sl_replica_t) * i->inoh_ino.ino_nrepls);
+
+	PSC_CRC_CALC(crc, i->inoh_replicas, sz);
+	if (crc != i->inoh_ino.ino_rs_crc) {
+		psc_warnx("Crc failure on replicas");
+		return (-EIO);
+	}		
+	return (0);
+}
+
+__static int 
+mds_repl_xattr_load_locked(sl_inodeh_t *i)
+{
+	char fidfn[FID_MAX_PATH];
+        size_t sz;
+	int rc;
+
+	DEBUG_INOH(PLL_INFO, i, "trying to load replica table");
+
+	INOH_LOCK_ENSURE(i);
+	psc_assert(i->inoh_ino.ino_nrepls);
+	psc_assert(!i->inoh_replicas);
+	psc_assert(!(i->inoh_flags & INOH_HAVE_REPS));
+
+	fid_makepath(fid, fidfn);
+	sz = (sizeof(sl_replica_t) * i->inoh_ino.ino_nrepls);
+
+	if (fid_getxattr(fidfn, SFX_REPLICAS,  i->inoh_replicas, sz)) {
+		psc_warnx("fid_getxattr failed to get %s", SFX_REPLICAS);
+		rc = -errno;
+		goto fail;
+
+	} else if (mds_repl_crc_check(i)) {
+		rc = -EIO;
+		goto fail;
+
+	} else {
+		i->inoh_flags |= INOH_HAVE_REPS;		
+		DEBUG_INOH(PLL_INFO, i, "replica table loaded");
+	}
+	return (0);
+	
+ fail:
+	DEBUG_INOH(PLL_INFO, i, "replica table load failed");
+	return (rc);
+}
+
+int
+mds_repl_load(sl_inodeh_t *i)
+{
+	int rc=0;
+
+	INOH_LOCK(i);
+	if (i->inoh_ino.ino_nrepls) {		
+		if  (i->inoh_flags & INOH_HAVE_REPS)
+			rc = mds_repl_crc_check(i);			
+	        else 
+			rc = mds_repl_xattr_load_locked(i);
+	}	
+ out:
+	INOH_ULOCK(i);
+	return (rc);
+}
