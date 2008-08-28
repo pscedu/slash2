0c213ead031ce1eac2607313417e2955376623d3
Jared Yanovich <yanovich@psc.edu>
2008-08-28 17:44:47 +0000

    make infrastructure cleanup/improvements - add an mkdep program (stolen from openbsd) so we have when a system   has no substitute and default to this - move some app-specific local make defines to projroot - remove lnet-lite make infrastructure, it is used at the moment
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@4011 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- mk/local.mk
+++ mk/local.mk
@@ -1,16 +1,7 @@
 # $Id$
 
-# Disappointingly, recent versions of gcc hide
-# standard headers in places other than /usr/include.
-MKDEP= `type -t mkdep >/dev/null 2>&1 && echo mkdep || echo makedepend -f.depend` \
-    $$(if ${CC} -v 2>&1 | grep -q gcc; then ${CC} -print-search-dirs | \
-    grep install | awk '{print "-I" $$2 "include"}' | sed 's/:/ -I/'; fi)
-LINT=		splint +posixlib
-CTAGS=		ctags
-
 DEFINES+=	-D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64 -DLINUX
 INCLUDES+=	-I${KERNEL_BASE}/include
 
 THREAD_LIBS?=	-lpthread
-PFL_LIBS?=	-L${PFL_BASE}/lib -lpfl
 LIBL?=		-ll
