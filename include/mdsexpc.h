fcc6a7dcb6b4c289c6a0fe823325a861a37f5948
Jared Yanovich <yanovich@psc.edu>
2008-09-17 20:24:40 +0000

    more
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@4198 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- include/mdsexpc.h
+++ include/mdsexpc.h
@@ -1,7 +1,7 @@
 #ifndef MDSEXPC_H
 #define MDSEXPC_H 1
 
-/* These structures provide back pointers into the Fid Cache to facilitate client-wise operations within the cache.  Such ops would include dereferencing the entire tree of client bmap references on connection close.  
+/* These structures provide back pointers into the Fid Cache to facilitate client-wise operations within the cache.  Such ops would include dereferencing the entire tree of client bmap references on connection close.
  */
 
 #include "psc_types.h"
@@ -12,12 +12,12 @@
 struct pscrpc_export;
 /*
  * mexpbcm (mds_export_bmapc_member) - mexpbcm references bmaps stored in the GFC (global fid cache) and acts as a bridge between GFC bmaps and the export(s) which reference them.  mexpbcm is tracked by the global fidcache (through the fcm's bmap export tree (at the bottom of the GFC's tree chain).  The bmexpcr struct (fidcache.h) points to it.
- * 
+ *
  * mexpbcm_lentry is used for scheduling revocation of the bmap via a listcache based queue.
- * 
+ *
  * mexpbcm is the lowest member of the exp fidcache chain and corresponds to the GFC's bmap tier.
  */
-struct mexpbcm {	
+struct mexpbcm {
 	sl_blkno_t              mexpbcm_blkno;
         u32                     mexpbcm_mode:29;
 	u32                     mexpbcm_net_cmd:2;
@@ -25,7 +25,7 @@ struct mexpbcm {
         struct bmapc_memb      *mexpbcm_bmap;
 	struct pscrpc_export   *mexpbcm_export;
 	struct psclist_head     mexpbcm_lentry;      /* ref for client cb   */
-	atomic_t                mexpbcm_msgcnt;      /* put to zero after rpc 
+	atomic_t                mexpbcm_msgcnt;      /* put to zero after rpc
 						      * has been issued */
         SPLAY_ENTRY(mexpbcm)    mexpbcm_exp_tentry;  /* ref from mexpfcm    */
 	SPLAY_ENTRY(mexpbcm)    mexpbcm_bmap_tentry; /* ref from bmap tree  */
@@ -42,7 +42,7 @@ enum mexpbcm_modes {
 	MEXPBCM_CIO_REQD = (1<<1),  /* cached-io callback outstanding       */
 	MEXPBCM_WR       = (1<<2),  /* otherwise read                       */
 	MEXPBCM_CDIO     = (1<<3),  /* client requested directio            */
-        MEXPBCM_DIO      = (1<<4),  /* otherwise caching mode               */ 
+        MEXPBCM_DIO      = (1<<4),  /* otherwise caching mode               */
 	MEXPBCM_INIT     = (1<<5),  /* on it's way, block on the fcmh waitq */
 	MEXPBCM_RPC_CANCEL = (1<<6)
 };
@@ -67,7 +67,7 @@ mexpbmapc_exp_cmp(const void *x, const void *y)
 
 	e = a->mexpbcm_export;
 	f = b->mexpbcm_export;
-	
+
 	if (e->exp_connection->c_peer.nid > f->exp_connection->c_peer.nid)
 		return (1);
 	if (e->exp_connection->c_peer.nid < f->exp_connection->c_peer.nid)
@@ -80,19 +80,19 @@ SPLAY_PROTOTYPE(exp_bmaptree, mexpbcm, mexpbcm_exp_tentry, mexpbmapc_cmp);
 /*
  * mexpfcm (mds_export_fidc_memb) - this structure interacts with the mds fid cache and the clients cache by mediation within the export.  It tracks which bmaps the client has cached, the operation (lamport) clocks of the fcm and the client.  mexpfcm is tracked by the export's fidcache and the fcm's bmap_lessees tree.
  * mecm_fcm_opcnt is used to detect changes in the fcm who keeps his update count in fcm_slfinfo.slf_opcnt.  At no time may mecm_fcm_opcnt be higher than the fcm (unless the fcm counter has flipped).  mecm_loc_opcnt and mecm_rem_opcnt serve a similar purpose but the update detection is between the client and the export.  So if client A updates the ctime or mode, exp(clientA) will inc mecm_fcm->fcm_slfinfo.slf_opcnt and desync the opcnt between the fcm and the other exports prompting the other exports to update their client's caches.  It may be the case that incrementing  mecm_fcm->fcm_slfinfo.slf_opcnt will cause rpc's to be sent to the set of clients using that fcm.
- * 
+ *
  * mexpfcm is in the middle of the exp fc chain and corresponds with the GFC fcm tier.
  */
 struct mexpfcm {
-	int                   mecm_flags;
-        struct fidc_memb_handle *mecm_fcmh; /* point to the fcm*/
-	u64                   mecm_fcm_opcnt;  /* detect fcm updates */
-	u64                   mecm_loc_opcnt;  /* count outstanding updates */
-	u64                   mecm_rem_opcnt;  /* detect remote updates */
-        psc_spinlock_t        mecm_lock;
-        struct exp_bmaptree   mecm_bmaps;      /* my tree of bmap pointers */
-        struct pscrpc_export *mecm_export;     /* backpointer to our export */
-        SPLAY_ENTRY(mexpfcm)  mecm_fcm_tentry; /* fcm tree entry */
+	int                   mexpfcm_flags;
+        struct fidc_memb_handle *mexpfcm_fcmh; /* point to the fcm*/
+	u64                   mexpfcm_fcm_opcnt;  /* detect fcm updates */
+	u64                   mexpfcm_loc_opcnt;  /* count outstanding updates */
+	u64                   mexpfcm_rem_opcnt;  /* detect remote updates */
+        psc_spinlock_t        mexpfcm_lock;
+        struct exp_bmaptree   mexpfcm_bmaps;      /* my tree of bmap pointers */
+        struct pscrpc_export *mexpfcm_export;     /* backpointer to our export */
+        SPLAY_ENTRY(mexpfcm)  mexpfcm_fcm_tentry; /* fcm tree entry */
 };
 
 #define MEXPFCM_LOCK(m)  spinlock(&(m)->mecm_lock
@@ -102,15 +102,17 @@ enum mexpfcm_states {
 	MEXPFCM_CLOSING = (1<<0)
 };
 
-#define mexpfcm2fid(m) ((struct mexpfcm *)(m))->mecm_fcm->fcmh_memb.fcm_fg.fg_fid
-#define mexpfcm2fidgen(m) ((struct mexpfcm *)(m))->mecm_fcm->fcmh_memb.fcm_fg
+#define mexpfcm2fid(m)		fcmh_2_fid((m)->mexpfcm_fcmh)
+#define mexpfcm2fidgen(m)	fcmh_2_fgp((m)->mexpfcm_fcmh)
 
 static inline int
 mexpfcm_cache_cmp(const void *x, const void *y)
 {
-        if (mexpfcm2fid(x) > mexpfcm2fid(y))
+	const struct mexpfcm *a = x, *b = y;
+
+        if (mexpfcm2fid(a) > mexpfcm2fid(b))
                 return 1;
-        else if (mexpfcm2fid(x) < mexpfcm2fid(y))
+        else if (mexpfcm2fid(a) < mexpfcm2fid(b))
                 return -1;
         return 0;
 }
@@ -120,8 +122,8 @@ struct mexp_cli {
 	struct slashrpc_cservice *mc_csvc;
 };
 
-/* This data structure will be used to handle ion failover and 
- *   the reassignment of bmaps to other ions.  This data structure is 
+/* This data structure will be used to handle ion failover and
+ *   the reassignment of bmaps to other ions.  This data structure is
  *   pointed accessed from (sl_resm_t *)->resm_pri.
  */
 struct mexp_ion {
@@ -157,8 +159,8 @@ struct bmap_mds_info {
 SPLAY_HEAD(fcm_exports, mexpfcm);
 SPLAY_PROTOTYPE(fcm_exports, mexpfcm, mexpfcm_fcm_tentry, mexpfcm_cache_cmp);
 
-static inline void 
-bmdsi_sanity_locked(struct bmapc_memb *b, int dio_check)
+static inline void
+bmdsi_sanity_locked(struct bmapc_memb *bmap, int dio_check)
 {
 	int wtrs, rdrs;
 	struct bmap_mds_info *mdsi = bmap->bcm_mds_pri;
@@ -170,14 +172,14 @@ bmdsi_sanity_locked(struct bmapc_memb *b, int dio_check)
 		psc_assert(bmap->bcm_bmapih.bmapi_mode & BMAP_MDS_DIO);
 }
 
-struct fidc_mds_info {	
-	struct fcm_exports fmdsi_exports; /* tree of mexpfcm */	
-	atomic fmdsi_ref;
+struct fidc_mds_info {
+	struct fcm_exports fmdsi_exports; /* tree of mexpfcm */
+	atomic_t fmdsi_ref;
 	u32    fmdsi_xid;
 };
 
 
-/* IOS round-robin counter for assigning IONs.  Attaches at res_pri. 
+/* IOS round-robin counter for assigning IONs.  Attaches at res_pri.
  */
 struct resprof_mds_info {
 	int rmi_cnt;
