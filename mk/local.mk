46e9a011cbe4afccddeb978c50b2332b536c794a
Jared Yanovich <yanovich@psc.edu>
2008-09-29 18:28:11 +0000

    bits for compiling on pople
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@4283 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- mk/local.mk
+++ mk/local.mk
@@ -5,3 +5,10 @@ INCLUDES+=	-I${KERNEL_BASE}/include
 
 THREAD_LIBS?=	-lpthread
 LIBL?=		-ll
+
+# On altix
+ifneq ($(wildcard /opt/sgi),)
+#DEFINES+=      -DCONFIG_NR_CPUS=2 -D_GNU_SOURCE
+DEFINES+=      -DCONFIG_NR_CPUS=2
+PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
+endif
