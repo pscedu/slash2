5094114cfdfc761aa4735d3a2237b97a0ea2ef5a
Jared Yanovich <yanovich@psc.edu>
2009-03-03 22:59:38 +0000

    copy proper fuse pkgconfig invocations from mount_zest and add local conf for wolverine
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@5446 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- mk/local.mk
+++ mk/local.mk
@@ -6,6 +6,12 @@ DEFINES+=	-D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64 -DYY_NO_UNPUT
 INCLUDES+=	-I${KERNEL_BASE}/include
 
 THREAD_LIBS?=	-lpthread
+
+FUSE_INCLUDES=	`${PKG_CONFIG} --cflags fuse | perl -ne 'print $$& while /-I\S+\s?/gc'`
+FUSE_DEFINES=	`${PKG_CONFIG} --cflags fuse | perl -ne 'print $$& while /-D\S+\s?/gc'`
+FUSE_CFLAGS=	`${PKG_CONFIG} --cflags fuse | perl -ne 'print $$& while /-[^ID]\S+\s?/gc'`
+FUSE_LIBS=	`${PKG_CONFIG} --libs fuse`
+
 LIBL?=		-ll
 
 ZFS_LIBS=	-L${ZFS_BASE}/zfs-fuse				\
