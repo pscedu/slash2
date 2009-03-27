3e9363e3ff8d826b5526f8ac28b9b2b15d9f1e36
Jared Yanovich <yanovich@psc.edu>
2009-03-27 21:11:18 +0000

    use the default TARGET value, which is PROG or LIBRARY
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@5705 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- mk/main.mk
+++ mk/main.mk
@@ -9,8 +9,4 @@ INCLUDES+=	-I${SLASH_BASE} -I.
 CFLAGS+=	${INCLUDES} ${DEFINES}
 YFLAGS+=	-d -o $@
 
-# Default to build a binary, but may be overridden after
-# this file has been included, e.g. for a ${LIBRARY}.
-TARGET?=	${PROG}
-
 include ${MAINMK}
