e28916098b3121746e422ac7b2c98d5305dc68c5
Jared Yanovich <yanovich@psc.edu>
2009-06-05 17:01:41 +0000

    warnings/usage
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@6519 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- tests/crc/crc.c
+++ tests/crc/crc.c
@@ -1,21 +1,43 @@
+/* $Id$ */
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
 #include "psc_util/crc.h"
 
 #include "inode.h"
 
-void
-main(void)
+const char *progname;
+
+__dead void
+usage(void)
+{
+	fprintf(stderr, "usage: %s\n", progname);
+	exit(1);
+}
+
+int
+main(int argc, char *argv[])
 {
 	psc_crc_t crc;
 	char b[1048576];
 	sl_blkh_t bmapod;
 
-	memset(b, 0, 1048576);
-	PSC_CRC_CALC(crc, b, 1048576);
-	printf("NULL 1MB buf CRC is 0x%llx\n", crc);
-	
+	progname = argv[0];
+	if (getopt(argc, argv, "") != -1)
+		usage();
+	argc -= optind;
+	if (argc)
+		usage();
+
+	memset(b, 0, sizeof(b));
+	PSC_CRC_CALC(crc, b, sizeof(b));
+	printf("NULL 1MB buf CRC is %#"PRIx64"\n", crc);
+
 	memset(&bmapod, 0, sizeof(sl_blkh_t));
 	PSC_CRC_CALC(crc, &bmapod, sizeof(sl_blkh_t));
-	printf("NULL sl_blkh_t CRC is 0x%llx\n", crc);
+	printf("NULL sl_blkh_t CRC is %#"PRIx64"\n", crc);
+	exit(0);
 }
