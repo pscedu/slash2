7f9fb9c22d081003dc007f31844374b09fb3a6dc
Jared Yanovich <yanovich@psc.edu>
2009-06-12 19:38:26 +0000

    these endian macros should not be here
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@6585 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- include/slashrpc.h
+++ include/slashrpc.h
@@ -472,39 +472,6 @@ void slashrpc_export_destroy(void *);
 struct slashrpc_cservice *rpc_csvc_create(u32, u32);
 int rpc_issue_connect(lnet_nid_t, struct pscrpc_import *, u64, u32);
 
-#define p_ntoh_u16(v) ntoh16(v)
-#define p_ntoh_u32(v) ntoh32(v)
-
-#define p_hton_u16(v) hton16(v)
-#define p_hton_u32(v) hton32(v)
-
-#define p_ntoh_s16(v) ntoh16(v)
-#define p_ntoh_s32(v) ntoh32(v)
-
-#define p_hton_s16(v) hton16(v)
-#define p_hton_s32(v) hton32(v)
-
-#if __BYTE_ORDER == __LITTLE_ENDIAN
-# define p_ntoh_u64(v)		((((v) & UINT64_C(0x00000000000000ff)) << 56) |	\
-				 (((v) & UINT64_C(0x000000000000ff00)) << 40) |	\
-				 (((v) & UINT64_C(0x0000000000ff0000)) << 24) |	\
-				 (((v) & UINT64_C(0x00000000ff000000)) <<  8) |	\
-				 (((v) & UINT64_C(0x000000ff00000000)) >>  8) |	\
-				 (((v) & UINT64_C(0x0000ff0000000000)) >> 24) |	\
-				 (((v) & UINT64_C(0x00ff000000000000)) >> 40) |	\
-				 (((v) & UINT64_C(0xff00000000000000)) >> 56))
-# define p_hton_u64(v) p_ntoh_u64(v)
-
-# define p_ntoh_s64(v) ERROR
-# define p_hton_s64(v) ERROR
-#else
-# define p_ntoh_u64(v) (v)
-# define p_hton_u64(v) (v)
-
-# define p_ntoh_s64(v) (v)
-# define p_hton_s64(v) (v)
-#endif
-
 extern lnet_process_id_t lpid;
 
 #endif /* _SLASHRPC_H_ */
