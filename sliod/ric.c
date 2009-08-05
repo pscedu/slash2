b0a945e79e4c3173cddedbec70dfb641b8685b20
Paul Nowoczynski <pauln@psc.edu>
2009-08-05 15:17:52 +0000

    create a unified call to handle read and write rpc's.  this new io handler can process unaligned read and write requests, up to 1MB in size.  Further, the requested 1MB region does not have to fit into a single sliver though this version of code requires that it does have to fit into 2 slivers.
    this update will break ric.c for now..
    
    
    
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@7332 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- sliod/ric.c
+++ sliod/ric.c
@@ -103,18 +103,25 @@ slric_handle_read(struct pscrpc_request *rq)
 }
 
 int
-slric_handle_write(struct pscrpc_request *rq)
+slric_handle_io(struct pscrpc_request *rq, int rw)
 {
 	struct pscrpc_bulk_desc *desc;
 	struct slash_fidgen fg;
 	struct srm_io_req *mq;
-	struct srm_io_rep *mp;
-	struct iovec iov;
-	sl_blkno_t bmapno;
+	struct srm_io_rep *mp;	
+	struct iovec iovs[2];
+	struct fidc_membh *fcmh;
+	struct bmapc_memb *bmap;
+	struct slvr_ref *slvr[2];
+	
+	sl_blkno_t bmapno, slvrno;
 	ssize_t nbytes;
 	uint64_t cfd;
+	uint32_t tsize, roff;
 	void *buf;
-	int fd;
+	int fd, nslvrs=1, i;
+
+	psc_assert(rw == SL_READ || rw == SL_WRITE);
 
 	RSX_ALLOCREP(rq, mq, mp);
 
@@ -132,29 +139,84 @@ slric_handle_write(struct pscrpc_request *rq)
 		psc_errorx("fdbuf_decrypt failed");
 		return (0);
 	}
-
-	buf = PSCALLOC(mq->size);
-	iov.iov_base = buf;
-	iov.iov_len = mq->size;
-	if ((mp->rc = rsx_bulkserver(rq, &desc, BULK_GET_SINK,
-			     SRIC_BULK_PORTAL, &iov, 1)) == 0) {
-		if ((fd = fid_ocreat(fg.fg_fid)) == -1) {
-			psc_error("fid_ocreat failed "FIDFMT, FIDFMTARGS(&fg));
-			mp->rc = -errno;
-		} else {
-			nbytes = pwrite(fd, buf, mq->size, mq->offset);
-			if (nbytes == -1) {
-				psc_error("pwrite failed "FIDFMT,
-					  FIDFMTARGS(&fg));
-				mp->rc = -errno;
-			} else
-				mq->size = nbytes;
-			close(fd);
-		}
+	/* Ensure that this request fits into the bmap's address range.
+	 */	
+	if ((mq->offset + mq->size) >= ((bmapno + 1) * SLASH_BMAP_SIZE)) {
+		psc_errorx("req offset / size outside of the bmap's "
+			   "address range off=%u len=%u", 
+			   mq->offset, mq->size);
+		mp->rc = -ERANGE;
+		return (0);
 	}
+	/* Lookup inode and fetch bmap, don't forget to decref bmap
+	 *  on failure.
+	 */
+	fcmh = iod_inode_lookup(fg.fg_fid);
+	psc_assert(fcmh);
+	/* ATM, not much to do here for write operations.
+	 */
+	if (iod_bmap_load(fcmh, &mq->sbdb, rw, &bmap)) {
+		fidc_membh_dropref(fcmh);
+		psc_errorx("failed to load bmap %u", bmapno);
+		return (0);
+	}	
+	/* Ensure the fid in the local filesystem is created and open,
+	 *  otherwise fail.
+	 */
+	if (iod_inode_open(fcmh, rw)) {
+		fidc_membh_dropref(fcmh);
+		DEBUG_FCMH(PLL_ERROR, fcmh, "error fidopen bmap=%u", bmapno);
+		return (0);
+	}
+	
+	slvrno = mq->offset / SLASH_SLVR_SIZE;
+	/* We should never have a request size > 1MB, therefore it would 
+	 *  never exceed two slivers.
+	 */
+	if (((mq->offset + mq->size) / SLASH_SLVR_SIZE) > slvrno)
+		nslvrs++;
+
+	/* This loop assumes that nslvrs is always <= 2.  Note that  
+	 *   once i > 0, roff is always 0.
+	 */
+	for (i=0, roff=(mq->offset - (slvrno*SLASH_SLVR_SIZE)), tsize=mq->size;
+	     i < nslvrs; i++, roff=0) {
+
+		slvr_ref[i] = slvr_lookup(slvrno + i, bmap_2_biodi(bmap), 1);
+		slvr_slab_prep(slvr_ref[i]);
+		/* Fault in pages either for read or RBW.
+		 */
+		slvr_io_prep(slvr_ref[i], roff, tsize, rw);
+
+		DEBUG_SLVR(PLL_TRACE, slvr_ref[i], "post io_prep");
+		/* mq->offset is the offset into the bmap, here we must
+		 *  translate it into the offset of the sliver.
+		 */
+		iovs[i].iov_base = slvr_ref[i]->slvr_slab->slb_base + roff;
+		tsize -= iovs[i].iov_len = MIN(tsize, SLASH_SLVR_SIZE - roff);
+	}
+
+	mp->rc = rsx_bulkserver(rq, &desc, 
+			(rw == SL_WRITE ? BULK_GET_SINK : BULK_PUT_SOURCE), 
+			SRIC_BULK_PORTAL, iovs, nslvrs);
+		  
+	if (mp->rc)
+		return (0);
+
+	/* Write the sliver back to the filesystem, but only the blocks
+	 *   which are marked '0' in the bitmap.   Here we don't care about
+	 *   buffer offsets since we're block aligned now
+	 */ 
+	for (i=0; i < nslvrs; i++) {	
+		if (rw == SL_WRITE)
+			if ((rc = slvr_fsbytes_io(s, SL_WRITE)))
+				return (rc);
+		slvr_io_done(s, rw);
+	}
+
 	if (desc)
 		pscrpc_free_bulk(desc);
-	free(buf);
+
 	return (0);
 }
 
