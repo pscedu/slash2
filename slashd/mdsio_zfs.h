50ba888004c0a1eb494fe4f1fbb37f95c11a92ce
Jared Yanovich <yanovich@psc.edu>
2009-08-26 18:47:23 +0000

    add proto for mdsio_zfs_release()
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@7715 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- slashd/mdsio_zfs.h
+++ slashd/mdsio_zfs.h
@@ -3,27 +3,15 @@
 #ifndef _SLASHD_MDSIO_H_
 #define _SLASHD_MDSIO_H_
 
-#include "bmap.h"
-#include "inodeh.h"
+struct bmapc_memb;
+struct slash_inode_handle;
 
-extern void *zfsVfs;
-
-extern int
-mdsio_zfs_bmap_read(struct bmapc_memb *bmap);
-
-extern int
-mdsio_zfs_bmap_write(struct bmapc_memb *bmap);
-
-extern int
-mdsio_zfs_inode_read(struct slash_inode_handle *i);
-
-extern int
-mdsio_zfs_inode_write(struct slash_inode_handle *i);
-
-extern int
-mdsio_zfs_inode_extras_read(struct slash_inode_handle *i);
-
-extern int
-mdsio_zfs_inode_extras_write(struct slash_inode_handle *i);
+int mdsio_zfs_bmap_read(struct bmapc_memb *);
+int mdsio_zfs_bmap_write(struct bmapc_memb *);
+int mdsio_zfs_inode_extras_read(struct slash_inode_handle *);
+int mdsio_zfs_inode_extras_write(struct slash_inode_handle *);
+int mdsio_zfs_inode_read(struct slash_inode_handle *);
+int mdsio_zfs_inode_write(struct slash_inode_handle *);
+int mdsio_zfs_release(struct slash_inode_handle *);
 
 #endif
