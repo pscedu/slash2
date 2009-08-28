be45266e1b7f63ac7db3d6979f295c1f3bb53eac
Jared Yanovich <yanovich@psc.edu>
2009-08-28 20:32:10 +0000

    add proto for mds_inode_release()
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@7753 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- slashd/mdslog.h
+++ slashd/mdslog.h
@@ -3,26 +3,18 @@
 #ifndef _SLASHD_MDSLOG_H_
 #define _SLASHD_MDSLOG_H_
 
-#include "psc_types.h"
 #include "inode.h"
-#include "inodeh.h"
-#include "bmap.h"
-#include "slashrpc.h"
 
-extern void
-mds_inode_sync(void *);
+struct bmapc_memb;
+struct fidc_membh;
+struct slash_inode_handle;
+struct srm_bmap_crcup;
 
-extern void
-mds_inode_addrepl_log(struct slash_inode_handle *, sl_ios_id_t, uint32_t);
+int  mds_inode_release(struct fidc_membh *);
+void mds_bmap_crc_log(struct bmapc_memb *, struct srm_bmap_crcup *);
+void mds_bmap_repl_log(struct bmapc_memb *);
+void mds_bmap_sync(void *);
+void mds_inode_addrepl_log(struct slash_inode_handle *, sl_ios_id_t, uint32_t);
+void mds_inode_sync(void *);
 
-extern void
-mds_bmap_repl_log(struct bmapc_memb *);
-
-extern void
-mds_bmap_crc_log(struct bmapc_memb *, struct srm_bmap_crcup *);
-
-extern void
-mds_bmap_sync(void *);
-
-
-#endif
+#endif /* _SLASHD_MDSLOG_H_ */
