dfd86891360ff9531c028dac4293d94066ef86aa
Jared Yanovich <yanovich@psc.edu>
2009-09-30 21:46:42 +0000

    grab last range to make this ipv6 safe
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@8170 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- share/nidport.c
+++ share/nidport.c
@@ -1,24 +1,33 @@
 /* $Id$ */
 
-#include <stdio.h>
+#include <netinet/in.h>
 
-void
+#include <stdio.h>
+#include <string.h>
+
+/*
+ * nidstr_extractport - Remove TCP port from a nidstr and
+ *	return its numerical value.
+ * @nidstr: NID string.
+ * @port: value-result port value.
+ */
+int
 nidstr_extractport(char *nidstr, in_port_t *port)
 {
 	char *p, *s, *endp;
 	long l;
 
 	*port = 0;
-	if ((p = strchr(nidstr, ':')) != NULL) {
+	if ((p = strrchr(nidstr, ':')) != NULL) {
 		if ((s = strchr(p, '@')) == NULL)
-			return;
+			return (-1);
 		*s++ = '\0';
 
 		/* Extract port */
 		l = strtol(p + 1, &endp, 10);
 		if (l == LONG_MIN || l == LONG_MAX ||
 		    endp + 1 != s || endp == p + 1)
-			return;
+			return (-1);
 		*port = l;
 
 		/* Copy network overtop of port */
@@ -27,4 +36,5 @@ nidstr_extractport(char *nidstr, in_port_t *port)
 			*p++ = *s++;
 		*p = '\0';
 	}
+	return (0);
 }
