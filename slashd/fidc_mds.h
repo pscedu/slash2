af0e6563284588eaf9fe002261cab231258263e8
Jared Yanovich <yanovich@psc.edu>
2009-10-03 05:15:10 +0000

    add proto for mds_fcmh_apply_fsize()
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@8204 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- slashd/fidc_mds.h
+++ slashd/fidc_mds.h
@@ -20,12 +20,8 @@ struct fidc_mds_info {
 	void                     *fmdsi_data;
 };
 
-#define fcmh_2_inoh(f)							\
-	(&((struct fidc_mds_info *)(f)->fcmh_fcoo->fcoo_pri)->fmdsi_inodeh)
-
-#define fcmh_2_fmdsi(f)						\
-	((struct fidc_mds_info *)(f)->fcmh_fcoo->fcoo_pri)
-
+#define fcmh_2_fmdsi(f)	((struct fidc_mds_info *)(f)->fcmh_fcoo->fcoo_pri)
+#define fcmh_2_inoh(f)	(&fcmh_2_fmdsi(f)->fmdsi_inodeh)
 
 static inline void
 fmdsi_init(struct fidc_mds_info *mdsi, struct fidc_membh *fcmh, void *pri)
@@ -40,5 +36,7 @@ fmdsi_init(struct fidc_mds_info *mdsi, struct fidc_membh *fcmh, void *pri)
 
 struct fidc_mds_info *fidc_fid2fmdsi(slfid_t, struct fidc_membh **);
 struct fidc_mds_info *fidc_fcmh2fmdsi(struct fidc_membh *);
+
+int mds_fcmh_apply_fsize(struct fidc_membh *, uint64_t);
 
 #endif /* _FIDC_MDS_H_ */
