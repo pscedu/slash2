04c164222682401e740f18903f3dfdc86967f15f
Jared Yanovich <yanovich@psc.edu>
2009-10-26 19:01:33 +0000

    remove INUM_SELF_MANAGE and spacing
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@8610 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- slashd/mdslog.c
+++ slashd/mdslog.c
@@ -10,13 +10,13 @@
 #include "psc_util/lock.h"
 #include "psc_util/log.h"
 
-#include "sljournal.h"
-#include "inode.h"
+#include "fidc_mds.h"
 #include "fidcache.h"
+#include "inode.h"
 #include "jflush.h"
-#include "mds_fidc.h"
 #include "mdsexpc.h"
 #include "mdsio_zfs.h"
+#include "sljournal.h"
 
 #ifdef INUM_SELF_MANAGE
 #include "sb.h"
@@ -38,17 +38,17 @@ mds_inode_sync(void *data)
 {
 	int locked, rc;
 	struct slash_inode_handle *inoh = data;
-	
+
 	locked = reqlock(&inoh->inoh_lock);
 
 	psc_assert((inoh->inoh_flags & INOH_INO_DIRTY) ||
 		   (inoh->inoh_flags & INOH_EXTRAS_DIRTY));
 
 	if (inoh->inoh_flags & INOH_INO_DIRTY) {
-		psc_crc_calc(&inoh->inoh_ino.ino_crc, 
+		psc_crc_calc(&inoh->inoh_ino.ino_crc,
 			     &inoh->inoh_ino, INO_OD_CRCSZ);
 		rc = mdsio_zfs_inode_write(inoh);
-		
+
 		if (rc)
 			DEBUG_INOH(PLL_FATAL, inoh, "rc=%d sync fail", rc);
 
@@ -62,11 +62,11 @@ mds_inode_sync(void *data)
 		rc = mdsio_zfs_inode_extras_write(inoh);
 
 		if (rc)
-			DEBUG_INOH(PLL_FATAL, inoh, "xtras rc=%d sync fail", 
+			DEBUG_INOH(PLL_FATAL, inoh, "xtras rc=%d sync fail",
 				   rc);
 		else
 			DEBUG_INOH(PLL_TRACE, inoh, "xtras sync ok");
-		
+
 		inoh->inoh_flags &= ~INOH_EXTRAS_DIRTY;
 	}
 
@@ -100,7 +100,7 @@ mds_bmap_sync(void *data)
 	psc_crc_calc(&bmapod->bh_bhcrc, bmapod, BMAP_OD_CRCSZ);
 	rc = mdsio_zfs_bmap_write(bmap);
 	if (rc)
-		DEBUG_BMAP(PLL_FATAL, bmap, "rc=%d errno=%d sync fail", 
+		DEBUG_BMAP(PLL_FATAL, bmap, "rc=%d errno=%d sync fail",
 			   rc, errno);
 	else
 		DEBUG_BMAP(PLL_INFO, bmap, "sync ok");
@@ -109,38 +109,38 @@ mds_bmap_sync(void *data)
 
 
 void
-mds_inode_addrepl_log(struct slash_inode_handle *inoh, sl_ios_id_t ios, 
+mds_inode_addrepl_log(struct slash_inode_handle *inoh, sl_ios_id_t ios,
 		      uint32_t pos)
 {
 	int rc;
-	struct slmds_jent_ino_addrepl jrir = { fcmh_2_fid(inoh->inoh_fcmh), 
+	struct slmds_jent_ino_addrepl jrir = { fcmh_2_fid(inoh->inoh_fcmh),
 					       ios, pos };
 
 	INOH_LOCK_ENSURE(inoh);
 	psc_assert((inoh->inoh_flags & INOH_INO_DIRTY) ||
 		   (inoh->inoh_flags & INOH_EXTRAS_DIRTY));
-	
+
 	psc_trace("jlog fid=%"PRIx64" ios=%u pos=%u",
-                  jrir.sjir_fid, jrir.sjir_ios, jrir.sjir_pos);
+		  jrir.sjir_fid, jrir.sjir_ios, jrir.sjir_pos);
 
 	jfi_prep(&inoh->inoh_jfi, mdsJournal);
 	psc_assert(inoh->inoh_jfi.jfi_handler == mds_inode_sync);
-        psc_assert(inoh->inoh_jfi.jfi_data == inoh);
+	psc_assert(inoh->inoh_jfi.jfi_data == inoh);
 
-	rc = pjournal_xadd(inoh->inoh_jfi.jfi_xh, MDS_LOG_INO_ADDREPL, &jrir, 
+	rc = pjournal_xadd(inoh->inoh_jfi.jfi_xh, MDS_LOG_INO_ADDREPL, &jrir,
 			   sizeof(struct slmds_jent_ino_addrepl));
 	if (rc)
 		psc_trace("jlog fid=%"PRIx64" ios=%x pos=%u rc=%d",
 			  jrir.sjir_fid, jrir.sjir_ios, jrir.sjir_pos, rc);
 
-	jfi_schedule(&inoh->inoh_jfi, &dirtyMdsData);	
+	jfi_schedule(&inoh->inoh_jfi, &dirtyMdsData);
 }
 
 /**
  * mds_bmap_repl_log - write a modified replication table to the journal.
  * Note:  bmap must be locked to prevent further changes from sneaking in
  *	before the repl table is committed to the journal.
- * XXX Another case for a rwlock, currently this code holds the lock while 
+ * XXX Another case for a rwlock, currently this code holds the lock while
  *     the doing IO to the journal.
  */
 void
@@ -169,7 +169,7 @@ mds_bmap_repl_log(struct bmapc_memb *bmap)
 	psc_assert(bmdsi->bmdsi_jfi.jfi_handler == mds_bmap_sync);
 	psc_assert(bmdsi->bmdsi_jfi.jfi_data == bmap);
 
-	rc = pjournal_xadd(bmdsi->bmdsi_jfi.jfi_xh, MDS_LOG_BMAP_REPL, &jrpg, 
+	rc = pjournal_xadd(bmdsi->bmdsi_jfi.jfi_xh, MDS_LOG_BMAP_REPL, &jrpg,
 			   sizeof(struct slmds_jent_repgen));
 	if (rc)
 		psc_fatalx("jlog fid=%"PRIx64" bmapno=%u bmapgen=%u rc=%d",
@@ -214,7 +214,7 @@ mds_bmap_crc_log(struct bmapc_memb *bmap, struct srm_bmap_crcup *crcup)
 
 	jcrc->sjc_fid = fcmh_2_fid(bmap->bcm_fcmh);
 	jcrc->sjc_ion = bmdsi->bmdsi_wr_ion->mi_resm->resm_nid;
-        jcrc->sjc_bmapno = bmap->bcm_blkno;
+	jcrc->sjc_bmapno = bmap->bcm_blkno;
 	jcrc->sjc_ncrcs = n;
 
 	while (n) {
@@ -223,7 +223,7 @@ mds_bmap_crc_log(struct bmapc_memb *bmap, struct srm_bmap_crcup *crcup)
 		memcpy(jcrc->sjc_crc, &crcup->crcs[t],
 		       (i * sizeof(struct srm_bmap_crcwire)));
 
-		rc = pjournal_xadd(bmdsi->bmdsi_jfi.jfi_xh, MDS_LOG_BMAP_CRC, 
+		rc = pjournal_xadd(bmdsi->bmdsi_jfi.jfi_xh, MDS_LOG_BMAP_CRC,
 				   jcrc, sizeof(struct slmds_jent_crc));
 		if (rc)
 			psc_fatalx("jlog fid=%"PRIx64" bmapno=%u rc=%d",
@@ -266,83 +266,10 @@ mds_journal_init(void)
 	int rc;
 
 	rc = snprintf(fn, sizeof(fn), "%s/%s",
-                      nodeInfo.node_res->res_fsroot, _PATH_SLJOURNAL);
-        if (rc == -1)
-                psc_fatal("snprintf");
+		      nodeInfo.node_res->res_fsroot, _PATH_SLJOURNAL);
+	if (rc == -1)
+		psc_fatal("snprintf");
 
 	mdsJournal = pjournal_replay(_PATH_SLJOURNAL, mds_journal_replay);
 	psc_assert(mdsJournal);
 }
-
-#ifdef INUM_SELF_MANAGE
-void
-mds_sb_sync(void *data)
-{
-	struct slash_sb_mem *sb=data;
-	int rc;
-
-	spinlock(&sb->sbm_lock);
-
-	psc_crc_calc(&sb->sbm_sbs->sbs_crc, sb->sbm_sbs, SBS_OD_CRCSZ);
-	rc = msync(sb->sbm_sbs, SBS_OD_SZ, MS_SYNC);
-	if (rc < 0)
-		psc_fatal("msync() of sb failed");
-
-	freelock(&sb->sbm_lock);
-
-	psc_dbg("sb=%p inum=%"PRIx64" crc="PRIxCRC" sync ok",
-		sb, sb->sbm_sbs->sbs_inum, sb->sbm_sbs->sbs_crc);
-}
-
-/**
- * mds_sb_getinum - get the next inode number from the sb structure.
- * Notes:  the inode is composed of a counter of size SL_SUPER_INODE_BITS
- *    which is prepended by the mds id bits.
- * Notes1: mds_sb_getinum() does not hold the sb lock while performing
- *    journal I/O.  This is because the highest sji_inum number is taken
- *    from the journal (upon restart) not the value at the latest position.
- *    So in other words, out of order jentries for inum updates don't matter.
- */
-sl_inum_t
-mds_sb_getinum(void)
-{
-	struct slmds_jent_inum sji;
-	sl_inum_t ino=0;
-
-	spinlock(&sbm.sbm_lock);
-
-	if (!((ino = ++sbm.sbm_inum_minor) % SLMDS_INUM_ALLOC_SZ)) {
-		struct jflush_item *jfi = &sbm.sbm_jfi;
-
-		sji.sji_inum = ++sbm.sbm_sbs->sbs_inum_major;
-		freelock(&sbm.sbm_lock);
-
-		psc_dbg("bumped inum_maj=%"PRId64, sji.sji_inum);
-
-		jfi_prep(jfi, &mdsJournal);
-		psc_assert(jfi->jfi_handler == mds_sb_sync);
-		psc_assert(jfi->jfi_data == &sbm);
-
-		if (pjournal_xadd(jfi->jfi_xh, MDS_LOG_SB, &sji, 
-				  sizeof(struct slmds_jent_inum)))
-			psc_fatalx("jlog sb pjournal_xadd() failed");
-
-		jfi_schedule(jfi, &dirtyMdsData);
-
-	} else {
-		sji.sji_inum = sbm.sbm_sbs.sbs_inum_major;
-		freelock(&sbm.sbm_lock);
-	}
-	/* Add the major bits to the ones already stored in ino.
-	 */
-	ino += (sji.sji_inum * SLMDS_INUM_ALLOC_SZ);
-	/* Prepend the mds_id bits to the ino.
-	 */
-	ino |= (sbm.sbm_sbs->sbs_mds_id << SL_SUPER_INODE_BITS);
-
-	psc_trace("ino=%"PRId64" inum_maj=%"PRId64" mds_id=%u",
-		  ino, sji.sji_inum, sbm.sbm_sbs->sbs_mds_id);
-
-	return (ino);
-}
-#endif
