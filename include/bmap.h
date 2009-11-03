102669a43e7caaf48aeb6848e5a34051355bf534
Zhihui Zhang <zhihui@psc.edu>
2009-11-03 16:26:08 +0000

    comment
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@8788 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- include/bmap.h
+++ include/bmap.h
@@ -44,8 +44,8 @@ struct bmapc_memb {
 	struct psc_waitq	 bcm_waitq;     /* XXX think about replacing
 						   me with bcm_fcmh->fcmh_waitq
 						*/
-	SPLAY_ENTRY(bmapc_memb)	 bcm_tentry;	/* fcm tree entry    */
-	struct psclist_head	 bcm_lentry;
+	SPLAY_ENTRY(bmapc_memb)	 bcm_tentry;	/* fcm splay tree entry    */
+	struct psclist_head	 bcm_lentry;	/* free pool */
 	void			*bcm_pri;	/* bmap_[mds|cli|iod]_info structure */
 };
 
