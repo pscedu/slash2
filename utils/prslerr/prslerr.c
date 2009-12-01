f54e779211ad5e7dcce6a82aab40358f51e382f5
Jared Yanovich <yanovich@psc.edu>
2009-12-01 23:35:39 +0000

    regen
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@9244 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- utils/prslerr/prslerr.c
+++ utils/prslerr/prslerr.c
@@ -36,7 +36,9 @@ main(int argc, char *argv[])
 	printf("%4d [SLERR_UNKNOWN_IOS]: %s\n", SLERR_UNKNOWN_IOS, slstrerror(SLERR_UNKNOWN_IOS));
 	printf("%4d [SLERR_ION_UNKNOWN]: %s\n", SLERR_ION_UNKNOWN, slstrerror(SLERR_ION_UNKNOWN));
 	printf("%4d [SLERR_ION_OFFLINE]: %s\n", SLERR_ION_OFFLINE, slstrerror(SLERR_ION_OFFLINE));
+	printf("%4d [SLERR_ION_NOTREPL]: %s\n", SLERR_ION_NOTREPL, slstrerror(SLERR_ION_NOTREPL));
 	printf("%4d [SLERR_XACT_FAIL]: %s\n", SLERR_XACT_FAIL, slstrerror(SLERR_XACT_FAIL));
+	printf("%4d [SLERR_SHORTIO]: %s\n", SLERR_SHORTIO, slstrerror(SLERR_SHORTIO));
 	/* end custom errnos */
 	exit(0);
 }
