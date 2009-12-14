326ebcd5e7cf6ef0d997cc4633bdfb0270e84cfb
Jared Yanovich <yanovich@psc.edu>
2009-12-14 23:12:31 +0000

    move sliver definitions to slvr.h where they belong
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@9493 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- sliod/bmap_iod.h
+++ sliod/bmap_iod.h
@@ -12,12 +12,12 @@
 #include "psc_util/lock.h"
 
 #include "bmap.h"
-#include "inode.h"
 #include "fidc_iod.h"
-#include "slvr.h"
+#include "inode.h"
 #include "slashrpc.h"
 
-extern struct psc_listcache iodBmapLru;
+struct bmap_iod_info;
+struct slvr_ref;
 
 /* For now only one of these structures is needed.  In the future
  *   we'll need one per MDS.
@@ -29,8 +29,6 @@ struct biod_infl_crcs {
 	struct psc_lockedlist   binfcrcs_ready;
 	struct psc_lockedlist   binfcrcs_infl;
 };
-
-struct bmap_iod_info;
 
 struct biod_crcup_ref {
 	uint64_t			 bcr_xid;
@@ -45,17 +43,16 @@ struct biod_crcup_ref {
 #define BCR_SCHEDULED                    0x01
 
 #define DEBUG_BCR(level, b, fmt, ...)					\
-        psc_logs((level), PSS_GEN,                                      \
-                 "bcr@%p fid="FIDFMT" xid=%"PRIu64" nups=%d fl=%d age=%lu" \
-                 " bmap@%p:%u :: "fmt,					\
-                 (b), FIDFMTARGS(&(b)->bcr_crcup.fg), (b)->bcr_xid,	\
+	psc_logs((level), PSS_GEN,                                      \
+		 "bcr@%p fid="FIDFMT" xid=%"PRIu64" nups=%d fl=%d age=%lu" \
+		 " bmap@%p:%u :: "fmt,					\
+		 (b), FIDFMTARGS(&(b)->bcr_crcup.fg), (b)->bcr_xid,	\
 		 (b)->bcr_crcup.nups, (b)->bcr_flags, (b)->bcr_age.tv_sec, \
 		 (b)->bcr_biodi->biod_bmap,				\
 		 (b)->bcr_biodi->biod_bmap->bcm_blkno,			\
 		 ## __VA_ARGS__)
 
 SPLAY_HEAD(biod_slvrtree, slvr_ref);
-SPLAY_PROTOTYPE(biod_slvrtree, slvr_ref, slvr_tentry, slvr_cmp);
 
 struct bmap_iod_info {
 	psc_spinlock_t          biod_lock;
@@ -70,17 +67,15 @@ struct bmap_iod_info {
 	uint32_t                biod_inflight;
 };
 
-
-
-static inline void
+static __inline void
 bcr_hold_2_ready(struct biod_infl_crcs *inf, struct biod_crcup_ref *bcr)
 {
 	int locked;
 
-	LOCK_ENSURE(&bcr->bcr_biodi->biod_lock);	
+	LOCK_ENSURE(&bcr->bcr_biodi->biod_lock);
 	psc_assert(bcr->bcr_biodi->biod_bcr == bcr);
 
-	locked = reqlock(&inf->binfcrcs_lock);	
+	locked = reqlock(&inf->binfcrcs_lock);
 	psc_assert(psclist_conjoint(&bcr->bcr_lentry));
 	pll_remove(&inf->binfcrcs_hold, bcr);
 	pll_addtail(&inf->binfcrcs_ready, bcr);
@@ -89,7 +84,7 @@ bcr_hold_2_ready(struct biod_infl_crcs *inf, struct biod_crcup_ref *bcr)
 	bcr->bcr_biodi->biod_bcr = NULL;
 }
 
-static inline void
+static __inline void
 bcr_hold_add(struct biod_infl_crcs *inf, struct biod_crcup_ref *bcr)
 {
 	psc_assert(psclist_disjoint(&bcr->bcr_lentry));
@@ -97,7 +92,7 @@ bcr_hold_add(struct biod_infl_crcs *inf, struct biod_crcup_ref *bcr)
 	atomic_inc(&inf->binfcrcs_nbcrs);
 }
 
-static inline void
+static __inline void
 bcr_hold_requeue(struct biod_infl_crcs *inf, struct biod_crcup_ref *bcr)
 {
 	int locked;
@@ -109,7 +104,7 @@ bcr_hold_requeue(struct biod_infl_crcs *inf, struct biod_crcup_ref *bcr)
 	ureqlock(&inf->binfcrcs_lock, locked);
 }
 
-static inline void
+static __inline void
 bcr_xid_check(struct biod_crcup_ref *bcr)
 {
 	int locked;
@@ -120,18 +115,18 @@ bcr_xid_check(struct biod_crcup_ref *bcr)
 	ureqlock(&bcr->bcr_biodi->biod_lock, locked);
 }
 
-static inline void
+static __inline void
 bcr_xid_last_bump(struct biod_crcup_ref *bcr)
 {
 	int locked;
 
-        locked = reqlock(&bcr->bcr_biodi->biod_lock);
+	locked = reqlock(&bcr->bcr_biodi->biod_lock);
 	bcr_xid_check(bcr);
 	bcr->bcr_biodi->biod_bcr_xid_last++;
 	ureqlock(&bcr->bcr_biodi->biod_lock, locked);
 }
 
-static inline void
+static __inline void
 bcr_ready_remove(struct biod_infl_crcs *inf, struct biod_crcup_ref *bcr)
 {
 	spinlock(&inf->binfcrcs_lock);
@@ -144,7 +139,6 @@ bcr_ready_remove(struct biod_infl_crcs *inf, struct biod_crcup_ref *bcr)
 	bcr_xid_last_bump(bcr);
 	PSCFREE(bcr);
 }
-
 
 #if 0
 static inline int
@@ -166,7 +160,7 @@ bcr_cmp(const void *x, const void *y)
 #define bmap_2_biodi_slvrs(b)	(&bmap_2_biodi(b)->biod_slvrs)
 #define bmap_2_biodi_wire(b)	bmap_2_biodi(b)->biod_bmap_wire
 
-#define BIOD_CRCUP_MAX_AGE		 2		/* in seconds */
+#define BIOD_CRCUP_MAX_AGE	 2		/* in seconds */
 
 enum slash_bmap_slv_states {
 	BMAP_SLVR_DATA = (1<<0), /* Data present, otherwise slvr is hole */
@@ -174,117 +168,6 @@ enum slash_bmap_slv_states {
 	//XXX ATM, 6 bits are left
 };
 
-#define slvr_2_biod(s)		((struct bmap_iod_info *)(s)->slvr_pri)
-#define slvr_2_bmap(s)		slvr_2_biod(s)->biod_bmap
-#define slvr_2_fcmh(s)		slvr_2_biod(s)->biod_bmap->bcm_fcmh
-#define slvr_2_fd(s)		slvr_2_fcmh(s)->fcmh_fcoo->fcoo_fd
-#define slvr_2_biodi_wire(s)	slvr_2_biod(s)->biod_bmap_wire
-
-#define slvr_2_buf(s, blk)						\
-	(void *)(((s)->slvr_slab->slb_base) + ((blk) * SLASH_SLVR_BLKSZ))
-
-#define slvr_2_fileoff(s, blk)						\
-	(off_t)((slvr_2_bmap(s)->bcm_blkno * SLASH_BMAP_SIZE) +		\
-		((s)->slvr_num * SLASH_SLVR_SIZE) +			\
-		((blk) * SLASH_SLVR_BLKSZ))
-
-#define slvr_2_crcbits(s)						\
-	slvr_2_biodi_wire((s))->bh_crcstates[(s)->slvr_num]
-
-#define slvr_2_crc(s)							\
-	slvr_2_biodi_wire((s))->bh_crcs[(s)->slvr_num].gc_crc
-
-#define SLVR_GETLOCK(s)		(&(slvr_2_biod(s))->biod_lock)
-#define SLVR_LOCK(s)		spinlock(SLVR_GETLOCK(s))
-#define SLVR_ULOCK(s)		freelock(SLVR_GETLOCK(s))
-#define SLVR_RLOCK(s)		reqlock(SLVR_GETLOCK(s))
-#define SLVR_URLOCK(s, lk)	ureqlock(SLVR_GETLOCK(s), (lk))
-#define SLVR_LOCK_ENSURE(s)	LOCK_ENSURE(SLVR_GETLOCK(s))
-#define SLVR_TRYLOCK(s)		trylock(SLVR_GETLOCK(s))
-#define SLVR_TRYREQLOCK(s, lk)	tryreqlock(SLVR_GETLOCK(s), lk)
-
-#define SLVR_WAKEUP(s)							\
-	psc_waitq_wakeall(&(slvr_2_bmap((s)))->bcm_waitq)
-
-#define SLVR_WAIT(s)							\
-	do {								\
-		DEBUG_SLVR(PLL_NOTIFY, (s), "SLVR_WAIT");		\
-		while (!((s)->slvr_flags & SLVR_DATARDY)) {		\
-			psc_waitq_wait(&(slvr_2_bmap((s)))->bcm_waitq,	\
-				       &(slvr_2_biod((s)))->biod_lock);	\
-			SLVR_LOCK((s));					\
-		}							\
-	} while (0)
-
-static inline void
-slvr_lru_pin_check(struct slvr_ref *s)
-{
-	SLVR_LOCK_ENSURE(s);
-	psc_assert(s->slvr_slab && psclist_conjoint(&s->slvr_lentry));
-	psc_assert(s->slvr_flags == (SLVR_LRU|SLVR_PINNED));
-}
-
-static inline int
-slvr_lru_tryunpin_locked(struct slvr_ref *s)
-{
-	SLVR_LOCK_ENSURE(s);
-	psc_assert(s->slvr_slab);
-	if (s->slvr_pndgwrts || s->slvr_pndgreads ||
-	    s->slvr_flags & SLVR_CRCDIRTY)
-		return (0);
-
-	psc_assert(s->slvr_flags & SLVR_LRU);
-	psc_assert(s->slvr_flags & SLVR_PINNED);
-	psc_assert(s->slvr_flags & SLVR_DATARDY);
-
-	psc_assert(!(s->slvr_flags &
-		     (SLVR_NEW|SLVR_FAULTING|
-		      SLVR_GETSLAB|SLVR_CRCDIRTY)));
-
-	s->slvr_flags &= ~SLVR_PINNED;
-	return (1);
-}
-
-static inline int
-slvr_lru_slab_freeable(struct slvr_ref *s)	
-{
-	int freeable = 1;
-	SLVR_LOCK_ENSURE(s);
-
-	psc_assert(s->slvr_flags & SLVR_LRU);
-
-	if (s->slvr_flags & SLVR_DATARDY)
-		psc_assert(!(s->slvr_flags &
-			     (SLVR_NEW|SLVR_FAULTING|SLVR_GETSLAB)));
-
-	if (!s->slvr_slab)
-		psc_assert(!(s->slvr_flags & 
-			     (SLVR_NEW|SLVR_FAULTING|
-			      SLVR_GETSLAB|SLVR_DATARDY)));		
-	
-	if (s->slvr_flags & SLVR_PINNED)
-		freeable = 0;
-
-	DEBUG_SLVR(PLL_INFO, s, "freeable=%d", freeable);
-
-	return (freeable);
-}
-
-static inline int
-slvr_lru_freeable(struct slvr_ref *s)
-{
-	int freeable=0;
-
-	if (s->slvr_slab ||
-	    s->slvr_flags & SLVR_PINNED   ||
-	    s->slvr_flags & SLVR_CRCDIRTY)
-		goto out;
-
-	psc_assert(slvr_lru_slab_freeable(s));
-
-	freeable = 1;
- out:
-	return (freeable);
-}
+extern struct psc_listcache iodBmapLru;
 
 #endif /* _SLASH_IOD_BMAP_H_ */
--- sliod/slvr.h
+++ sliod/slvr.h
@@ -9,9 +9,10 @@
 #include "psc_util/atomic.h"
 #include "psc_util/log.h"
 
-#include "slashrpc.h"
 #include "bmap.h"
+#include "bmap_iod.h"
 #include "buffer.h"
+#include "slashrpc.h"
 
 struct bmap_iod_info;
 
@@ -30,13 +31,11 @@ struct slvr_ref {
 	psc_crc64_t		 slvr_crc;		/* used if there's no bmap_wire present, only is valid if !SLVR_CRCDIRTY */
 	void			*slvr_pri;		/* private pointer used for backpointer to bmap_iod_info */
 	struct sl_buffer	*slvr_slab;
-	struct psclist_head	 slvr_lentry;		/* dirty queue */	
+	struct psclist_head	 slvr_lentry;		/* dirty queue */
 	SPLAY_ENTRY(slvr_ref)	 slvr_tentry;		/* bmap tree entry */
 };
 
-#define SLVR_2_BLK(s) ((s)->slvr_num * (SLASH_BMAP_SIZE/SLASH_BMAP_BLKSZ))
-
-enum slvr_states {
+enum {
 	SLVR_NEW	= (1 <<  0),	/* newly initialized */
 	SLVR_SPLAYTREE	= (1 <<  1),	/* registered in the splay tree */
 	SLVR_UNUSED1	= (1 <<  2),	/* unused1 */
@@ -54,22 +53,66 @@ enum slvr_states {
 	SLVR_REPLDST	= (1 << 14)     /* slvr is replication destination */
 };
 
+#define SLVR_2_BLK(s)		((s)->slvr_num * (SLASH_BMAP_SIZE / SLASH_BMAP_BLKSZ))
 
-#define SLVR_FLAG(field, str) ((field) ? (str) : "-")
-#define DEBUG_SLVR_FLAGS(s)						\
-	SLVR_FLAG(((s)->slvr_flags & SLVR_NEW), "n"),			\
-		SLVR_FLAG(((s)->slvr_flags & SLVR_FAULTING), "f"),	\
-		SLVR_FLAG(((s)->slvr_flags & SLVR_GETSLAB), "G"),	\
-		SLVR_FLAG(((s)->slvr_flags & SLVR_PINNED), "p"),	\
-		SLVR_FLAG(((s)->slvr_flags & SLVR_CRCDIRTY), "D"),	\
-		SLVR_FLAG(((s)->slvr_flags & SLVR_DATARDY), "d"),	\
-		SLVR_FLAG(((s)->slvr_flags & SLVR_LRU), "l"),		\
-		SLVR_FLAG(((s)->slvr_flags & SLVR_FREEING), "F"),	\
-		SLVR_FLAG(((s)->slvr_flags & SLVR_SLBFREEING), "b"),	\
-		SLVR_FLAG(((s)->slvr_flags & SLVR_REPLSRC), "S"),	\
-		SLVR_FLAG(((s)->slvr_flags & SLVR_REPLDST), "T")	\
+#define SLVR_GETLOCK(s)		(&(slvr_2_biod(s))->biod_lock)
+#define SLVR_LOCK(s)		spinlock(SLVR_GETLOCK(s))
+#define SLVR_ULOCK(s)		freelock(SLVR_GETLOCK(s))
+#define SLVR_RLOCK(s)		reqlock(SLVR_GETLOCK(s))
+#define SLVR_URLOCK(s, lk)	ureqlock(SLVR_GETLOCK(s), (lk))
+#define SLVR_LOCK_ENSURE(s)	LOCK_ENSURE(SLVR_GETLOCK(s))
+#define SLVR_TRYLOCK(s)		trylock(SLVR_GETLOCK(s))
+#define SLVR_TRYREQLOCK(s, lk)	tryreqlock(SLVR_GETLOCK(s), lk)
+
+#define SLVR_WAKEUP(s)							\
+	psc_waitq_wakeall(&(slvr_2_bmap((s)))->bcm_waitq)
+
+#define SLVR_WAIT(s)							\
+	do {								\
+		DEBUG_SLVR(PLL_NOTIFY, (s), "SLVR_WAIT");		\
+		while (!((s)->slvr_flags & SLVR_DATARDY)) {		\
+			psc_waitq_wait(&(slvr_2_bmap((s)))->bcm_waitq,	\
+				       &(slvr_2_biod((s)))->biod_lock);	\
+			SLVR_LOCK((s));					\
+		}							\
+	} while (0)
+
+#define slvr_2_biod(s)		((struct bmap_iod_info *)(s)->slvr_pri)
+#define slvr_2_bmap(s)		slvr_2_biod(s)->biod_bmap
+#define slvr_2_fcmh(s)		slvr_2_biod(s)->biod_bmap->bcm_fcmh
+#define slvr_2_fd(s)		slvr_2_fcmh(s)->fcmh_fcoo->fcoo_fd
+#define slvr_2_biodi_wire(s)	slvr_2_biod(s)->biod_bmap_wire
+
+#define slvr_2_buf(s, blk)						\
+	((void *)(((s)->slvr_slab->slb_base) + ((blk) * SLASH_SLVR_BLKSZ)))
+
+#define slvr_2_fileoff(s, blk)						\
+	((off_t)((slvr_2_bmap(s)->bcm_blkno * SLASH_BMAP_SIZE) +	\
+		((s)->slvr_num * SLASH_SLVR_SIZE) +			\
+		((blk) * SLASH_SLVR_BLKSZ)))
+
+#define slvr_2_crcbits(s)						\
+	slvr_2_biodi_wire((s))->bh_crcstates[(s)->slvr_num]
+
+#define slvr_2_crc(s)							\
+	slvr_2_biodi_wire((s))->bh_crcs[(s)->slvr_num].gc_crc
+
+#define slvr_io_done(s, rw)						\
+	((rw) == SL_WRITE ? slvr_wio_done(s) : slvr_rio_done(s))
 
 #define SLVR_FLAGS_FMT "%s%s%s%s%s%s%s%s%s%s%s"
+#define DEBUG_SLVR_FLAGS(s)						\
+	(s)->slvr_flags & SLVR_NEW		? "n" : "-",		\
+	(s)->slvr_flags & SLVR_FAULTING		? "f" : "-",		\
+	(s)->slvr_flags & SLVR_GETSLAB		? "G" : "-",		\
+	(s)->slvr_flags & SLVR_PINNED		? "p" : "-",		\
+	(s)->slvr_flags & SLVR_CRCDIRTY		? "D" : "-",		\
+	(s)->slvr_flags & SLVR_DATARDY		? "d" : "-",		\
+	(s)->slvr_flags & SLVR_LRU		? "l" : "-",		\
+	(s)->slvr_flags & SLVR_FREEING		? "F" : "-",		\
+	(s)->slvr_flags & SLVR_SLBFREEING	? "b" : "-",		\
+	(s)->slvr_flags & SLVR_REPLSRC		? "S" : "-",		\
+	(s)->slvr_flags & SLVR_REPLDST		? "T" : "-"		\
 
 #define DEBUG_SLVR(level, s, fmt, ...)					\
 	psc_logs((level), PSS_GEN,					\
@@ -81,32 +124,100 @@ enum slvr_states {
 		 (s)->slvr_pri, (s)->slvr_slab, DEBUG_SLVR_FLAGS(s),	\
 		 ## __VA_ARGS__)
 
-static inline int
-slvr_cmp(const void *x, const void *y)
-{
-        const struct slvr_ref *a = x, *b = y;
-
-        if (a->slvr_num > b->slvr_num)
-                return (1);
-        if (a->slvr_num < b->slvr_num)
-                return (-1);
-        return (0);
-}
-
-struct slvr_ref * slvr_lookup(uint16_t, struct bmap_iod_info *, int);
+struct slvr_ref *
+	slvr_lookup(uint16_t, struct bmap_iod_info *, int);
 void	slvr_cache_init(void);
+void    slvr_clear_inuse(struct slvr_ref *, int, uint32_t);
 int	slvr_do_crc(struct slvr_ref *);
 int	slvr_fsbytes_io(struct slvr_ref *, int);
 int	slvr_fsbytes_wio(struct slvr_ref *, uint32_t, uint32_t);
 int	slvr_io_prep(struct slvr_ref *, uint32_t, uint32_t, int);
-void	slvr_rio_done(struct slvr_ref *);
 void	slvr_repl_prep(struct slvr_ref *, int);
+void	slvr_rio_done(struct slvr_ref *);
+void    slvr_schedule_crc(struct slvr_ref *);
 void	slvr_slab_prep(struct slvr_ref *, int);
 void	slvr_wio_done(struct slvr_ref *);
-void    slvr_schedule_crc(struct slvr_ref *);
 void    slvr_worker_init(void);
 
-#define slvr_io_done(s, rw) \
-	((rw) == SL_WRITE ? slvr_wio_done(s) : slvr_rio_done(s))
+static __inline int
+slvr_cmp(const void *x, const void *y)
+{
+	const struct slvr_ref *a = x, *b = y;
+
+	return (CMP(a->slvr_num, b->slvr_num));
+}
+
+SPLAY_PROTOTYPE(biod_slvrtree, slvr_ref, slvr_tentry, slvr_cmp);
+
+static __inline void
+slvr_lru_pin_check(struct slvr_ref *s)
+{
+	SLVR_LOCK_ENSURE(s);
+	psc_assert(s->slvr_slab && psclist_conjoint(&s->slvr_lentry));
+	psc_assert(s->slvr_flags == (SLVR_LRU|SLVR_PINNED));
+}
+
+static __inline int
+slvr_lru_tryunpin_locked(struct slvr_ref *s)
+{
+	SLVR_LOCK_ENSURE(s);
+	psc_assert(s->slvr_slab);
+	if (s->slvr_pndgwrts || s->slvr_pndgreads ||
+	    s->slvr_flags & SLVR_CRCDIRTY)
+		return (0);
+
+	psc_assert(s->slvr_flags & SLVR_LRU);
+	psc_assert(s->slvr_flags & SLVR_PINNED);
+	psc_assert(s->slvr_flags & SLVR_DATARDY);
+
+	psc_assert(!(s->slvr_flags &
+		     (SLVR_NEW|SLVR_FAULTING|
+		      SLVR_GETSLAB|SLVR_CRCDIRTY)));
+
+	s->slvr_flags &= ~SLVR_PINNED;
+	return (1);
+}
+
+static __inline int
+slvr_lru_slab_freeable(struct slvr_ref *s)
+{
+	int freeable = 1;
+	SLVR_LOCK_ENSURE(s);
+
+	psc_assert(s->slvr_flags & SLVR_LRU);
+
+	if (s->slvr_flags & SLVR_DATARDY)
+		psc_assert(!(s->slvr_flags &
+			     (SLVR_NEW|SLVR_FAULTING|SLVR_GETSLAB)));
+
+	if (!s->slvr_slab)
+		psc_assert(!(s->slvr_flags &
+			     (SLVR_NEW|SLVR_FAULTING|
+			      SLVR_GETSLAB|SLVR_DATARDY)));
+
+	if (s->slvr_flags & SLVR_PINNED)
+		freeable = 0;
+
+	DEBUG_SLVR(PLL_INFO, s, "freeable=%d", freeable);
+
+	return (freeable);
+}
+
+static __inline int
+slvr_lru_freeable(struct slvr_ref *s)
+{
+	int freeable=0;
+
+	if (s->slvr_slab ||
+	    s->slvr_flags & SLVR_PINNED   ||
+	    s->slvr_flags & SLVR_CRCDIRTY)
+		goto out;
+
+	psc_assert(slvr_lru_slab_freeable(s));
+
+	freeable = 1;
+ out:
+	return (freeable);
+}
 
 #endif
