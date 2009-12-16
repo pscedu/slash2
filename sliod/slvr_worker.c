2ea355a79c0c9d4674e658b350ada66eba827d19
Jared Yanovich <yanovich@psc.edu>
2009-12-16 04:25:34 +0000

    nbrequest_flush -> nbreqset_flush
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@9506 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- sliod/slvr_worker.c
+++ sliod/slvr_worker.c
@@ -105,7 +105,7 @@ slvr_worker_push_crcups(void)
 	
 	ENTRY;
 
-	nbrequest_reap(slvrNbReqSet);
+	nbreqset_reap(slvrNbReqSet);
 	/*
 	 * Check if an earlier CRC update RPC, if any, has finished.  If one
 	 * is still inflight, we won't be able to initiate a new one.
