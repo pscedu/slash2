e7d98b8ff7c6853292a710c539d3dd132ea03e37
Jared Yanovich <yanovich@psc.edu>
2009-12-17 20:34:23 +0000

    we didnt write this.  thats why i originally had the copyright moniker
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@9570 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- mount_slash/fuse_listener.c
+++ mount_slash/fuse_listener.c
@@ -1,23 +1,5 @@
 /* $Id$ */
 /*
- * %PSC_START_COPYRIGHT%
- * -----------------------------------------------------------------------------
- * Copyright (c) 2006-2009, Pittsburgh Supercomputing Center (PSC).
- *
- * Permission to use, copy, and modify this software and its documentation
- * without fee for personal use or non-commercial use within your organization
- * is hereby granted, provided that the above copyright notice is preserved in
- * all copies and that the copyright and this permission notice appear in
- * supporting documentation.  Permission to redistribute this software to other
- * organizations or individuals is not permitted without the written permission
- * of the Pittsburgh Supercomputing Center.  PSC makes no representations about
- * the suitability of this software for any purpose.  It is provided "as is"
- * without express or implied warranty.
- * -----------------------------------------------------------------------------
- * %PSC_END_COPYRIGHT%
- */
-
-/*
  * CDDL HEADER START
  *
  * The contents of this file are subject to the terms of the
