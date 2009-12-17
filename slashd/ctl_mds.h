2f98ca35b47b5c848ad3c08198717544832d7189
Jared Yanovich <yanovich@psc.edu>
2009-12-17 20:20:11 +0000

    make copyright seemed to work here too
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@9568 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- slashd/ctl_mds.h
+++ slashd/ctl_mds.h
@@ -1,4 +1,21 @@
 /* $Id$ */
+/*
+ * %PSC_START_COPYRIGHT%
+ * -----------------------------------------------------------------------------
+ * Copyright (c) 2006-2009, Pittsburgh Supercomputing Center (PSC).
+ *
+ * Permission to use, copy, and modify this software and its documentation
+ * without fee for personal use or non-commercial use within your organization
+ * is hereby granted, provided that the above copyright notice is preserved in
+ * all copies and that the copyright and this permission notice appear in
+ * supporting documentation.  Permission to redistribute this software to other
+ * organizations or individuals is not permitted without the written permission
+ * of the Pittsburgh Supercomputing Center.  PSC makes no representations about
+ * the suitability of this software for any purpose.  It is provided "as is"
+ * without express or implied warranty.
+ * -----------------------------------------------------------------------------
+ * %PSC_END_COPYRIGHT%
+ */
 
 /*
  * Control interface for querying and modifying
