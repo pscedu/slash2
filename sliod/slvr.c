283bbdf0c495b2b40ae5e272b174a1a059f6cfe9
Paul Nowoczynski <pauln@psc.edu>
2010-01-11 23:15:06 +0000

    revert to crc back from adler
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@9747 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- sliod/slvr.c
+++ sliod/slvr.c
@@ -106,8 +106,8 @@ slvr_do_crc(struct slvr_ref *s)
 
 	} else if (s->slvr_flags & SLVR_CRCDIRTY) {
 
-		//psc_crc64_calc(&s->slvr_crc, slvr_2_buf(s, 0), SL_CRC_SIZE);
-		s->slvr_crc = adler32(0, slvr_2_buf(s, 0), SL_CRC_SIZE);
+		psc_crc64_calc(&s->slvr_crc, slvr_2_buf(s, 0), SL_CRC_SIZE);
+		//s->slvr_crc = adler32(0, slvr_2_buf(s, 0), SL_CRC_SIZE);
 
 		DEBUG_SLVR(PLL_TRACE, s, "crc=%"PRIx64, s->slvr_crc);
 
