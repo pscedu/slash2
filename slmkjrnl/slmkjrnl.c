4039708d493025fb119f78747204fe757166aaf2
Jared Yanovich <yanovich@psc.edu>
2010-01-14 23:07:54 +0000

    make this guy more unixy
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@9849 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- slmkjrnl/slmkjrnl.c
+++ slmkjrnl/slmkjrnl.c
@@ -26,31 +26,44 @@
 #include "psc_util/journal.h"
 #include "psc_util/log.h"
 
-#include "sljournal.h"
 #include "pathnames.h"
+#include "slerr.h"
+#include "sljournal.h"
 
+int format;
+int query;
+int verbose;
 const char *progname;
 
 __dead void
 usage(void)
 {
-	fprintf(stderr, "usage: [-d] %s\n", progname);
+	fprintf(stderr, "usage: [-fqv] [-c file] %s\n", progname);
 	exit(1);
 }
 
 int
 main(int argc, char *argv[])
 {
-	char	c;
-	int	rc;
-	int	dumponly = 0;
+	const char *fn = _PATH_SLJOURNAL;
+	char c;
+	int rc;
 
-	progname = argv[0];
 	pfl_init();
-	while ((c = getopt(argc, argv, "d")) != -1)
+	progname = argv[0];
+	while ((c = getopt(argc, argv, "c:fqv")) != -1)
 		switch (c) {
-		case 'd':
-			dumponly = 1;
+		case 'c':
+			fn = optarg;
+			break;
+		case 'f':
+			format = 1;
+			break;
+		case 'q':
+			query = 1;
+			break;
+		case 'v':
+			verbose = 1;
 			break;
 		default:
 			usage();
@@ -60,20 +73,24 @@ main(int argc, char *argv[])
 	if (argc)
 		usage();
 
+	if (!format && !query)
+		usage();
+
 	if (mkdir(_PATH_SLASHD_DIR, 0700) == -1)
 		if (errno != EEXIST)
 			err(1, "mkdir: %s", _PATH_SLASHD_DIR);
 
-	if (!dumponly) {
-		rc = pjournal_format(_PATH_SLJOURNAL, SLJ_MDS_JNENTS, SLJ_MDS_ENTSIZE, SLJ_MDS_RA, 0);
-		if (rc == 0)
-			printf("SLASH log file %s has been created with %d %d-byte entries.\n",
-				_PATH_SLJOURNAL, SLJ_MDS_JNENTS, SLJ_MDS_ENTSIZE);
-		else {
-			printf("Fail to format SLASH log file %s.\n", _PATH_SLJOURNAL);
-			exit(0);
-		}
+	if (format) {
+		rc = pjournal_format(fn, SLJ_MDS_JNENTS,
+		    SLJ_MDS_ENTSIZE, SLJ_MDS_RA, 0);
+		if (rc)
+			psc_fatalx("failing formatting journal %s: %s",
+			    fn, slstrerror(rc));
+		if (verbose)
+			warnx("created log file %s with %d %d-byte entries",
+			    fn, SLJ_MDS_JNENTS, SLJ_MDS_ENTSIZE);
 	}
-	pjournal_dump(_PATH_SLJOURNAL);
+	if (query)
+		pjournal_dump(fn, verbose);
 	exit(0);
 }
