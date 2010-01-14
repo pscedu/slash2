d6c943040072eee2b85e86f8906b0da85be4696b
Jared Yanovich <yanovich@psc.edu>
2010-01-14 00:06:19 +0000

    - overhaul the make infrastructure:     (o) introduce MODULES which do the platform-specific bindings to get code 	compiled that uses external interfaces, such as fuse, zfs, libpthread, 	etc.     (o) remove the depend target.  depends are now discovered automatically 	when source files change.
    - use the in-tree fuse when linking apps with fuse instead of relying on a
      system installation.
    
    
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@9795 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- mk/local.mk
+++ mk/local.mk
@@ -1,13 +1,1 @@
 # $Id$
-
-INCLUDES+=	-I${KERNEL_BASE}/include
-
-ZFS_LIBS=	-L${ZFS_BASE}/zfs-fuse				\
-		-L${ZFS_BASE}/lib/libavl			\
-		-L${ZFS_BASE}/lib/libnvpair			\
-		-L${ZFS_BASE}/lib/libsolkerncompat		\
-		-L${ZFS_BASE}/lib/libumem			\
-		-L${ZFS_BASE}/lib/libzfscommon			\
-		-L${ZFS_BASE}/lib/libzpool			\
-		-lzfs-fuse -lzpool-kernel -lzfscommon-kernel	\
-		-lnvpair-kernel -lavl -lumem -lsolkerncompat
--- mk/main.mk
+++ mk/main.mk
@@ -5,7 +5,5 @@ include ${SLASH_BASE}/mk/local.mk
 INCLUDES+=	-I${PFL_BASE}/include
 INCLUDES+=	-I${SLASH_BASE}/include
 INCLUDES+=	-I${SLASH_BASE} -I.
-INCLUDES+=	-I${ZFS_BASE}
-INCLUDES+=	-I${LNET_BASE}/include
 
 include ${MAINMK}
