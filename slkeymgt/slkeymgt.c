3ea350b856d76ba45e8d54769e7485ab1a73bf41
Jared Yanovich <yanovich@psc.edu>
2010-02-19 00:04:18 +0000

    simple utility to ease the life of tsuite
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@10415 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

---
+++ slkeymgt/slkeymgt.c
@@ -0,0 +1,67 @@
+/* $Id$ */
+/*
+ * %PSC_START_COPYRIGHT%
+ * -----------------------------------------------------------------------------
+ * Copyright (c) 2006-2010, Pittsburgh Supercomputing Center (PSC).
+ *
+ * Permission to use, copy, and modify this software and its documentation
+ * without fee for personal use or non-commercial use within your organization
+ * is hereby granted, provided that the above copyright notice is preserved in
+ * all copies and that the copyright and this permission notice appear in
+ * supporting documentation.  Permission to redistribute this software to other
+ * organizations or individuals is not permitted without the written permission
+ * of the Pittsburgh Supercomputing Center.  PSC makes no representations about
+ * the suitability of this software for any purpose.  It is provided "as is"
+ * without express or implied warranty.
+ * -----------------------------------------------------------------------------
+ * %PSC_END_COPYRIGHT%
+ */
+
+#include <err.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "pfl/pfl.h"
+#include "psc_util/log.h"
+
+#include "fdbuf.h"
+#include "pathnames.h"
+
+int		 create;
+const char	*sl_datadir = SL_PATH_DATADIR;
+const char	*progname;
+
+__dead void
+usage(void)
+{
+	fprintf(stderr, "usage: %s [-c] [-D dir]\n", progname);
+	exit(1);
+}
+
+int
+main(int argc, char *argv[])
+{
+	int c;
+
+	pfl_init();
+	progname = argv[0];
+	while ((c = getopt(argc, argv, "cD:")) != -1)
+		switch (c) {
+		case 'c':
+			create = 1;
+			break;
+		case 'D':
+			sl_datadir = optarg;
+			break;
+		default:
+			usage();
+		}
+	argc -= optind;
+	if (argc)
+		usage();
+	if (create)
+		fdbuf_createkeyfile();
+	else
+		fdbuf_checkkeyfile();
+	exit(0);
+}
