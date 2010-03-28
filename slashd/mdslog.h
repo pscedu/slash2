69aa8e7455dadb7dc6ecff20d9c076e443c1abb6
Paul Nowoczynski <pauln@psc.edu>
2010-03-28 04:13:51 +0000

    add a new mds logging type for bmap sequence number
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@10954 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- slashd/mdslog.h
+++ slashd/mdslog.h
@@ -25,7 +25,8 @@
 enum {
 	MDS_LOG_BMAP_REPL     = (1 << (1 + PJE_LASTBIT)),
 	MDS_LOG_BMAP_CRC      = (1 << (2 + PJE_LASTBIT)),
-	MDS_LOG_INO_ADDREPL   = (1 << (3 + PJE_LASTBIT))
+	MDS_LOG_BMAP_SEQ      = (1 << (3 + PJE_LASTBIT)),
+	MDS_LOG_INO_ADDREPL   = (1 << (4 + PJE_LASTBIT))
 };
 
 struct bmapc_memb;
