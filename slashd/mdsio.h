28a06785b7a57f7f4cb79d24b7de038b89adecb6
Zhihui Zhang <zhihui@psc.edu>
2010-04-27 20:15:34 +0000

    compiler fix
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@11610 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- slashd/mdsio.h
+++ slashd/mdsio.h
@@ -71,8 +71,10 @@ int mdsio_statfs(struct statvfs *);
 int mdsio_link(mdsio_fid_t, mdsio_fid_t, const char *, struct slash_fidgen *, const struct slash_creds *, struct srt_stat *);
 int mdsio_lookup(mdsio_fid_t, const char *, struct slash_fidgen *, mdsio_fid_t *, const struct slash_creds *, struct srt_stat *);
 int mdsio_lookup_slfid(slfid_t, const struct slash_creds *, struct srt_stat *, mdsio_fid_t *);
-int mdsio_mkdir(mdsio_fid_t, const char *, mode_t, const struct slash_creds *, struct srt_stat *, struct slash_fidgen *, mdsio_fid_t *, sl_getslfid_cb);
-int mdsio_opencreate(mdsio_fid_t, const struct slash_creds *, int, mode_t, const char *, struct slash_fidgen *, mdsio_fid_t *, struct srt_stat *, void *, sl_jlog_cb, sl_getslfid_cb);
+int mdsio_mkdir(mdsio_fid_t, const char *, mode_t, const struct slash_creds *, struct srt_stat *, \
+	struct slash_fidgen *, mdsio_fid_t *, sl_jlog_cb, sl_getslfid_cb);
+int mdsio_opencreate(mdsio_fid_t, const struct slash_creds *, int, mode_t, const char *, \
+	struct slash_fidgen *, mdsio_fid_t *, struct srt_stat *, void *, sl_jlog_cb, sl_getslfid_cb);
 int mdsio_opendir(mdsio_fid_t, const struct slash_creds *, struct slash_fidgen *, struct srt_stat *, void *);
 int mdsio_readdir(const struct slash_creds *, size_t, off_t, void *, size_t *, void *, int, void *);
 int mdsio_rename(mdsio_fid_t, const char *, mdsio_fid_t, const char *, const struct slash_creds *);
