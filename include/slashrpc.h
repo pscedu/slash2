4b0e924cd73f19f49e38af531dc6197cb3d90ae4
Paul Nowoczynski <pauln@psc.edu>
2010-05-13 18:32:40 +0000

    remove struct items relating to the release of bmap refs on the mds
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@11714 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- include/slashrpc.h
+++ include/slashrpc.h
@@ -344,11 +344,8 @@ struct srm_bmap_crcwire {
 struct srm_bmap_crcup {
 	struct slash_fidgen	fg;
 	uint64_t		fsize;		/* largest known size */
-	uint64_t                seq;            /* for bmap odtable release */
-	uint64_t                key;            /* for bmap odtable release */
 	uint32_t		blkno;		/* bmap block number */
-	uint32_t		nups:31;	/* number of CRC updates */
-	uint32_t		rls:1;		/* try to release the bmap odtable entry */
+	uint32_t		nups;	        /* number of CRC updates */
 	struct srm_bmap_crcwire	crcs[0];	/* see above, MAX_BMAP_INODE_PAIRS max */
 } __packed;
 
