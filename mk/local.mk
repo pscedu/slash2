819ede94859ca73c6dd5466b7a60bd8975094fcb
Jared Yanovich <yanovich@psc.edu>
2010-06-04 17:53:44 +0000

    remove redundant local.mk
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@11962 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- mk/local.mk
+++
@@ -1,7 +0,0 @@
-# $Id$
-
-SLASH_MODULES+=		cli
-SLASH_MODULES+=		ion
-SLASH_MODULES+=		mds
-
--include ${SLASH_BASE}/mk/local.mk.local
--- mk/main.mk
+++ mk/main.mk
@@ -1,15 +1,19 @@
 # $Id$
 
-include ${SLASH_BASE}/mk/local.mk
+INCLUDES+=		-I${SLASH_BASE}/include
+INCLUDES+=		-I${SLASH_BASE}
 
-INCLUDES+=	-I${SLASH_BASE}/include
-INCLUDES+=	-I${SLASH_BASE}
+DEFINES+=		-DAPP_STRERROR=slstrerror
 
-DEFINES+=	-DAPP_STRERROR=slstrerror
-
-SRC_PATH+=	${ZFS_BASE}
+SRC_PATH+=		${ZFS_BASE}
 ifneq ($(realpath ${SLASH_BASE}),${CURDIR})
-SRC_PATH+=	${SLASH_BASE}
+SRC_PATH+=		${SLASH_BASE}
 endif
+
+SLASH_MODULES+=		cli
+SLASH_MODULES+=		ion
+SLASH_MODULES+=		mds
+
+-include ${SLASH_BASE}/mk/local.mk
 
 include ${MAINMK}
