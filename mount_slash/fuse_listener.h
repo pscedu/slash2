6e7912eea4d94030b3097879312175978105b77c
Zhihui Zhang <zhihui@psc.edu>
2010-06-04 18:28:50 +0000

    remove use_ino
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@11964 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- mount_slash/fuse_listener.h
+++ mount_slash/fuse_listener.h
@@ -23,7 +23,7 @@
 #include <fuse_lowlevel.h>
 
 /* The following options are explained in FUSE README file */
-#define FUSE_OPTIONS "use_ino,allow_other,max_write=134217728,big_writes"
+#define FUSE_OPTIONS "allow_other,max_write=134217728,big_writes"
 
 void	slash2fuse_listener_exit(void);
 int	slash2fuse_listener_init(void);
