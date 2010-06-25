c72fc80664ea66740f52af162448f11904b72eb1
Paul Nowoczynski <pauln@psc.edu>
2010-06-25 20:27:38 +0000

    make some changes to all journals to be placed onto raw devices
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@12145 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- slmkjrnl/slmkjrnl.c
+++ slmkjrnl/slmkjrnl.c
@@ -40,7 +40,7 @@ const char *progname;
 __dead void
 usage(void)
 {
-	fprintf(stderr, "usage: %s [-fqsv] [-D dir]\n", progname);
+	fprintf(stderr, "usage: %s [-fqsv] [-D dir] [-b dev] [-n entries]\n", progname);
 	exit(1);
 }
 
@@ -48,16 +48,21 @@ int
 main(int argc, char *argv[])
 {
 	char c, fn[PATH_MAX];
-	int rc;
+	int rc, fname=0;
 	unsigned int options;
+	ssize_t nents=SLJ_MDS_JNENTS;
 
 	pfl_init();
 	options = PJH_OPT_NONE;
 	progname = argv[0];
-	while ((c = getopt(argc, argv, "D:fqv")) != -1)
+	while ((c = getopt(argc, argv, "D:n:b:fqv")) != -1)
 		switch (c) {
 		case 'D':
 			datadir = optarg;
+			break;
+		case 'b':
+			fname = 1;
+			strncpy(fn, optarg, PATH_MAX);
 			break;
 		case 'f':
 			format = 1;
@@ -67,6 +72,9 @@ main(int argc, char *argv[])
 			break;
 		case 'v':
 			verbose = 1;
+			break;
+		case 'n':
+			nents = strtol(optarg, NULL, 10);
 			break;
 		default:
 			usage();
@@ -79,15 +87,16 @@ main(int argc, char *argv[])
 	if (!format && !query)
 		usage();
 
-	if (mkdir(datadir, 0700) == -1)
-		if (errno != EEXIST)
-			err(1, "mkdir: %s", datadir);
-
-	xmkfn(fn, "%s/%s", datadir, SL_FN_OPJOURNAL);
+	if (!fname) {
+		if (mkdir(datadir, 0700) == -1)
+			if (errno != EEXIST)
+				err(1, "mkdir: %s", datadir);
+		
+		xmkfn(fn, "%s/%s", datadir, SL_FN_OPJOURNAL);
+	}
 
 	if (format) {
-		rc = pjournal_format(fn, SLJ_MDS_JNENTS,
-		    SLJ_MDS_ENTSIZE, SLJ_MDS_RA);
+		rc = pjournal_format(fn, nents, SLJ_MDS_ENTSIZE, SLJ_MDS_RA);
 		if (rc)
 			psc_fatalx("failing formatting journal %s: %s",
 			    fn, slstrerror(rc));
