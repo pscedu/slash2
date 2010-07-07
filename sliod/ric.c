02e24376e8a5fd60d0df66c6ee5603b3e080ba57
Paul Nowoczynski <pauln@psc.edu>
2010-07-07 14:45:42 +0000

    don't decrement opcnt if the bmap is null
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@12268 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- sliod/ric.c
+++ sliod/ric.c
@@ -236,13 +236,14 @@ sli_ric_handle_io(struct pscrpc_request *rq, enum rw rw)
 		psc_assert(!tsize);
 
  out:
-	bmap_op_done_type(bmap, BMAP_OPCNT_LOOKUP);
+	if (bmap)
+		bmap_op_done_type(bmap, BMAP_OPCNT_LOOKUP);
 
 	/* XXX In situations where errors occur (such as an ENOSPC from
 	 *   iod_inode_open()) then we must have a way to notify other
 	 *   threads blocked on DATARDY.
 	 */
-	fcmh_op_done_type(fcmh, FCMH_OPCNT_LOOKUP_FIDC); /* reaper will return it to the pool */
+	fcmh_op_done_type(fcmh, FCMH_OPCNT_LOOKUP_FIDC);
 
 	return (rc);
 }
