e8be107dd5dc5ef2edb67ca93a9ef997a9c1fbd6
Zhihui Zhang <zhihui@psc.edu>
2010-07-08 16:01:26 +0000

    fix a bug
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@12285 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- slashd/mdslog.c
+++ slashd/mdslog.c
@@ -179,6 +179,9 @@ mds_redo_namespace(__unusedx struct psc_journal_enthdr *pje)
 	struct srt_stat stat;
 	struct slmds_jent_namespace *jnamespace;
 
+	jnamespace = (struct slmds_jent_namespace *)pje->pje_data;
+	psc_assert(jnamespace->sjnm_magic == SJ_NAMESPACE_MAGIC);
+
 	stat.sst_uid = jnamespace->sjnm_uid;
 	stat.sst_gid = jnamespace->sjnm_gid;
 	stat.sst_mode = jnamespace->sjnm_mode;
@@ -187,8 +190,6 @@ mds_redo_namespace(__unusedx struct psc_journal_enthdr *pje)
 	stat.sst_mtime = jnamespace->sjnm_mtime;
 	stat.sst_ctime = jnamespace->sjnm_ctime;
 
-	jnamespace = (struct slmds_jent_namespace *)pje->pje_data;
-	psc_assert(jnamespace->sjnm_magic == SJ_NAMESPACE_MAGIC);
 	switch (jnamespace->sjnm_op) {
 	    case NS_OP_CREATE:
 		rc = mdsio_replay_create(
