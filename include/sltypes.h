79056ce336296416c9ee7dda621be6e832811ff8
Zhihui Zhang <zhihui@psc.edu>
2010-07-19 14:16:36 +0000

    tweak
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@12412 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- include/sltypes.h
+++ include/sltypes.h
@@ -138,6 +138,6 @@ struct srt_dirent {
 
 #define	MAX_NAME_BUF_SIZE	377
 
-#define SLASH2_CURSOR_FLAG	0x12345678
+#define SLASH2_CURSOR_FLAG	0x12345678	/* overload the ioflag of zfs_write() */
 
 #endif /* _SL_TYPES_H_ */
