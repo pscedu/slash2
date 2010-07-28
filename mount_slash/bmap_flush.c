34923b56988935520285716f3feb5d00e6309d45
Jared Yanovich <yanovich@psc.edu>
2010-07-28 16:31:51 +0000

    space
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@12623 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- mount_slash/bmap_flush.c
+++ mount_slash/bmap_flush.c
@@ -837,7 +837,7 @@ ms_bmap_release(struct sl_resm *resm)
 		    rmci->rmci_bmaprls.bmaps[i].bmapno,
 		    rmci->rmci_bmaprls.bmaps[i].key,
 		    rmci->rmci_bmaprls.bmaps[i].seq,
-		    mp ? mp->bidrc[i]: rc);		/* mp could be NULL if !rc */
+		    mp ? mp->bidrc[i] : rc);		/* mp could be NULL if !rc */
 	rmci->rmci_bmaprls.nbmaps = 0;
  out:
 
