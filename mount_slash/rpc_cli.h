c1340042843aa3d57e5c45a375db23b5a93d022f
Jared Yanovich <yanovich@psc.edu>
2010-07-28 21:18:27 +0000

    bump RCM channel to support the replst_master piggy
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@12637 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- mount_slash/rpc_cli.h
+++ mount_slash/rpc_cli.h
@@ -28,8 +28,8 @@ struct pscrpc_request;
 /* Slash RPC channel for client from MDS. */
 #define SRCM_NTHREADS	8
 #define SRCM_NBUFS	64
-#define SRCM_BUFSZ	384
-#define SRCM_REPSZ	384
+#define SRCM_BUFSZ	512
+#define SRCM_REPSZ	512
 #define SRCM_SVCNAME	"msrcmthr"
 
 #define slc_geticsvc(resm)							\
