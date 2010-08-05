4b0904a5fa5aa19d6d6bb299f41564ac3fac9c22
Jared Yanovich <yanovich@psc.edu>
2010-08-05 16:27:21 +0000

    move non-slash related debug stuff into PFL
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@12830 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- include/slutil.h
+++ include/slutil.h
@@ -20,38 +20,16 @@
 #ifndef _SLUTIL_H_
 #define _SLUTIL_H_
 
-#include <sys/types.h>
-
-#include <inttypes.h>
-
 struct stat;
 struct statvfs;
 
 struct slash_creds;
+struct slash_fidgen;
 struct srt_stat;
 
-#define DEBUG_STATBUF(level, stb, fmt, ...)					\
-	psc_logs((level), PSS_GEN,						\
-	    "stb (%p) dev:%"PRIu64" inode:%"PRId64" mode:0%o "			\
-	    "nlink:%"PRIu64" uid:%u gid:%u "					\
-	    "rdev:%"PRIu64" sz:%"PRId64" blksz:%"PSCPRI_BLKSIZE_T" "		\
-	    "blkcnt:%"PRId64" atime:%lu mtime:%lu ctime:%lu " fmt,		\
-	    (stb), (uint64_t)(stb)->st_dev, (stb)->st_ino, (stb)->st_mode,	\
-	    (uint64_t)(stb)->st_nlink, (stb)->st_uid, (stb)->st_gid,		\
-	    (uint64_t)(stb)->st_rdev, (stb)->st_size, (stb)->st_blksize,	\
-	    (stb)->st_blocks, (stb)->st_atime, (stb)->st_mtime,			\
-	    (stb)->st_ctime, ## __VA_ARGS__)
-
-void	dump_fflags(int);
-void	dump_statbuf(int, const struct stat *);
-
-void	print_flag(const char *, int *);
-
 void	sl_externalize_stat(const struct stat *, struct srt_stat *);
-void	sl_internalize_stat(const struct srt_stat *, struct stat *);
+void	sl_internalize_stat(const struct slash_fidgen *, const struct srt_stat *, struct stat *);
 void	sl_externalize_statfs(const struct statvfs *, struct srt_statfs *);
 void	sl_internalize_statfs(const struct srt_statfs *, struct statvfs *);
-
-int	checkcreds(const struct srt_stat *, const struct slash_creds *, int);
 
 #endif /* _SLUTIL_H_ */
