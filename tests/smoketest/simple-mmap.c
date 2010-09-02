e275416aa239f67ebc3c07886bbd9af461c3444a
Jared Yanovich <yanovich@psc.edu>
2010-09-02 21:46:45 +0000

    do the standard tidy up of code.  various explanations:
    - many systems are strict that sys/types.h (and other non-POSIX type
      defining headers) be included before other API headers.  i.e. you may
      make no guarentees that sys/stat.h will pull in sys/types.h itself.
    
      so it is always best to pull in sys/* headers first.
    
      POSIX mandates that everything outside of sys/* may be included in any
      order, so we maintain this sorted so it is easy to avoid duplicates.
    
    - use err(3).  it makes code way easier, prints to stderr, adds the
      error message, and exits all for you.  its available mostly everywhere
      and is very easy to add to compat for the few systems that don't have
      it.
    
    - use exit(3) instead of return() from main, since exit(3) has ties into
      libc which do things like run the atexit(3) callbacks, stdio flushing,
      etc.
    
    - mode_t is a short on some systems (darwin), so casting is pointless
      since it gets casted back to an int anyway.
    
    
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@13413 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- tests/smoketest/simple-mmap.c
+++ tests/smoketest/simple-mmap.c
@@ -1,48 +1,44 @@
-#include <stdio.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <unistd.h>
+/* $Id$ */
+/* %PSC_COPYRIGHT% */
+
+#include <sys/types.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
-#include <sys/types.h>
+
+#include <err.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #define	FILE_SIZE	4978
 
-int main(int argc, char *argv[])
+int
+main(int argc, char *argv[])
 {
-    char ch;
-    char *map;
-    int i, fd, ret;
-    char *filename = "mmap-test.dat";
+	char ch;
+	char *map;
+	int i, fd, ret;
+	char *filename = "mmap-test.dat";
 
-    fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, (mode_t)0600);
-    if (fd == -1) {
-	printf("Error opening file for writing.\n");
-	exit(1);
-    }
-    ret  = lseek(fd, FILE_SIZE - 1, SEEK_SET);
-    if (ret == -1) {
-	printf("Error lseeking to the end of the file.\n");
-	exit(1);
-    }
-    ch = 0x30;
-    ret = write(fd, &ch, 1);
-    if (ret != 1) {
-	printf("Error writing last byte of the file.\n");
-	exit(1);
-    }
-    map = mmap(0, FILE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    if (map == MAP_FAILED) {
-	printf("Error mmapping the file.\n");
-	exit(1);
-    }
-    for (i = 0; i < FILE_SIZE; i++) {
-	map[i] = (i % 10) + 0x30; 
-    }
-    if (munmap(map, FILE_SIZE) == -1) {
-	printf("Error un-mmapping the file.\n");
-        exit(1);
-    }
-    close(fd);
-    return 0;
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		err(1, "Error opening file %s for writing.", filename);
+	ret  = lseek(fd, FILE_SIZE - 1, SEEK_SET);
+	if (ret == -1)
+		err(1, "Error lseeking to the end of the file.");
+	ch = 0x30;
+	ret = write(fd, &ch, 1);
+	if (ret != 1)
+		err(1, "Error writing last byte of the file.");
+	map = mmap(0, FILE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	if (map == MAP_FAILED) {
+		err(1, "Error mmapping the file.");
+	for (i = 0; i < FILE_SIZE; i++) {
+		map[i] = (i % 10) + 0x30;
+	}
+	if (munmap(map, FILE_SIZE) == -1)
+		err(1, "Error un-mmapping the file.");
+	close(fd);
+	exit(0);
 }
