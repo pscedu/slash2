7d1c36a35d3ccbe5f480c9444f3fe20d483bb721
Jared Yanovich <yanovich@psc.edu>
2010-09-10 17:29:18 +0000

    regen copyright
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@13589 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- utils/tsuite/tsuite.pl
+++ utils/tsuite/tsuite.pl
@@ -1,6 +1,20 @@
 #!/usr/bin/perl -W
 # $Id$
-# %PSC_COPYRIGHT%
+# %PSC_START_COPYRIGHT%
+# -----------------------------------------------------------------------------
+# Copyright (c) 2009-2010, Pittsburgh Supercomputing Center (PSC).
+#
+# Permission to use, copy, and modify this software and its documentation
+# without fee for personal use or non-commercial use within your organization
+# is hereby granted, provided that the above copyright notice is preserved in
+# all copies and that the copyright and this permission notice appear in
+# supporting documentation.  Permission to redistribute this software to other
+# organizations or individuals is not permitted without the written permission
+# of the Pittsburgh Supercomputing Center.  PSC makes no representations about
+# the suitability of this software for any purpose.  It is provided "as is"
+# without express or implied warranty.
+# -----------------------------------------------------------------------------
+# %PSC_END_COPYRIGHT%
 
 use Getopt::Std;
 use POSIX qw(:sys_wait_h :errno_h);
