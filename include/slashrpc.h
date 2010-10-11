8f6a4dd133046add840ad5b3cb7656aabf505a4e
Zhihui Zhang <zhihui@psc.edu>
2010-10-11 17:50:34 +0000

    comment
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@14148 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- include/slashrpc.h
+++ include/slashrpc.h
@@ -403,7 +403,7 @@ struct srm_bmap_crcwire {
 
 struct srm_bmap_crcup {
 	struct slash_fidgen	fg;
-	uint64_t		fsize;		/* largest known size - mdsio_apply_fcmh_size() */
+	uint64_t		fsize;		/* largest known size applied in mds_bmap_crc_update() */
 	uint32_t		blkno;		/* bmap block number */
 	uint32_t		nups;		/* number of CRC updates */
 	uint32_t                utimgen;
