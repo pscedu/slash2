e4feb267b8c9cbb1be34fa56935ef880a6709d71
Jared Yanovich <yanovich@psc.edu>
2011-01-24 23:26:39 +0000

    regen
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@15154 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- utils/typedump/typedump.c
+++ utils/typedump/typedump.c
@@ -294,6 +294,7 @@ main(int argc, char *argv[])
 	PRTYPE(struct srt_authbuf_footer);
 	PRTYPE(struct srt_authbuf_secret);
 	PRTYPE(struct srt_bmapdesc);
+	PRTYPE(struct srt_bmapminseq);
 	PRTYPE(struct srt_reclaim_entry);
 	PRTYPE(struct srt_stat);
 	PRTYPE(struct srt_statfs);
