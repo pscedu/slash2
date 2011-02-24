44883d2ea223892f21ace14771dd7dcce8db06f6
Jared Yanovich <yanovich@psc.edu>
2011-02-24 00:57:48 +0000

    align
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@15634 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- include/cache_params.h
+++ include/cache_params.h
@@ -22,34 +22,34 @@
 
 /* Begin hand computed */
 
-#define FIDC_MDS_DEFSZ			(32 * 1024)
-#define FIDC_CLI_DEFSZ			( 2 * 1024)
-#define FIDC_ION_DEFSZ			(32 * 1024)
+#define FIDC_MDS_DEFSZ		(32 * 1024)
+#define FIDC_CLI_DEFSZ		( 2 * 1024)
+#define FIDC_ION_DEFSZ		(32 * 1024)
 
-#define SLASH_SLVRS_PER_BMAP		128
-#define SLASH_SLVR_SIZE			(1024 * (off_t)1024)
-#define SLASH_SLVR_BLKSZ		(32 * 1024)
+#define SLASH_SLVRS_PER_BMAP	128
+#define SLASH_SLVR_SIZE		(1024 * (off_t)1024)
+#define SLASH_SLVR_BLKSZ	(32 * 1024)
 
-#define SLB_NBLK			32
-#define SLB_NDEF			64
-#define SLB_MIN				64
-#define SLB_MAX				128
+#define SLB_NBLK		32
+#define SLB_NDEF		64
+#define SLB_MIN			64
+#define SLB_MAX			128
 
 /* XXX make it 4 so that status bits of a replica do not straddle a byte boundary */
-#define SL_BITS_PER_REPLICA		3
+#define SL_BITS_PER_REPLICA	3
 
 /* End hand computed */
 
-#define SL_REPLICA_MASK			((uint8_t)((1 << SL_BITS_PER_REPLICA) - 1))
-#define SL_REPLICA_NBYTES		((SL_MAX_REPLICAS * SL_BITS_PER_REPLICA) / NBBY)	/* 64-bit align */
+#define SL_REPLICA_MASK		((uint8_t)((1 << SL_BITS_PER_REPLICA) - 1))
+#define SL_REPLICA_NBYTES	((SL_MAX_REPLICAS * SL_BITS_PER_REPLICA) / NBBY)	/* 64-bit align */
 
-#define SLASH_BMAP_SIZE			(SLASH_SLVRS_PER_BMAP * SLASH_SLVR_SIZE)		/* 128 MiB */
+#define SLASH_BMAP_SIZE		(SLASH_SLVRS_PER_BMAP * SLASH_SLVR_SIZE)		/* 128 MiB */
 
-#define SLASH_SLVR_BLKMASK		(SLASH_SLVR_BLKSZ - 1)
-#define SLASH_BLKS_PER_SLVR		(SLASH_SLVR_SIZE / SLASH_SLVR_BLKSZ)
+#define SLASH_SLVR_BLKMASK	(SLASH_SLVR_BLKSZ - 1)
+#define SLASH_BLKS_PER_SLVR	(SLASH_SLVR_SIZE / SLASH_SLVR_BLKSZ)
 
 /* aliases */
-#define SLASH_CRCS_PER_BMAP		SLASH_SLVRS_PER_BMAP
-#define SLASH_BMAP_CRCSIZE			SLASH_SLVR_SIZE
+#define SLASH_CRCS_PER_BMAP	SLASH_SLVRS_PER_BMAP
+#define SLASH_BMAP_CRCSIZE	SLASH_SLVR_SIZE
 
 #endif /* _CACHEPARAMS_H_ */
