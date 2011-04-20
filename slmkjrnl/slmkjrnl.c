1a3b1c834f418afd6465db96906b775ab357c820
Zhihui Zhang <zhihui@psc.edu>
2011-04-20 19:13:25 +0000

    beef up journal dump
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@16224 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- slmkjrnl/slmkjrnl.c
+++ slmkjrnl/slmkjrnl.c
@@ -34,6 +34,8 @@
 #include "sljournal.h"
 
 #include "slashd/mdsio.h"
+#include "slashd/mdslog.h"
+#include "slashd/namespace.h"
 #include "slashd/subsys_mds.h"
 
 int		 format;
@@ -53,6 +55,370 @@ usage(void)
 	    "usage: %s [-fqv] [-b block-device] [-D dir] [-n nentries]\n",
 	    progname);
 	exit(1);
+}
+
+__static void *
+pjournal_alloc_buf(struct psc_journal *pj)
+{
+        return (psc_alloc(PJ_PJESZ(pj) * pj->pj_hdr->pjh_readahead,
+            PAF_PAGEALIGN | PAF_LOCK));
+}
+
+/**
+ * pjournal_open - Initialize the in-memory representation of a journal.
+ * @fn: path to journal on file system.
+ */
+struct psc_journal *
+pjournal_open(const char *fn)
+{
+	struct psc_journal_hdr *pjh;
+	struct psc_journal *pj;
+	struct stat statbuf;
+	const char *basefn;
+	uint64_t chksum;
+	ssize_t pjhlen;
+	ssize_t nb;
+
+	pj = PSCALLOC(sizeof(*pj));
+
+	strlcpy(pj->pj_name, pfl_basename(fn), sizeof(pj->pj_name));
+
+	pj->pj_fd = open(fn, O_RDWR | O_DIRECT);
+	if (pj->pj_fd == -1)
+		psc_fatal("failed to open journal %s", fn);
+	if (fstat(pj->pj_fd, &statbuf) == -1)
+		psc_fatal("failed to stat journal %s", fn);
+	basefn = strrchr(fn, '/');
+	if (basefn)
+		basefn++;
+	else
+		basefn = fn;
+
+	/*
+	 * O_DIRECT may impose alignment restrictions so align the buffer
+	 * and perform I/O in multiples of file system block size.
+	 */
+	pjhlen = PSC_ALIGN(sizeof(*pjh), statbuf.st_blksize);
+	pjh = psc_alloc(pjhlen, PAF_PAGEALIGN | PAF_LOCK);
+	nb = pread(pj->pj_fd, pjh, pjhlen, 0);
+	if (nb != pjhlen)
+		psc_fatal("failed to read journal header");
+
+	pj->pj_hdr = pjh;
+	if (pjh->pjh_magic != PJH_MAGIC)
+		psc_fatal("journal header has a bad magic number "
+		    "%#"PRIx64, pjh->pjh_magic);
+
+	if (pjh->pjh_version != PJH_VERSION)
+		psc_fatal("journal header has an invalid version "
+		    "number %d", pjh->pjh_version);
+
+	PSC_CRC64_INIT(&chksum);
+	psc_crc64_add(&chksum, pjh, offsetof(struct psc_journal_hdr, pjh_chksum));
+	PSC_CRC64_FIN(&chksum);
+
+	if (pjh->pjh_chksum != chksum) 
+		psc_fatal("journal header has an invalid checksum "
+		    "value %"PSCPRIxCRC64" vs %"PSCPRIxCRC64,
+		    pjh->pjh_chksum, chksum);
+
+	if (S_ISREG(statbuf.st_mode)) {
+		if (statbuf.st_size != (off_t)(pjhlen + pjh->pjh_nents * PJ_PJESZ(pj)))
+			psc_fatal("size of the journal log %"PSCPRIdOFFT"d does not match "
+				  "specs in its header", statbuf.st_size);
+	}
+
+	return (pj);
+}
+
+/**
+ * pjournal_release - Release resources associated with an in-memory
+ *	journal.
+ * @pj: journal to release.
+ */
+__static void
+pjournal_release(struct psc_journal *pj)
+{
+	struct psc_journal_enthdr *pje;
+	int n;
+
+	DYNARRAY_FOREACH(pje, n, &pj->pj_bufs)
+		psc_free(pje, PAF_LOCK | PAF_PAGEALIGN, PJ_PJESZ(pj));
+	psc_dynarray_free(&pj->pj_bufs);
+	psc_free(pj->pj_hdr, PAF_LOCK | PAF_PAGEALIGN, pj->pj_hdr->pjh_iolen);
+	PSCFREE(pj);
+}
+
+/**
+ * pjournal_format - Initialize an on-disk journal.
+ * @fn: file path to store journal.
+ * @nents: number of entries journal may contain.
+ * @entsz: size of a journal entry.
+ * Returns 0 on success, errno on error.
+ */
+int
+pjournal_format(const char *fn, uint32_t nents, uint32_t entsz, uint32_t ra)
+{
+	struct psc_journal_enthdr *pje;
+	struct psc_journal_hdr pjh;
+	struct psc_journal pj;
+	struct stat stb;
+	unsigned char *jbuf;
+	uint32_t i, slot;
+	int rc, fd;
+	ssize_t nb;
+
+	if (nents % ra)
+		psc_fatalx("number of slots (%u) should be a multiple of "
+		    "readahead (%u)", nents, ra);
+
+	memset(&pj, 0, sizeof(struct psc_journal));
+	pj.pj_hdr = &pjh;
+
+	rc = 0;
+	fd = open(fn, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		psc_fatal("%s", fn);
+
+	if (fstat(fd, &stb) == -1)
+		psc_fatal("stat %s", fn);
+
+	pj.pj_fd = fd;
+
+	pjh.pjh_entsz = entsz;
+	pjh.pjh_nents = nents;
+	pjh.pjh_version = PJH_VERSION;
+	pjh.pjh_readahead = ra;
+	pjh.pjh_iolen = PSC_ALIGN(sizeof(pjh), stb.st_blksize);
+	pjh.pjh_magic = PJH_MAGIC;
+
+	PSC_CRC64_INIT(&pjh.pjh_chksum);
+	psc_crc64_add(&pjh.pjh_chksum, &pjh,
+	    offsetof(struct psc_journal_hdr, pjh_chksum));
+	PSC_CRC64_FIN(&pjh.pjh_chksum);
+
+	nb = pwrite(pj.pj_fd, &pjh, pjh.pjh_iolen, 0);
+	if ((size_t)nb != pjh.pjh_iolen)
+		psc_fatal("failed to write journal header");
+
+	jbuf = pjournal_alloc_buf(&pj);
+	for (i = 0; i < ra; i++) {
+		pje = PSC_AGP(jbuf, PJ_PJESZ(&pj) * i);
+		pje->pje_magic = PJE_MAGIC;
+		pje->pje_type = PJE_FORMAT;
+		pje->pje_xid = PJE_XID_NONE;
+		pje->pje_len = 0;
+
+		PSC_CRC64_INIT(&pje->pje_chksum);
+		psc_crc64_add(&pje->pje_chksum, pje,
+		    offsetof(struct psc_journal_enthdr, pje_chksum));
+		psc_crc64_add(&pje->pje_chksum, pje->pje_data,
+		    pje->pje_len);
+		PSC_CRC64_FIN(&pje->pje_chksum);
+	}
+
+	for (slot = 0; slot < pjh.pjh_nents; slot += ra) {
+		nb = pwrite(pj.pj_fd, jbuf, PJ_PJESZ(&pj) * ra,
+		    PJ_GETENTOFF(&pj, slot));
+		if ((size_t)nb != PJ_PJESZ(&pj) * ra)
+			break;
+	}
+	if (close(fd) == -1)
+		psc_fatal("failed to close journal");
+	psc_free(jbuf, PAF_LOCK | PAF_PAGEALIGN, PJ_PJESZ(&pj) * ra);
+	psclog_info("journal %s formatted: %d slots, %d readahead, error=%d",
+	    fn, nents, ra, rc);
+	return (rc);
+}
+
+void
+pjournal_dump_entry(uint32_t slot, struct psc_journal_enthdr *pje)
+{
+	int type;
+	struct slmds_jent_repgen *jrpg;
+	struct slmds_jent_crc *jcrc;
+	struct slmds_jent_bmapseq *sjsq;
+	struct slmds_jent_ino_addrepl *jrir;
+	struct slmds_jent_assign_rep *logentry;
+	struct slmds_jent_namespace *sjnm;
+	struct slmds_jent_bmap_assign *jrba;
+	char name[SL_NAME_MAX + 1];
+
+	type = pje->pje_type & ~(_PJE_FLSHFT - 1);
+	printf("%6d: ", slot);
+	switch (type) {
+	    case MDS_LOG_BMAP_REPL:
+		jrpg = PJE_DATA(pje);
+		printf("type=%3d, xid=%#"PRIx64", txg=%#"PRIx64", fid="SLPRI_FID, 
+			type, pje->pje_xid, pje->pje_txg, jrpg->sjp_fid);
+		break;
+	    case MDS_LOG_BMAP_CRC:
+		jcrc = PJE_DATA(pje);
+		printf("type=%3d, xid=%#"PRIx64", txg=%#"PRIx64", fid="SLPRI_FID, 
+			type, pje->pje_xid, pje->pje_txg, jcrc->sjc_fid);
+		break;
+	    case MDS_LOG_BMAP_SEQ:
+		sjsq = PJE_DATA(pje);
+		printf("type=%3d, xid=%#"PRIx64", txg=%#"PRIx64", " 
+		       "LWM=%#"PRIx64", HWM=%#"PRIx64,
+			type, pje->pje_xid, pje->pje_txg, 
+			sjsq->sjbsq_low_wm,
+			sjsq->sjbsq_high_wm);
+		break;
+	    case MDS_LOG_INO_ADDREPL:
+		jrir = PJE_DATA(pje);
+		printf("type=%3d, xid=%#"PRIx64", txg=%#"PRIx64", fid="SLPRI_FID, 
+			type, pje->pje_xid, pje->pje_txg, jrir->sjir_fid);
+		break;
+	    case MDS_LOG_BMAP_ASSIGN:
+		logentry = PJE_DATA(pje);
+		if (logentry->sjar_flag & SLJ_ASSIGN_REP_FREE)
+			printf("type=%3d, xid=%#"PRIx64", txg=%#"PRIx64", item=%d", 
+				type, pje->pje_xid, pje->pje_txg, logentry->sjar_elem);
+		else {
+			jrba = &logentry->sjar_bmap;
+			printf("type=%3d, xid=%#"PRIx64", txg=%#"PRIx64", fid="SLPRI_FID, 
+				type, pje->pje_xid, pje->pje_txg, jrba->sjba_fid);
+		}
+		break;
+	    case MDS_LOG_NAMESPACE:
+		sjnm = PJE_DATA(pje);
+		printf("type=%3d, xid=%#"PRIx64", txg=%#"PRIx64", fid="SLPRI_FID", ", 
+			type, pje->pje_xid, pje->pje_txg, sjnm->sjnm_target_fid);
+
+		name[0]='\0';
+		if (sjnm->sjnm_namelen) {
+			memcpy(name, sjnm->sjnm_name, sjnm->sjnm_namelen);
+			name[sjnm->sjnm_namelen] = '\0';
+		}
+
+		switch (sjnm->sjnm_op) {
+		    case NS_OP_RECLAIM:
+			printf("op=reclaim");
+			break;
+		    case NS_OP_CREATE:
+			printf("op=create, name=%s", name);
+			break;
+		    case NS_OP_MKDIR:
+			printf("op=mkdir, name=%s", name);
+			break;
+		    case NS_OP_LINK:
+			printf("op=link");
+			break;
+		    case NS_OP_SYMLINK:
+			printf("op=symlink");
+			break;
+		    case NS_OP_RENAME:
+			printf("op=rename");
+			break;
+		    case NS_OP_UNLINK:
+			printf("op=unlink, name=%s", name);
+			break;
+		    case NS_OP_RMDIR:
+			printf("op=rmdir, name=%s", name);
+			break;
+		    case NS_OP_SETSIZE:
+			printf("op=setsize");
+			break;
+		    case NS_OP_SETATTR:
+			printf("op=setattr, mask=%#x", sjnm->sjnm_mask);
+			break;
+	    	    default:
+			psc_fatalx("invalid namespace op %d", sjnm->sjnm_op);
+		}
+		break;
+	    default:
+		psc_fatalx("invalid type %d", type);
+		break;
+	}
+	printf("\n");
+}
+
+/**
+ * pjournal_dump - Dump the contents of a journal file.
+ * @fn: journal filename to query.
+ * @verbose: whether to report stats summary or full dump.
+ */
+int
+pjournal_dump(const char *fn, int verbose)
+{
+	int i, count, ntotal, nmagic, nchksum, nformat, ndump;
+	struct psc_journal_enthdr *pje;
+	struct psc_journal_hdr *pjh;
+	struct psc_journal *pj;
+	unsigned char *jbuf;
+	uint32_t ra, slot;
+	uint64_t chksum;
+	ssize_t nb;
+
+	ntotal = nmagic = nchksum = nformat = ndump = 0;
+
+	pj = pjournal_open(fn);
+	pjh = pj->pj_hdr;
+
+	printf("Journal header info for %s:\n"
+	    "  entsize %u\n"
+	    "  nents %u\n"
+	    "  version %u\n"
+	    "  readahead %u\n"
+	    "  start_offset %#"PRIx64"\n",
+	    fn, PJ_PJESZ(pj), pjh->pjh_nents, pjh->pjh_version,
+	    pjh->pjh_readahead, pjh->pjh_start_off);
+
+	jbuf = pjournal_alloc_buf(pj);
+
+	for (slot = 0, ra = pjh->pjh_readahead;
+	    slot < pjh->pjh_nents; slot += count) {
+
+		count = (pjh->pjh_nents - slot <= ra) ?
+		    (pjh->pjh_nents - slot) : ra;
+		nb = pread(pj->pj_fd, jbuf, PJ_PJESZ(pj) * count, PJ_GETENTOFF(pj, slot));
+		if (nb !=  PJ_PJESZ(pj) * count)
+			printf("Failed to read %d log entries at slot %d.\n", 
+				count, slot);
+
+		for (i = 0; i < count; i++) {
+			ntotal++;
+			pje = (void *)&jbuf[PJ_PJESZ(pj) * i];
+			if (pje->pje_magic != PJE_MAGIC) {
+				nmagic++;
+				printf("Journal slot %d has a bad magic number.\n", 
+					slot + i);
+				continue;
+			}
+			if (pje->pje_type == PJE_FORMAT) {
+				nformat++;
+				continue;
+			}
+
+			PSC_CRC64_INIT(&chksum);
+			psc_crc64_add(&chksum, pje,
+			    offsetof(struct psc_journal_enthdr, pje_chksum));
+			psc_crc64_add(&chksum, pje->pje_data, pje->pje_len);
+			PSC_CRC64_FIN(&chksum);
+
+			if (pje->pje_chksum != chksum) {
+				nchksum++;
+				printf("Journal slot %d has a bad checksum", 
+					slot + i);
+				continue;
+			}
+			if (verbose) {
+				ndump++;
+				pjournal_dump_entry(slot+i, pje);
+			}
+		}
+
+	}
+	if (close(pj->pj_fd) == -1)
+		printf("failed closing journal %s", fn);
+
+	psc_free(jbuf, PAF_LOCK | PAF_PAGEALIGN, PJ_PJESZ(pj));
+	pjournal_release(pj);
+
+	printf("%d slot(s) total, %d in use, %d formatted, %d bad magic, %d bad checksum(s)\n",
+	    ntotal, ndump, nformat, nmagic, nchksum);
+	return (0);
 }
 
 int
