1441dfd1146ea9b2df154df8a4a3e8dccd0df300
Jared Yanovich <yanovich@psc.edu>
2011-05-19 22:04:04 +0000

    factor the rlim adjustment code
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@16659 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- sliod/fidc_iod.c
+++ sliod/fidc_iod.c
@@ -26,11 +26,10 @@
 #include "fidc_iod.h"
 #include "fidcache.h"
 #include "mkfn.h"
+#include "pathnames.h"
 #include "slconfig.h"
 #include "sltypes.h"
 #include "slutil.h"
-
-#define FID_PATH_NAME ".slmd/fidns"
 
 /**
  * sli_fid_makepath - Build the pathname in the FID object root that corresponds
@@ -46,11 +45,12 @@ sli_fg_makepath(const struct slash_fidgen *fg, char *fid_path)
 	a[1] = (fg->fg_fid & UINT64_C(0x00000000000f0000)) >> (BPHXC * 4);
 	a[2] = (fg->fg_fid & UINT64_C(0x000000000000f000)) >> (BPHXC * 3);
 
-	for (i=0; i < FID_PATH_DEPTH; i++)
+	for (i = 0; i < FID_PATH_DEPTH; i++)
 		a[i] += a[i] < 10 ? 0x30 : 0x57;
 
-	xmkfn(fid_path, "%s/%s/%c/%c/%c/%016"PRIx64"_%"PRIx64,
-	    globalConfig.gconf_fsroot, FID_PATH_NAME,
+	xmkfn(fid_path, "%s/%s/%s/%c/%c/%c/%016"PRIx64"_%"PRIx64,
+	    globalConfig.gconf_fsroot,
+	    SL_RPATH_META_DIR, SL_RPATH_FIDNS_DIR,
 	    a[0], a[1], a[2], fg->fg_fid, fg->fg_gen);
 
 	psclog_dbg("fid="SLPRI_FID" fidpath=%s", fg->fg_fid, fid_path);
@@ -60,14 +60,17 @@ static int
 sli_open_backing_file(struct fidc_membh *fcmh)
 {
 	char fidfn[PATH_MAX];
-	int rc = 0;
+	int incr, rc = 0;
 
+	incr = psc_rlim_adj(RLIMIT_NOFILE, 1);
 	sli_fg_makepath(&fcmh->fcmh_fg, fidfn);
 	DEBUG_FCMH(PLL_INFO, fcmh, "before opening new backing file");
 	fcmh_2_fd(fcmh) = open(fidfn, O_CREAT | O_RDWR, 0600);
-	if (fcmh_2_fd(fcmh) == -1)
+	if (fcmh_2_fd(fcmh) == -1) {
 		rc = errno;
-
+		if (incr)
+			psc_rlim_adj(RLIMIT_NOFILE, -1);
+	}
 	return (rc);
 }
 
@@ -76,7 +79,7 @@ sli_fcmh_getattr(struct fidc_membh *fcmh)
 {
 	struct stat stb;
 
-	if (fstat(fcmh_2_fd(fcmh), &stb))
+	if (fstat(fcmh_2_fd(fcmh), &stb) == -1)
 		return (-errno);
 
 	FCMH_LOCK(fcmh);
@@ -84,18 +87,16 @@ sli_fcmh_getattr(struct fidc_membh *fcmh)
 	// XXX get ptruncgen and gen
 	fcmh->fcmh_flags |= FCMH_HAVE_ATTRS;
 	FCMH_ULOCK(fcmh);
-
 	return (0);
 }
 
 int
 sli_fcmh_reopen(struct fidc_membh *fcmh, const struct slash_fidgen *fg)
 {
-	int rc = 0, incr;
+	int rc = 0;
 
 	FCMH_LOCK_ENSURE(fcmh);
 	psc_assert(fg->fg_fid == fcmh_2_fid(fcmh));
-
 
 	/* If our generation number is still unknown try to set it here.
 	 */
@@ -114,43 +115,37 @@ sli_fcmh_reopen(struct fidc_membh *fcmh, const struct slash_fidgen *fg)
 		/* Need to reopen the backing file and possibly
 		 *   remove the old one.
 		 */
-		if (close(fcmh_2_fd(fcmh)))
+		if (close(fcmh_2_fd(fcmh)) == -1)
 			DEBUG_FCMH(PLL_ERROR, fcmh, "close() failed errno=%d",
 			   errno);
+		else
+			psc_rlim_adj(RLIMIT_NOFILE, -1);
 
 		oldfg.fg_fid = fcmh_2_fid(fcmh);
 		oldfg.fg_gen = fcmh_2_gen(fcmh);
 
 		fcmh_2_gen(fcmh) = fg->fg_gen;
 
-		incr = psc_rlim_adj(RLIMIT_NOFILE, 1);
-
 		rc = sli_open_backing_file(fcmh);
 		/* Notify upper layers that open() has failed
 		 */
-		if (rc) {
+		if (rc)
 			fcmh->fcmh_flags |= FCMH_CTOR_FAILED;
-			if (incr)
-				psc_rlim_adj(RLIMIT_NOFILE, -1);
-		}
 
 		/* Do some upfront garbage collection.
 		 */
 		sli_fg_makepath(&oldfg, fidfn);
-		if (unlink(fidfn))
+		if (unlink(fidfn) == -1)
 			DEBUG_FCMH(PLL_ERROR, fcmh, "unlink() failed errno=%d",
 				   errno);
 
 	} else if (fg->fg_gen == fcmh_2_gen(fcmh) &&
 		   (fcmh->fcmh_flags & FCMH_CTOR_DELAYED)) {
 
-		incr = psc_rlim_adj(RLIMIT_NOFILE, 1);
 		rc = sli_open_backing_file(fcmh);
 		if (!rc)
 			fcmh->fcmh_flags &=
 				~(FCMH_CTOR_FAILED | FCMH_CTOR_DELAYED);
-		else if (rc && incr)
-			psc_rlim_adj(RLIMIT_NOFILE, -1);
 
 		DEBUG_FCMH(PLL_NOTIFY, fcmh, "open FCMH_CTOR_DELAYED (rc=%d)",
 		   rc);
@@ -171,7 +166,7 @@ int
 sli_fcmh_ctor(struct fidc_membh *fcmh)
 {
 	struct fcmh_iod_info *fii;
-	int incr, rc = 0;
+	int rc = 0;
 
 	if (fcmh->fcmh_fg.fg_gen == FGEN_ANY) {
 		fcmh->fcmh_flags |= FCMH_CTOR_DELAYED;
@@ -183,20 +178,13 @@ sli_fcmh_ctor(struct fidc_membh *fcmh)
 		return (0);
 	}
 
-	incr = psc_rlim_adj(RLIMIT_NOFILE, 1);
-
-	/* try to get an file descriptor for this backing obj */
 	fii = fcmh_2_fii(fcmh);
-	memset(fii, 0, sizeof(struct fcmh_iod_info));
 
+	/* try to get a file descriptor for this backing obj */
 	rc = sli_open_backing_file(fcmh);
-
-	/* oops, an error; if we increased the rlim, decrease it */
-	if (rc && incr)
-		psc_rlim_adj(RLIMIT_NOFILE, -1);
-	else
+	if (rc == 0)
 		rc = sli_fcmh_getattr(fcmh);
-	DEBUG_FCMH(PLL_INFO, fcmh, "after opening new backing file, rc = %d", rc);
+	DEBUG_FCMH(PLL_INFO, fcmh, "after opening new backing file rc=%d", rc);
 
 	return (rc);
 }
