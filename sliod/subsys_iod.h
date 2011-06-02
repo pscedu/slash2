9699a5d701401d1d1df63d478ca5e6815ae3e0eb
Jared Yanovich <yanovich@psc.edu>
2011-06-02 22:16:15 +0000

    copyright
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@16871 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- sliod/subsys_iod.h
+++ sliod/subsys_iod.h
@@ -1,4 +1,21 @@
 /* $Id$ */
+/*
+ * %PSC_START_COPYRIGHT%
+ * -----------------------------------------------------------------------------
+ * Copyright (c) 2010, Pittsburgh Supercomputing Center (PSC).
+ *
+ * Permission to use, copy, and modify this software and its documentation
+ * without fee for personal use or non-commercial use within your organization
+ * is hereby granted, provided that the above copyright notice is preserved in
+ * all copies and that the copyright and this permission notice appear in
+ * supporting documentation.  Permission to redistribute this software to other
+ * organizations or individuals is not permitted without the written permission
+ * of the Pittsburgh Supercomputing Center.  PSC makes no representations about
+ * the suitability of this software for any purpose.  It is provided "as is"
+ * without express or implied warranty.
+ * -----------------------------------------------------------------------------
+ * %PSC_END_COPYRIGHT%
+ */
 
 #ifndef _SUBSYS_IOD_H_
 #define _SUBSYS_IOD_H_
