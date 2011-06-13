3a05e6bcf278f3ba207612b3d7ee3a4b8ba682e7
Jared Yanovich <yanovich@psc.edu>
2011-06-13 23:54:40 +0000

    more fixes
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@17034 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- utils/tsuite/tsuite.pl
+++ utils/tsuite/tsuite.pl
@@ -254,7 +254,7 @@ sub parse_conf {
 				} elsif ($line =~ m{^\s*#\s*\@zfspool\s*=\s*(\w+)\s+(.*)\s*$}) {
 					$r->{zpool_name} = $1;
 					$r->{zpool_cachefile} = "$base/$1.zcf";
-					$r->{zpool_args} = $3;
+					$r->{zpool_args} = $2;
 				} elsif ($line =~ /^\s*#\s*\@prefmds\s*=\s*(\w+\@\w+)\s*$/) {
 					$r->{prefmds} = $1;
 				} elsif ($line =~ /^\s*fsroot\s*=\s*(\S+)\s*;\s*$/) {
@@ -302,8 +302,6 @@ close CLICMD;
 parse_conf();
 
 if ($build) {
-	execute "cd $src/distrib/fuse && ./configure >/dev/null && make >/dev/null";
-	fatalx "make failed" if $?;
 	execute "cd $src/zfs && make build >/dev/null";
 	fatalx "make failed" if $?;
 	execute "cd $src/slash_nara && make build >/dev/null";
@@ -322,11 +320,15 @@ foreach $i (@mds) {
 
 		screen -S SLMDS -X quit || true
 
+		# run pickle probe tests
+		(cd $src && make printvar-CC >/dev/null)
+
+		pkill zfs-fuse || true
 		$zfs_fuse &
 		sleep 2
 		$zpool destroy $i->{zpool_name} || true
-		$zpool set cachefile=$i->{zpool_cachefile} $i->{zpool_name}
 		$zpool create -f $i->{zpool_name} $i->{zpool_args}
+		$zpool set cachefile=$i->{zpool_cachefile} $i->{zpool_name}
 		$slimmns_format /$i->{zpool_name}
 		sync; sync
 		umount /$i->{zpool_name}
