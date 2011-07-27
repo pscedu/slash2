c12b796d92dba9cd6b353b3fff12752875e03d7f
Jared Yanovich <yanovich@psc.edu>
2011-07-27 19:52:25 +0000

    tidy
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@17352 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- utils/cursor/cursor_mgr.c
+++ utils/cursor/cursor_mgr.c
@@ -26,49 +26,47 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#include "fid.h"
 #include "pfl/cdefs.h"
 #include "pfl/pfl.h"
-
 #include "psc_util/journal.h"
+
+#include "fid.h"
 
 const char *progname;
 
 __dead void
 usage(void)
 {
-	fprintf(stderr, "usage: %s [-dv] [-x txg] [-f fid] -c cursor_file\n", progname);
+	fprintf(stderr, "usage: %s [-dv] [-f fid] [-x txg] -c cursor_file\n",
+	    progname);
 	exit(1);
 }
 
 int
 main(int argc, char *argv[])
 {
-	char *p, tmbuf[26];
-	char *cursor_file = NULL, c;
+	uint64_t newtxg = 0, newfid = 0, fid, cycle, newcycle;
+	char *p, tmbuf[26], *cursor_file = NULL, c;
 	int dump = 0, verbose = 0, fd, rc;
-	uint64_t newtxg = 0, newfid = 0;
 	struct psc_journal_cursor cursor;
-	uint64_t fid, cycle, newcycle;
 
 	progname = argv[0];
-
-	while ((c = getopt(argc, argv, "dvc:x:f:")) != -1)
+	while ((c = getopt(argc, argv, "c:df:vx:")) != -1)
 		switch (c) {
+		case 'c':
+			cursor_file = optarg;
+			break;
 		case 'd':
 			dump = 1;
 			break;
-		case 'c':
-			cursor_file = optarg;
+		case 'f':
+			newfid = strtol(optarg, NULL, 0);
 			break;
 		case 'v':
 			verbose = 1;
 			break;
 		case 'x':
 			newtxg = strtol(optarg, NULL, 0);
-			break;
-		case 'f':
-			newfid = strtol(optarg, NULL, 0);
 			break;
 		default:
 			usage();
@@ -85,7 +83,7 @@ main(int argc, char *argv[])
 
 	rc = pread(fd, &cursor, sizeof(struct psc_journal_cursor), 0);
 	if (rc != sizeof(struct psc_journal_cursor))
-		err(1, "cursor file pread() failed");
+		err(1, "cursor file read");
 
 	ctime_r((time_t *)&cursor.pjc_timestamp, tmbuf);
 	p = strchr(tmbuf, '\n');
@@ -94,45 +92,43 @@ main(int argc, char *argv[])
 
 	cycle = FID_GET_CYCLE(cursor.pjc_fid);
 	if (dump || verbose) {
-		printf("Cursor Contents:\n"
-			"\tpjc_magic = %"PRIx64"\n"
-			"\tpjc_version = %"PRIx64"\n"
-			"\tpjc_timestamp = %"PRId64" (%s) \n"
-			"\tpjc_uuid = %s\n"
-			"\tpjc_commit_txg =  %"PRId64"\n"
-			"\tpjc_distill_xid =  %"PRId64"\n"
-			"\tpjc_fid = 0x%"PRIx64" (flag = %"PRIx64", site id = %"PRIx64", cycle = %"PRIx64", fid = %"PRIx64") \n"
-			"\tpjc_seqno_lwm = %"PRIx64"\n"
-			"\tpjc_seqno_hwm = %"PRIx64"\n"
-			"\tpjc_tail = %"PRIx64"\n"
-			"\tpjc_update_seqno = %"PRIx64"\n"
-			"\tpjc_reclaim_seqno = %"PRIx64"\n"
-			"\tpjc_replay_xid = %"PRIx64"\n\n",
-
-			cursor.pjc_magic, cursor.pjc_version, 
-
-			cursor.pjc_timestamp, tmbuf,
-
-			cursor.pjc_uuid, cursor.pjc_commit_txg, cursor.pjc_distill_xid,
-
-			cursor.pjc_fid, 
-			FID_GET_FLAGS(cursor.pjc_fid),
-			FID_GET_SITEID(cursor.pjc_fid),
-			FID_GET_CYCLE(cursor.pjc_fid),
-			FID_GET_INUM(cursor.pjc_fid),
-
-			cursor.pjc_seqno_lwm, cursor.pjc_seqno_hwm,
-			cursor.pjc_tail, cursor.pjc_update_seqno,
-			cursor.pjc_reclaim_seqno, cursor.pjc_replay_xid);
+		printf("Cursor contents:\n"
+		    "\tmagic = %"PRIx64"\n"
+		    "\tversion = %"PRIx64"\n"
+		    "\ttimestamp = %"PRId64" (%s)\n"
+		    "\tuuid = %s\n"
+		    "\tcommit_txg = %"PRId64"\n"
+		    "\tdistill_xid = %"PRId64"\n"
+		    "\tfid = 0x%"PRIx64" (flag = %"PRIx64", site id = %"PRIx64", cycle = %"PRIx64", fid = %"PRIx64")\n"
+		    "\tseqno_lwm = %"PRIx64"\n"
+		    "\tseqno_hwm = %"PRIx64"\n"
+		    "\ttail = %"PRIx64"\n"
+		    "\tupdate_seqno = %"PRIx64"\n"
+		    "\treclaim_seqno = %"PRIx64"\n"
+		    "\treplay_xid = %"PRIx64"\n\n",
+		    cursor.pjc_magic,
+		    cursor.pjc_version,
+		    cursor.pjc_timestamp, tmbuf,
+		    cursor.pjc_uuid,
+		    cursor.pjc_commit_txg,
+		    cursor.pjc_distill_xid,
+		    cursor.pjc_fid,
+		    FID_GET_FLAGS(cursor.pjc_fid),
+		    FID_GET_SITEID(cursor.pjc_fid),
+		    FID_GET_CYCLE(cursor.pjc_fid),
+		    FID_GET_INUM(cursor.pjc_fid),
+		    cursor.pjc_seqno_lwm, cursor.pjc_seqno_hwm,
+		    cursor.pjc_tail, cursor.pjc_update_seqno,
+		    cursor.pjc_reclaim_seqno, cursor.pjc_replay_xid);
 
 		fid = cursor.pjc_fid;
 		fid = fid | FID_MAX;
 
-		printf("The max FID for this cycle is 0x%"PRIx64"\n", fid);
+		printf("The max FID for this cycle is %#"PRIx64"\n", fid);
 
 		if (cycle < ((UINT64_C(1) << SLASH_ID_CYCLE_BITS) - 1)) {
 			fid++;
-			printf("The first FID for the next cycle is 0x%"PRIx64"\n", fid);
+			printf("The first FID for the next cycle is %#"PRIx64"\n", fid);
 		}
 
 		if (dump)
@@ -149,16 +145,15 @@ main(int argc, char *argv[])
 
 	if (newfid) {
 		newcycle = FID_GET_CYCLE(cursor.pjc_fid);
-		if (newcycle <= cycle) {
-			fprintf(stderr, "You must increase the cycle when setting a new fid.\n");
-			exit(0);
-		}
+		if (newcycle <= cycle)
+			errx(1, "cycle must be increased when setting a new FID");
 		cursor.pjc_fid = newfid;
 	}
-	
+
 	rc = pwrite(fd, &cursor, sizeof(struct psc_journal_cursor), 0);
 	if (rc != sizeof(struct psc_journal_cursor))
-		err(1, "cursor file pwrite() failed");
-
+		err(1, "cursor file write");
+	if (close(fd) == -1)
+		err(1, "cursor file close");
 	exit(0);
 }
