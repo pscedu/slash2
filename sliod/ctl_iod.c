921ca8de5174c9f42b4f19c38001e70a6587dff8
Zhihui Zhang <zhihui@psc.edu>
2011-08-26 16:31:44 +0000

    typo
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@17597 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- sliod/ctl_iod.c
+++ sliod/ctl_iod.c
@@ -291,8 +291,7 @@ sli_import(const char *fn, const struct stat *stb, void *arg)
 				 * XXX
 				 * If we fail here, we should undo import
 				 * above. However, with checks earlier,
-				 * we will probably won't fail for EXDEV
-				 * here.
+				 * we probably won't fail for EXDEV here.
 				 */
 				sli_fg_makepath(&mp->fg, fidfn);
 				if (link(fn, fidfn) == -1)
