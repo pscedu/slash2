86cb2fa43b3fdc78169be466ccd2c83b3f3fc265
Zhihui Zhang <zhihui@psc.edu>
2011-09-23 14:33:14 +0000

    log fd used
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@17893 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- sliod/fidc_iod.c
+++ sliod/fidc_iod.c
@@ -72,6 +72,8 @@ sli_open_backing_file(struct fidc_membh *fcmh)
 		if (incr)
 			psc_rlim_adj(RLIMIT_NOFILE, -1);
 	}
+	psclog_info("open backing file:  path=%s, fd = %d, rc = %d", 
+	    fidfn, fcmh_2_fd(fcmh), rc);
 	return (rc);
 }
 
