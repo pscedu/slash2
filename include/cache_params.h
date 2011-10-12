7e65f44729a03e7c229c0a20680e26c074cb4a77
Paul Nowoczynski <pauln@psc.edu>
2011-10-12 11:58:21 +0000

    smaller mem footprint
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@18131 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- include/cache_params.h
+++ include/cache_params.h
@@ -23,7 +23,7 @@
 /* Begin hand computed */
 
 #define FIDC_MDS_DEFSZ		(32 * 1024)
-#define FIDC_CLI_DEFSZ		( 2 * 1024)
+#define FIDC_CLI_DEFSZ		64
 #define FIDC_ION_DEFSZ		(32 * 1024)
 
 #define SLASH_SLVRS_PER_BMAP	128
