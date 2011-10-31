119ea4aeccb5c383ad9750635af1e9d69be67944
Jared Yanovich <yanovich@psc.edu>
2011-10-31 23:39:44 +0000

    nasty workaround for a FUSE bug
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@18297 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- msctl/msctl.c
+++ msctl/msctl.c
@@ -122,7 +122,7 @@ fn2fid(const char *fn)
 		err(1, "statvfs %s", fn);
 
 #ifndef HAVE_NO_FUSE_FSID
-	if (sfb.f_fsid != SLASH_FSID)
+	if (sfb.f_fsid != SLASH_FSID && sfb.f_fsid)
 		errx(1, "%s: file is not in a SLASH file system (fsid=%lx)",
 		    fn, sfb.f_fsid);
 #endif
