663c72a1d5c0b6f0289fa88d0ae571ba06133642
Jared Yanovich <yanovich@psc.edu>
2012-01-18 21:01:11 +0000

    tidy
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@18651 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- slashd/mdslog.c
+++ slashd/mdslog.c
@@ -1165,8 +1165,9 @@ mds_send_batch_reclaim(uint64_t batchno)
 	len = size = offsetof(struct srt_reclaim_entry, _pad);
 	for (i = 1; i < count; i++) {
 		if (i < count - 1 && entryp->xid >= xid)
-			psclog_warnx("Out of order log entries: %d, %d, %"PRIx64", %"PRIx64"\n",
-				i, count, xid, entryp->xid);
+			psclog_warnx("Out of order log entries: %d, %d, "
+			    "%"PRIx64", %"PRIx64, i, count, xid,
+			    entryp->xid);
 		entryp++;
 		next_entryp = PSC_AGP(next_entryp, len);
 		memmove(next_entryp, entryp, len);
