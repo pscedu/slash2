791f569003f36eb2f367ce8eedd5ddc4fe1ceb93
Paul Nowoczynski <pauln@psc.edu>
2012-02-06 22:53:57 +0000

    increase client fidc size to help with readdir performance
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@18703 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- include/cache_params.h
+++ include/cache_params.h
@@ -23,8 +23,8 @@
 /* Begin hand computed */
 
 #define FIDC_MDS_DEFSZ		(32 * 1024)
-#define FIDC_CLI_DEFSZ		64
-#define FIDC_ION_DEFSZ		(32 * 1024)
+#define FIDC_CLI_DEFSZ		1024
+#define FIDC_ION_DEFSZ		(8 * 1024)
 
 #define SLASH_SLVRS_PER_BMAP	128
 #define SLASH_SLVR_SIZE		(1024 * (off_t)1024)
