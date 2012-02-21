a8a90253da3c2166bc447dd72a834a1bfccc691c
Paul Nowoczynski <pauln@psc.edu>
2012-02-21 16:39:00 +0000

    override some errno's to deal with inconsistent cross platform errno's
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@18774 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- include/slerr.h
+++ include/slerr.h
@@ -54,5 +54,7 @@ char *slstrerror(int);
 #define SLERR_BMAP_LEASEEXT_FAILED	(_SLERR_START + 28)
 #define SLERR_BMAP_LEASEREASSIGN_FAILED	(_SLERR_START + 29)
 #define SLERR_RES_BADTYPE		(_SLERR_START + 30)
-
+#define SLERR_ALREADY                   (_SLERR_START + 31)
+#define SLERR_INVAL                     (_SLERR_START + 32)
+#define SLERR_2BIG                      (_SLERR_START + 33)
 #endif /* _SLERR_H_ */
