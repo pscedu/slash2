15ed84b67e803ff68568f958d8200bd8f7aafe95
Jared Yanovich <yanovich@psc.edu>
2012-06-27 16:29:35 +0000

    fix infinite loop by reverting two bogus patches
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@19234 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- mount_slash/rpc_cli.c
+++ mount_slash/rpc_cli.c
@@ -126,7 +126,6 @@ slc_rmc_retry_pfcc(__unusedx struct pscfs_clientctx *pfcc, int *rc)
 	case EHOSTDOWN:
 	case EHOSTUNREACH:
 	case EIO:
-	case ENOENT:
 	case ENETDOWN:
 	case ENETRESET:
 	case ENETUNREACH:
@@ -135,7 +134,7 @@ slc_rmc_retry_pfcc(__unusedx struct pscfs_clientctx *pfcc, int *rc)
 	case ETIMEDOUT:
 		break;
 	default:
-		return (*rc);
+		return (0);
 	}
 
 //	retry = global setting
