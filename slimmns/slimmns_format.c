cfaa44ac060dd911f89f69d394101fba6fc6b1af
Zhihui Zhang <zhihui@psc.edu>
2012-08-13 18:40:13 +0000

    duplicate code (cause slashd start failure)
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@19501 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- slimmns/slimmns_format.c
+++ slimmns/slimmns_format.c
@@ -196,9 +196,6 @@ slnewfs_create(const char *fsroot, uint32_t depth)
 
 	if (!fsuuid)
 		fsuuid = psc_random64();
-	fprintf(fp, "%#16"PRIx64"\n", fsuuid);
-	if (!fsuuid)
-		fsuuid = psc_random64();
 	fprintf(fp, "%16"PRIx64"\n", fsuuid);
 	if (!ion)
 		printf("The UUID of the file system is %#16"PRIx64"\n", fsuuid);
