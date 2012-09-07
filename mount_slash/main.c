2aa678e77bd0b355eb74c2fa7a738bd670b49faf
Zhihui Zhang <zhihui@psc.edu>
2012-09-07 17:24:01 +0000

    compiler fix
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@19626 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- mount_slash/main.c
+++ mount_slash/main.c
@@ -123,17 +123,6 @@ struct psc_poolmgr		*slc_biorq_pool;
 
 uint32_t			 sys_upnonce;
 
-/* names must match the field definitions in struct slash2_client_opstats */
-struct slash2_client_opstats msl_opstats = {
-
-	{ "read", 		0 },
-	{ "read_ahead",		0 },
-	{ "read_retry",		0 },
-	{ "write",		0 },
-	{ "write_retry",	0 },
-	{ NULL,			0 }
-};
-
 __inline int
 fcmh_checkcreds(struct fidc_membh *f, const struct slash_creds *crp,
     int accmode)
