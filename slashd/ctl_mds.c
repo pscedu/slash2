07d01d5062546f863042a51243aaa704a1a11d32
Jared Yanovich <yanovich@psc.edu>
2013-01-02 07:48:41 +0000

    oops
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@20343 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- slashd/ctl_mds.c
+++ slashd/ctl_mds.c
@@ -540,7 +540,7 @@ slmctlrep_getbml(int fd, struct psc_ctlmsghdr *mh, void *m)
 		BML_LOCK(bml);
 		strlcpy(scbl->scbl_resname,
 		    bml->bml_ios && bml->bml_ios != IOS_ID_ANY ?
-		    "<any>" : libsl_id2res(bml->bml_ios)->res_name,
+		    libsl_id2res(bml->bml_ios)->res_name : "<any>",
 		    sizeof(scbl->scbl_resname));
 		scbl->scbl_fg = bml_2_bmap(bml)->bcm_fcmh->fcmh_fg;
 		scbl->scbl_bno = bml_2_bmap(bml)->bcm_bmapno;
