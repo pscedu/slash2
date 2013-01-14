4aa3d36685395c5d52e7c572ff8c2effdd23a59c
Jared Yanovich <yanovich@psc.edu>
2013-01-14 04:32:33 +0000

    fix crash
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@20464 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- mount_slash/mount_slash_int.c
+++ mount_slash/mount_slash_int.c
@@ -260,6 +260,7 @@ msl_biorq_build(struct msl_fsrqinfo *q, struct bmapc_memb *b, char *buf,
 
 			BMPCE_WAIT(e);
 
+			BMPCE_LOCK(e);
 			bmpce_release_locked(e, bmpc);
 			BMPC_LOCK(bmpc);
 			goto restart;
