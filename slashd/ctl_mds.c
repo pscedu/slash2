06d4c4fc665d9313b0e7c7f060927180536b5617
Zhihui Zhang <zhihui@psc.edu>
2013-01-17 18:45:01 +0000

    sanity check opstats
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@20509 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- slashd/ctl_mds.c
+++ slashd/ctl_mds.c
@@ -640,7 +640,8 @@ struct pfl_opstat pflctl_opstats[] = {
 	PFL_OPSTAT_INIT("setxattr"),
 	PFL_OPSTAT_INIT("statfs"),
 	PFL_OPSTAT_INIT("symlink"),
-	PFL_OPSTAT_INIT("unlink")
+	PFL_OPSTAT_INIT("unlink"),
+	PFL_OPSTAT_INIT(NULL)
 };
 
 PFLCTL_SVR_DEFS;
@@ -648,6 +649,12 @@ PFLCTL_SVR_DEFS;
 void
 slmctlthr_main(const char *fn)
 {
+	int i;
+
+	i = 0;
+	while (pflctl_opstats[i].pos_name != NULL) i++;
+	psc_assert(i == SLM_OPST_MAX);
+
 	psc_ctlparam_register("faults", psc_ctlparam_faults);
 	psc_ctlparam_register("log.file", psc_ctlparam_log_file);
 	psc_ctlparam_register("log.format", psc_ctlparam_log_format);
