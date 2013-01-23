140b9c9b561f54e090df5812f6c87e9f541a8ad4
Jared Yanovich <yanovich@psc.edu>
2013-01-23 17:21:21 +0000

    notice should reach log
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@20555 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- sliod/sliod.sh
+++ sliod/sliod.sh
@@ -31,7 +31,7 @@ ulimit -n 100000
 ulimit -c $((1024 * 1024 * 1024 * 50))
 export LD_LIBRARY_PATH=/usr/local/lib
 export PSC_SYSLOG=1
-export PSC_LOG_LEVEL=warn
+export PSC_LOG_LEVEL=notice
 export PSC_LOG_LEVEL_info=info
 export PSC_LOG_FILE=$base/log/$host.$name/%t
 export PSC_LOG_FILE_LINK=$base/log/$host.$name/latest
