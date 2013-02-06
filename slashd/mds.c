ce6b4ab599743854cd1c5bf231ce7332cf8ac45d
Jared Yanovich <yanovich@psc.edu>
2013-02-06 00:47:23 +0000

    tidy
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@20692 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- slashd/mds.c
+++ slashd/mds.c
@@ -185,7 +185,7 @@ mds_bmap_directio_locked(struct bmapc_memb *b, enum rw rw,
 			 */
 			BML_ULOCK(bml);
 
-			DEBUG_BMAP(PLL_NOTIFY, b, "dup lease");
+			DEBUG_BMAP(PLL_NOTICE, b, "dup lease");
 
 			return (0);
 		}
