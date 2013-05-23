e8e6171b1b3cefe5984b62a84f499c5a4884ea75
Jared Yanovich <yanovich@psc.edu>
2013-05-23 23:46:12 +0000

    more unused
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@21485 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- share/ctlcli_common.c
+++ share/ctlcli_common.c
@@ -91,11 +91,7 @@ sl_conn_prdat(const struct psc_ctlmsghdr *mh, const void *m)
 	char *p, *site, nid[NI_MAXHOST], *res, addrbuf[RESM_ADDRBUF_SZ];
 	const struct slctlmsg_conn *scc = m;
 	const char *addr, *stype, *prid;
-	int type;
 
-	type = scc->scc_type;
-	if (type < 0 || type >= (int)nitems(slconn_restypes))
-		type = nitems(slconn_restypes) - 1;
 	stype = slconn_restypes[scc->scc_type];
 
 	strlcpy(addrbuf, scc->scc_addrbuf, sizeof(addrbuf));
