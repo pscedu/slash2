af218a2df455230220cf31231e9d7e86dc4e8245
Zhihui Zhang <zhihui@psc.edu>
2013-05-31 19:52:21 +0000

    comment
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@21529 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- slashd/main_mds.c
+++ slashd/main_mds.c
@@ -122,8 +122,9 @@ append_path(const char *newpath)
 }
 
 /*
- * Use system() to import pool and mount file systems. Note that the path 
- * is built in at compile time and added by append_path() at run time.
+ * Use system() calls to import pool and mount file systems. Note that the paths 
+ * needed by the system() is built in at compile time and added by append_path() 
+ * at run time.
  */
 void
 import_zpool(const char *zpoolname, const char *zfspoolcf)
