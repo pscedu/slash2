b48c4d901b88026fc8fd6a16f9f68042010b3c35
Jared Yanovich <yanovich@psc.edu>
2013-08-21 22:29:55 +0000

    copyright
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@22220 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- utils/tsuite/tsuite.pl
+++ utils/tsuite/tsuite.pl
@@ -2,17 +2,22 @@
 # $Id$
 # %PSCGPL_START_COPYRIGHT%
 # -----------------------------------------------------------------------------
-# Copyright (c) 2009-2012, Pittsburgh Supercomputing Center (PSC).
+# Copyright (c) 2009-2013, Pittsburgh Supercomputing Center (PSC).
 #
-# Permission to use, copy, and modify this software and its documentation
-# without fee for personal use or non-commercial use within your organization
-# is hereby granted, provided that the above copyright notice is preserved in
-# all copies and that the copyright and this permission notice appear in
-# supporting documentation.  Permission to redistribute this software to other
-# organizations or individuals is not permitted without the written permission
-# of the Pittsburgh Supercomputing Center.  PSC makes no representations about
-# the suitability of this software for any purpose.  It is provided "as is"
-# without express or implied warranty.
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License contained in the file
+# `COPYING-GPL' at the top of this distribution or at
+# https://www.gnu.org/licenses/gpl-2.0.html for more details.
+#
+# Pittsburgh Supercomputing Center	phone: 412.268.4960  fax: 412.268.5832
+# 300 S. Craig Street			e-mail: remarks@psc.edu
+# Pittsburgh, PA 15213			web: http://www.psc.edu/
 # -----------------------------------------------------------------------------
 # %PSC_END_COPYRIGHT%
 
