1713387325dab08218bf2e703c99daf2ce2d8b56
Zhihui Zhang <zhihui@psc.edu>
2013-09-17 19:53:07 +0000

    cruft
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@22307 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- mount_slash/mount_slash_int.c
+++ mount_slash/mount_slash_int.c
@@ -1312,7 +1312,6 @@ msl_pages_schedflush(struct bmpc_ioreq *r)
 	r->biorq_ref++;
 	BIORQ_SETATTR(r, BIORQ_FLUSHRDY);
 	psc_assert(psclist_disjoint(&r->biorq_lentry));
-//	psc_assert(PSC_SPLAY_ENTRY_CONJOINT(bmpc_biorq_tree, r));
 	BIORQ_ULOCK(r);
 
 	bmpc->bmpc_pndgwr++;
