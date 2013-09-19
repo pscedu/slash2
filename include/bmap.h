a61aeb689e1c3bda76df3a4c4e27711d5dca559e
Zhihui Zhang <zhihui@psc.edu>
2013-09-19 18:14:10 +0000

    INFO --> DEBUG
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@22336 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- include/bmap.h
+++ include/bmap.h
@@ -247,7 +247,7 @@ struct bmap {
 // XXX ensure locked???
 #define bmap_op_start_type(b, type)					\
 	do {								\
-		DEBUG_BMAP(PLL_INFO, (b),				\
+		DEBUG_BMAP(PLL_DEBUG, (b),				\
 		    "took reference (type=%u)", (type));		\
 		psc_atomic32_inc(&(b)->bcm_opcnt);			\
 	} while (0)
@@ -255,7 +255,7 @@ struct bmap {
 #define bmap_op_done_type(b, type)					\
 	do {								\
 		(void)BMAP_RLOCK(b);					\
-		DEBUG_BMAP(PLL_INFO, (b),				\
+		DEBUG_BMAP(PLL_DEBUG, (b),				\
 		    "drop reference (type=%u)", (type));		\
 		psc_assert(psc_atomic32_read(&(b)->bcm_opcnt) > 0);	\
 		psc_atomic32_dec(&(b)->bcm_opcnt);			\
