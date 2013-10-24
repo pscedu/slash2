f4a0ab34ceae938721ec35dc51040ba739b00b85
Jared Yanovich <yanovich@psc.edu>
2013-10-24 20:38:26 +0000

    no sense in having two different files
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@22536 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- slashd/journal_mds.h
+++ slashd/journal_mds.h
@@ -25,10 +25,19 @@
 #ifndef _JOURNAL_MDS_H_
 #define _JOURNAL_MDS_H_
 
+#include "pfl/journal.h"
+
 #include "lnet/types.h"
 
-#include "slashd/mdsio.h"
 #include "slashrpc.h"
+#include "sltypes.h"
+
+#include "slashd/mdsio.h"
+
+struct bmapc_memb;
+struct fidc_membh;
+struct srm_bmap_crcup;
+struct srt_stat;
 
 #define SLJ_MDS_JNENTS			(128 * 1024)		/* 131072 */
 #define SLJ_MDS_READSZ			1024			/* SLJ_MDS_JNENTS % SLJ_MDS_READSZ == 0 */
@@ -184,5 +193,56 @@ struct slmds_jent_namespace {
  * than or this size.
  */
 #define	SLJ_MDS_ENTSIZE			512
+
+/*
+ * Keep track of the bmap associated with a CRC update to save FID and
+ * bmap lookups.
+ */
+struct sl_mds_crc_log {
+	struct bmapc_memb		*scl_bmap;
+	struct srm_bmap_crcup		*scl_crcup;
+	sl_ios_id_t			 scl_iosid;
+};
+
+#define MDS_LOG_BMAP_REPLS		(_PJE_FLSHFT << 0)
+#define MDS_LOG_BMAP_CRC		(_PJE_FLSHFT << 1)
+#define MDS_LOG_BMAP_SEQ		(_PJE_FLSHFT << 2)
+#define MDS_LOG_BMAP_ASSIGN		(_PJE_FLSHFT << 3)
+#define MDS_LOG_INO_REPLS		(_PJE_FLSHFT << 4)
+#define MDS_LOG_NAMESPACE		(_PJE_FLSHFT << 5)
+#define _MDS_LOG_LAST_TYPE		(_PJE_FLSHFT << 5)
+
+/*
+ * A structure used to describe the log application progress on each site.
+ */
+struct site_progress {
+	int				sp_siteid;
+	uint64_t			sp_seqno;
+};
+
+void	mdslog_bmap_crc(void *, uint64_t, int);
+void	mdslog_bmap_repls(void *, uint64_t, int);
+void	mdslog_ino_repls(void *, uint64_t, int);
+void	mdslog_namespace(int, uint64_t, uint64_t, uint64_t,
+	    const struct srt_stat *, int, const char *, const char *, void *);
+
+void	mdslogfill_bmap_repls(struct bmapc_memb *, struct slmds_jent_bmap_repls *);
+void	mdslogfill_ino_repls(struct fidc_membh *, struct slmds_jent_ino_repls *);
+
+void	mds_journal_init(int, uint64_t);
+
+int	mds_bmap_crc_update(struct bmapc_memb *, sl_ios_id_t, struct srm_bmap_crcup *);
+
+void	mds_reserve_slot(int);
+void	mds_unreserve_slot(int);
+
+int	mds_replay_namespace(struct slmds_jent_namespace *, int);
+int	mds_replay_handler(struct psc_journal_enthdr *);
+
+extern struct psc_journal		*slm_journal;
+extern struct psc_journal_cursor	 mds_cursor;
+
+extern uint64_t				 current_update_batchno;
+extern uint64_t				 current_reclaim_batchno;
 
 #endif /* _JOURNAL_MDS_H_ */
