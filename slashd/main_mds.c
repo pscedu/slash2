164a192e46eb9c14a09a70cf51df2d91971b6c7a
Jared Yanovich <yanovich@psc.edu>
2013-11-30 18:07:02 +0000

    add back index
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@22745 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- slashd/main_mds.c
+++ slashd/main_mds.c
@@ -638,6 +638,9 @@ main(int argc, char *argv[])
 		    "CREATE INDEX 'upsch_bno_idx'"
 		    " ON 'upsch' ('bno')");
 		dbdo(NULL, NULL,
+		    "CREATE INDEX 'upsch_uid_idx'"
+		    " ON 'upsch' ('uid')");
+		dbdo(NULL, NULL,
 		    "CREATE INDEX 'upsch_gid_idx'"
 		    " ON 'upsch' ('gid')");
 	}
