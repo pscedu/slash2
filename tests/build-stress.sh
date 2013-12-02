4947144de97ab0c4efaeea8018069a51a1f152a4
Zhihui Zhang <zhihui@psc.edu>
2013-12-02 19:11:46 +0000

    improve test
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@22759 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- tests/build-stress.sh
+++ tests/build-stress.sh
@@ -3,11 +3,35 @@
 # 03/11/2013: Build slash2 source code in a loop.
 #
 
+function cleanup()
+{
+    echo ""
+
+    end_time=$SECONDS
+
+    ss=`expr $end_time - $start_time`
+    
+    mm=`expr $ss / 60` 
+    ss=`expr $ss % 60` 
+    hh=`expr $mm / 60` 
+    mm=`expr $mm % 60` 
+
+    printf "\nTotal time elapsed %02d::%02d::%02d\n" $hh $mm $ss 
+    exit 0
+}
+
 if [ $# -ne 3 ] 
 then
     echo "Usage: build-stress.sh change dir count"
     exit 0
 fi
+
+# trap Control+C
+
+trap 'cleanup' 2
+
+start_time=$SECONDS
+
 
 mydir=`pwd`
 for ((i=0; i < $3; i++))
@@ -46,3 +70,5 @@ do
     cd $mydir
 
 done
+
+cleanup
