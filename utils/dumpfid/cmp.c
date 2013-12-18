1fe3be9c2e811b4f7bb896b935e267e8f5613251
Jared Yanovich <yanovich@psc.edu>
2013-12-18 17:53:59 +0000

    compile
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@22866 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

---
+++ utils/dumpfid/cmp.c
@@ -0,0 +1,19 @@
+/* $Id$ */
+
+#ifdef HAVE_FTS
+#  undef _FILE_OFFSET_BITS	/* FTS is not 64-bit ready */
+#endif
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <stdio.h>
+#include <fts.h>
+
+#include "pfl/cdefs.h"
+
+int
+cmp(const FTSENT **a, const FTSENT **b)
+{
+	return (CMP((*a)->fts_statp->st_ino, (*b)->fts_statp->st_ino));
+}
