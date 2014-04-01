2731bd36e8954002b8cb57d28d831690b4ddc86f
Zhihui Zhang <zhihui@psc.edu>
2014-04-01 19:29:33 +0000

    comment
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@23494 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- sliod/fidc_iod.c
+++ sliod/fidc_iod.c
@@ -313,9 +313,9 @@ sli_fcmh_dtor(__unusedx struct fidc_membh *f)
 }
 
 struct sl_fcmh_ops sl_fcmh_ops = {
-/* sfop_ctor */		sli_fcmh_ctor,
-/* sfop_dtor */		sli_fcmh_dtor,
-/* sfop_getattr */	NULL,
-/* sfop_postsetattr */	NULL,
-/* sfop_modify */	sli_fcmh_reopen
+	sli_fcmh_ctor,		/* sfop_ctor */		
+	sli_fcmh_dtor,		/* sfop_dtor */		
+	NULL,			/* sfop_getattr */	
+	NULL,			/* sfop_postsetattr */	
+	sli_fcmh_reopen		/* sfop_modify */	
 };
