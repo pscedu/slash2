c9f55c88e8d42b0a4084178f77ada0546271c711
Chris Ganas <ganas@psc.edu>
2014-04-02 18:26:51 +0000

    progress on remote test_handle
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@23508 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- utils/tsuite2/test_handle.py
+++ utils/tsuite2/test_handle.py
@@ -1,15 +1,40 @@
-import sys, json
+import sys, json, pkgutil, time
 
 class TestHandler(object):
   """Object used to set up testing environment and passed along to tests for runtime information."""
 
-  def __init__(self, json_constants):
+  def __init__(self, json_constants, modules):
     self.runtime = json.loads(json_constants)
+    self.modules = modules
 
   def run_tests(self):
-    """Run all tests from the tests directory."""
+    """Run all tests from the tests directory and print results"""
+    tset_results = []
+    for module in self.modules:
+      test = {"name": module.__name__[:-3]}
+      test["setup"]=module.setup()
+
+      start = time.time()
+      test["operate"]=module.operate()
+      elapsed = time.time() - start
+      test["operate"]["elapsed"] = elapsed
+
+      test["cleanup"]=module.cleanup()
+
+      tset_results["tests"].append(test)
+    print json.loads(tset_result)
+
+def load_all_modules_from_dir(dirname):
+  modules = []
+  for importer, package_name, _ in pkgutil.iter_modules([dirname]):
+    full_package_name = '%s.%s' % (dirname, package_name)
+    if full_package_name not in sys.modules:
+      module = importer.find_module(package_name).load_module(full_package_name)
+      modules.append(module)
+  return modules
 
 if __name__ == "__main__":
   #Ran from script
-  TestHandler(sys.argv[1])
+  modules = load_all_modules_from_dir("./modules")
+  TestHandler(sys.argv[1], modules)
 
