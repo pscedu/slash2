52d530b02f551d2b56ffc8342c490dcc834b3c22
Jared Yanovich <yanovich@psc.edu>
2014-04-09 04:28:51 +0000

    plug in rusage
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@23559 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- sliod/ctl_iod.c
+++ sliod/ctl_iod.c
@@ -571,6 +571,7 @@ slictlthr_main(const char *fn)
 	psc_ctlparam_register("pool", psc_ctlparam_pool);
 	psc_ctlparam_register("rlim", psc_ctlparam_rlim);
 	psc_ctlparam_register("run", psc_ctlparam_run);
+	psc_ctlparam_register("rusage", psc_ctlparam_rusage);
 
 	psc_ctlparam_register_simple("reclaim.xid",
 	    slictlparam_reclaim_xid_get, NULL);
