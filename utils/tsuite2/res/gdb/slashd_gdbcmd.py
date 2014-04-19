4576ee6bcf2aad65cd8f5ebaf0635b85159d6260
Tim Becker <beckert@psc.edu>
2014-04-19 02:09:14 +0000

    slashd python gdbcmd file
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@23600 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- utils/tsuite2/res/gdb/slashd_gdbcmd.py
+++ utils/tsuite2/res/gdb/slashd_gdbcmd.py
@@ -1,6 +1,14 @@
 import gdb
 
-def resource_handler(event):
-  print "yay signal: {0}".format(event.stop_signal)
+def check_resource_usage(event):
+  logfile = open("%base%/mds_resource_usage", "a")
+  logfile.write(gdb.execute("info proc status", to_string=True))
+  logfile.close()
+  gdb.execute("c")
 
-gdb.events.stop.connect(resource_handler)
+gdb.execute("set confirm off")
+gdb.execute("set height 0")
+gdb.execute("handle SIGUSR1 ignore")
+gdb.execute("run -S %base%/ctl/slashd.%h.sock -f %base%/slash.conf -D %datadir% -p %zpool_cache% %zpool_name%")
+
+gdb.events.stop.connect(check_resource_usage)
