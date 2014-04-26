f1150677ad4f0012c173fe0a7059f767474290e0
Tim Becker <beckert@psc.edu>
2014-04-26 22:28:10 +0000

    getrusage() isn't fully implemented on linux. Implemented memory usage statistics via gdb for linux
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@23659 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- utils/tsuite2/res/gdb/sliod_gdbcmd.py
+++ utils/tsuite2/res/gdb/sliod_gdbcmd.py
@@ -1,10 +1,18 @@
 import gdb
 
+query_num = 0
 def check_resource_usage(event):
     if isinstance(event, gdb.SignalEvent) and event.stop_signal == "SIGUSR1":
         logfile = open("%base%/mds_resource_usage", "a")
-        logfile.write(gdb.execute("info proc status", to_string=True))
+        logfile.write("Query %d:\n" % query_num)
+        query_num += 1
+
+        rusage = gdb.execute("info proc status", to_string=True)
+        fields = ["VmPeak", "VmSize", "Threads",]
+        [[logfile.write(line) if field in line for field in fields] for line in rusage.split("\n")]
+        logfile.write("%s\n" % ("-"*10))
         logfile.close()
+
         gdb.execute("c")
 
 gdb.execute("set confirm off")
