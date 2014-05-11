9e3203a0b4340d4f087fae90661beb5a1bb8e0b7
Jared Yanovich <yanovich@psc.edu>
2014-05-11 18:27:26 +0000

    fix compilation about RBW*P removal
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@23779 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- msctl/msctl.c
+++ msctl/msctl.c
@@ -817,14 +817,14 @@ ms_biorq_prdat(__unusedx const struct psc_ctlmsghdr *mh, const void *m)
 	const struct msctlmsg_biorq *msr = m;
 
 	printf("%016"SLPRIxFID" %5d %3d %9d %6d "
-	    "%c%c%c%c%c%c%c%c%c%c%c%c%c%c "
+	    "%c%c%c%c%c%c%c%c%c%c%c%c "
 	    "%3d %10s %10"PRId64" %2d %2d\n",
 	    msr->msr_fid, msr->msr_bno, msr->msr_ref, msr->msr_off,
 	    msr->msr_len,
 	    msr->msr_flags & BIORQ_READ			? 'r' : '-',
 	    msr->msr_flags & BIORQ_WRITE		? 'w' : '-',
-	    msr->msr_flags & BIORQ_RBWFP		? 'f' : '-',
-	    msr->msr_flags & BIORQ_RBWLP		? 'l' : '-',
+//	    msr->msr_flags & BIORQ_RBWFP		? 'f' : '-',
+//	    msr->msr_flags & BIORQ_RBWLP		? 'l' : '-',
 	    msr->msr_flags & BIORQ_SCHED		? 's' : '-',
 	    msr->msr_flags & BIORQ_DIO			? 'd' : '-',
 	    msr->msr_flags & BIORQ_FORCE_EXPIRE		? 'x' : '-',
