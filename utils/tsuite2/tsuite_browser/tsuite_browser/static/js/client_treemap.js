9b8ce08c4fae0dcd8779390555f9a0aa33b67edc
Chris Ganas <ganas@psc.edu>
2014-05-11 06:34:42 +0000

    treemap logic outlined, working on overall test table.
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@23778 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- utils/tsuite2/tsuite_browser/tsuite_browser/static/js/client_treemap.js
+++ utils/tsuite2/tsuite_browser/tsuite_browser/static/js/client_treemap.js
@@ -1,23 +1,165 @@
-var test_data = [('localhost', {'tests': [{'setup': {'pass': True, 'error': None}, 'operate': {'pass': True, 'elapsed': 1.5020370483398438e-05, 'error': None}, 'name': 'sl2_test1', 'resource_usage': {}, 'cleanup': {'pass': True, 'error': None}}, {'setup': {'pass': True, 'error': None}, 'operate': {'pass': True, 'elapsed': 1.2002780437469482, 'error': None}, 'name': 'sl2_test2', 'resource_usage': {}, 'cleanup': {'pass': True, 'error': None}}]}), ('127.0.0.1', {'tests': [{'setup': {'pass': True, 'error': None}, 'operate': {'pass': True, 'elapsed': 1.5020370483398438e-05, 'error': None}, 'name': 'sl2_test1', 'resource_usage': {}, 'cleanup': {'pass': True, 'error': None}}, {'setup': {'pass': True, 'error': None}, 'operate': {'pass': True, 'elapsed': 1.2002780437469482, 'error': None}, 'name': 'sl2_test2', 'resource_usage': {}, 'cleanup': {'pass': True, 'error': None}}]})]
-
-
+var test_data ={
+ "zero": [
+  {
+   "client": "localhost",
+   "result": {
+    "resource_usage": {},
+    "name": "zero",
+    "operate": {
+     "elapsed": 0.8008420467376709,
+     "error": null,
+     "pass": true
+    }
+   }
+  },
+  {
+   "client": "127.0.0.1",
+   "result": {
+    "resource_usage": {},
+    "name": "zero",
+    "operate": {
+     "elapsed": 0.8008420467376709,
+     "error": null,
+     "pass": true
+    }
+   }
+  }
+ ],
+ "read": [
+  {
+   "client": "localhost",
+   "result": {
+    "resource_usage": {},
+    "name": "read",
+    "operate": {
+     "elapsed": 2.0020549297332764,
+     "error": null,
+     "pass": true
+    }
+   }
+  },
+  {
+   "client": "127.0.0.1",
+   "result": {
+    "resource_usage": {},
+    "name": "read",
+    "operate": {
+     "elapsed": 2.0020549297332764,
+     "error": null,
+     "pass": true
+    }
+   }
+  }
+ ],
+ "buffer": [
+  {
+   "client": "localhost",
+   "result": {
+    "resource_usage": {},
+    "name": "buffer",
+    "operate": {
+     "elapsed": 1.2012419700622559,
+     "error": null,
+     "pass": true
+    }
+   }
+  },
+  {
+   "client": "127.0.0.1",
+   "result": {
+    "resource_usage": {},
+    "name": "buffer",
+    "operate": {
+     "elapsed": 1.2012419700622559,
+     "error": null,
+     "pass": true
+    }
+   }
+  }
+ ],
+ "write": [
+  {
+   "client": "localhost",
+   "result": {
+    "resource_usage": {},
+    "name": "write",
+    "operate": {
+     "elapsed": 1.201253890991211,
+     "error": null,
+     "pass": true
+    }
+   }
+  },
+  {
+   "client": "127.0.0.1",
+   "result": {
+    "resource_usage": {},
+    "name": "write",
+    "operate": {
+     "elapsed": 1.201253890991211,
+     "error": null,
+     "pass": true
+    }
+   }
+  }
+ ]
+}
+ 
 google.load("visualization", "1", {packages:["treemap"]});
-      google.setOnLoadCallback(drawChart);
-      function drawChart() {
-        // Create and populate the data table.
-        var data = google.visualization.arrayToDataTable([
-          ['Test', 'Parent', 'Market trade volume (size)', 'Market increase/decrease (color)'],
-          ['Ran Tests',    null,                 0,                               0],
-          ['Zaire',     'Africa',             8,                               10]
-        ]);
 
-        // Create and draw the visualization.
-        var tree = new google.visualization.TreeMap(document.getElementById('chart_div'));
-        tree.draw(data, {
-          minColor: '#f00',
-          midColor: '#ddd',
-          maxColor: '#0d0',
-          headerHeight: 15,
-          fontColor: 'black',
-          showScale: true});
-        }
+function get_test_size(results) {
+  var sum = _.reduce(results, function(memo, next) {
+    return memo + next.result.operate.elapsed;
+  }, 0);
+
+  return sum;
+}
+
+function get_test_color(results) {
+  var sum = _.reduce(results, function(memo, next) {
+    return memo + next.result.operate.elapsed;
+  }, 0);
+
+  return sum;
+}
+
+function get_average_elapsed(results) {
+  return _.reduce(results, function(memo, next) {
+    return memo + next.result.operate.elapsed;
+  }, 0) / results.length;
+}
+
+$(function() {
+
+  var data = [
+    ['Test', 'Parent', 'Time (size)', 'Time (color)'],
+    ['Tests', null, 0, 0]
+  ];
+  
+  _.map(test_data, function(results, test_name) {
+    var size = get_test_size(results);
+    var color = get_test_color(results);
+    
+    data.push([test_name, "Tests", size, color]);
+
+    _.each(results, function(test) {
+      console.log(test);
+      data.push([test_name+"@"+test.client, test_name, test.result.operate.elapsed, test.result.operate.elapsed]);
+    });
+  });
+  
+  // Create and draw the visualization.
+  var tree = new google.visualization.TreeMap($("#client_treemap")[0]);
+  tree.draw(google.visualization.arrayToDataTable(data), {
+    minColor: '#f00',
+    midColor: '#ddd',
+    maxColor: '#0d0',
+    headerHeight: 15,
+    fontColor: 'black',
+    title: "Test Results",
+    showScale: false,
+    useWeightedAverageForAggregation: true
+  });
+
+
+});
