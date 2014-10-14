7ecae104c01938a539345f85da297ac64f00847f
Zhihui Zhang <zhihui@psc.edu>
2014-10-14 19:28:43 +0000

    bump the number of read-ahead threads from 1 to 2.
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@24482 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- mount_slash/bmap_cli.h
+++ mount_slash/bmap_cli.h
@@ -40,7 +40,7 @@
 
 #define NUM_ATTR_FLUSH_THREADS		1
 
-#define NUM_READAHEAD_THREADS		1
+#define NUM_READAHEAD_THREADS		2
 
 /**
  * bmap_cli_data - private data associated with a bmap used by a SLASH2 client
