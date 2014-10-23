f5260912f5a617ea09b3bbe0f6c2480639bebf09
Zhihui Zhang <zhihui@psc.edu>
2014-10-23 16:11:19 +0000

    remove debug bits
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@24589 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- mount_slash/bmap_cli.c
+++ mount_slash/bmap_cli.c
@@ -686,8 +686,6 @@ msl_bmap_reap_init(struct bmap *b, const struct srt_bmapdesc *sbd)
 	}
 	bmap_op_start_type(b, BMAP_OPCNT_REAPER);
 
-	psc_assert(b->bcm_flags & BMAP_DIO);
-
 	BMAP_URLOCK(b, locked);
 
 	DEBUG_BMAP(PLL_INFO, b,
