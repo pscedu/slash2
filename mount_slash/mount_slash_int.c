4c0cdd4452463c577393246c5a860d521e430fff
Zhihui Zhang <zhihui@psc.edu>
2014-11-07 18:28:24 +0000

    tweak log statement
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@24685 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- mount_slash/mount_slash_int.c
+++ mount_slash/mount_slash_int.c
@@ -1073,7 +1073,6 @@ msl_pages_dio_getput(struct bmpc_ioreq *r)
 		    &msl_diowr_stat : &msl_diord_stat, size);
 
 	if (rc == -SLERR_AIOWAIT) {
-		DEBUG_BIORQ(PLL_DIAG, r, "aio op=%d", op);
 		rc = 0;
 		if (op == SRMT_WRITE) {
 			q = r->biorq_fsrqi;
@@ -1111,6 +1110,8 @@ msl_pages_dio_getput(struct bmpc_ioreq *r)
 		sl_csvc_decref(csvc);
 
 	PSCFREE(iovs);
+
+	DEBUG_BIORQ(PLL_DIAG, r, "rc=%d", rc);
 	return (rc);
 }
 
