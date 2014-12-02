0a624f5d0e2c646b3163992c324fda5ccc808fa6
Jared Yanovich <yanovich@psc.edu>
2014-12-02 06:05:00 +0000

    spacing
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@24859 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- utils/tsuite2/slash2_tests/citrus_tests/basic_write.py
+++ utils/tsuite2/slash2_tests/citrus_tests/basic_write.py
@@ -5,8 +5,8 @@ test_file = ""
 test_env = {
     host:"adamantium",
     env: {
-        "SLASH_MDS_NID": "mds0@tsuite",
-        "SLASH2_PIOS_ID" : "io1@tsuite"
+	"SLASH_MDS_NID": "mds0@tsuite",
+	"SLASH2_PIOS_ID" : "io1@tsuite"
     }
 }
 
@@ -21,10 +21,10 @@ def setup(directory):
     results["pass"] = True
 
     try:
-        open(test_file, 'a').close()
+	open(test_file, 'a').close()
     except:
-        results["pass"] = False
-        results["error"] = 'Could not create file'
+	results["pass"] = False
+	results["error"] = 'Could not create file'
 
     return results
 
@@ -35,25 +35,25 @@ def operate(max_size=3):
     data = ''.join([chr(a + 97) for a in xrange(26)]) # a-z
 
     try:
-        # write data
-        f = open(test_file, 'w')
-        f.write(data)
-        f.close()
+	# write data
+	f = open(test_file, 'w')
+	f.write(data)
+	f.close()
     except:
-        results["pass"] = False
-        results["error"] = 'Could not write to file'
+	results["pass"] = False
+	results["error"] = 'Could not write to file'
 
     try:
-        #read data
-        f = open(test_file, 'r')
-        read_data = f.read(len(data))
+	#read data
+	f = open(test_file, 'r')
+	read_data = f.read(len(data))
 
-        #verify that data is there and at end of file
-        results["pass"] = read_data == data and len(f.read(1)) == 0
-        f.close()
+	#verify that data is there and at end of file
+	results["pass"] = read_data == data and len(f.read(1)) == 0
+	f.close()
     except:
-        results["pass"] = False
-        results["error"] = 'Could not read from file'
+	results["pass"] = False
+	results["error"] = 'Could not read from file'
 
     return results
 
@@ -64,10 +64,10 @@ def cleanup():
     results["pass"] = True
 
     try:
-        os.remove(test_file)
+	os.remove(test_file)
     except:
-        results["pass"] = False
-        results["error"] = 'Could not remove file'
+	results["pass"] = False
+	results["error"] = 'Could not remove file'
 
     return results
 
