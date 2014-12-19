2b9928718f7970a4f7a63d8d3890aa90c2346c55
Jared Yanovich <yanovich@psc.edu>
2014-12-19 01:31:05 +0000

    fix
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@25025 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- slashd/rmc.c
+++ slashd/rmc.c
@@ -1400,8 +1400,7 @@ slm_rmc_handle_statfs(struct pscrpc_request *rq)
 		mp->rc = -SLERR_RES_UNKNOWN;
 		return (0);
 	}
-	mp->ssfb.sf_bsize = 0;
-	mp->ssfb.sf_iosize = 0;
+	mp->ssfb.sf_frsize = 0;
 	mp->ssfb.sf_blocks = 0;
 	mp->ssfb.sf_bfree = 0;
 	mp->ssfb.sf_bavail = 0;
