21a9da3cdc59b5d3ace3bf5cb0c8544170770819
Zhihui Zhang <zhihui@psc.edu>
2015-01-15 21:01:41 +0000

    well, it actually works fine.
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@25223 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- mount_slash/fidc_cli.c
+++ mount_slash/fidc_cli.c
@@ -96,7 +96,7 @@ slc_fcmh_setattrf(struct fidc_membh *f, struct srt_stat *sstb, int flags)
 		flags &= ~FCMH_SETATTRF_SAVELOCAL;
 
 	psc_assert(sstb->sst_gen != FGEN_ANY);
-#if 0
+#if 1
 	/* XXX FixMe: broken by global mount */
 	psc_assert(f->fcmh_fg.fg_fid == sstb->sst_fid);
 #endif
