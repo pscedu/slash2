49935afef929d06e1d688a128f1f1935beb6437a
Jared Yanovich <yanovich@psc.edu>
2015-02-05 05:30:38 +0000

    missed
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@25368 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

---
+++ mk/pre.mk
@@ -0,0 +1,33 @@
+# $Id$
+# %PSCGPL_COPYRIGHT%
+
+ifndef INCL_SLASH2_PRE_MK
+INCL_SLASH2_PRE_MK=1
+
+INCLUDES+=		-I${SLASH_BASE}/include
+INCLUDES+=		-I${SLASH_BASE}
+INCLUDES+=		${SQLITE3_INCLUDES}
+
+DEFINES+=		-DSL_STK_VERSION=$$(svn info | awk '{ if ($$0 ~ /^Revision: /) print $$2 }')
+SRCS+=			${SLASH_BASE}/share/slerr.c
+
+SRC_PATH+=		${SLASH_BASE}/include
+SRC_PATH+=		${SLASH_BASE}/mount_slash
+SRC_PATH+=		${SLASH_BASE}/share
+SRC_PATH+=		${SLASH_BASE}/slashd
+SRC_PATH+=		${SLASH_BASE}/sliod
+
+SLASH_MODULES?=		cli ion mds
+
+-include ${SLASH_BASE}/mk/local.mk
+
+ifeq (${CURDIR},$(realpath ${SLASH_BASE}/mount_slash))
+ ifneq ($(filter acl,${SLASH_OPTIONS}),)
+  SRCS+=		${SLASH_BASE}/mount_slash/acl_cli.c
+  DEFINES+=		-DSLOPT_POSIX_ACLS
+  DEFINES+=		-DSLOPT_POSIX_ACLS_REVERT
+  LDFLAGS+=		-lacl
+ endif
+endif
+
+endif
