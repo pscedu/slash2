98d0a3d50328ecc1f20687b5b4d6114a3ec11749
Jared Yanovich <yanovich@psc.edu>
2015-03-03 01:06:33 +0000

    copyright
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@25504 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- mk/main.mk
+++ mk/main.mk
@@ -1,5 +1,24 @@
 # $Id$
-# %PSCGPL_COPYRIGHT%
+# %PSCGPL_START_COPYRIGHT%
+# -----------------------------------------------------------------------------
+# Copyright (c) 2007-2015, Pittsburgh Supercomputing Center (PSC).
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License contained in the file
+# `COPYING-GPL' at the top of this distribution or at
+# https://www.gnu.org/licenses/gpl-2.0.html for more details.
+#
+# Pittsburgh Supercomputing Center	phone: 412.268.4960  fax: 412.268.5832
+# 300 S. Craig Street			e-mail: remarks@psc.edu
+# Pittsburgh, PA 15213			web: http://www.psc.edu/
+# -----------------------------------------------------------------------------
+# %PSC_END_COPYRIGHT%
 
 include ${SLASH_BASE}/mk/pre.mk
 include ${MAINMK}
