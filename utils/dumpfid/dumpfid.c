a3fdd76639d4505ba081187d015f72124c86b933
Jared Yanovich <yanovich@psc.edu>
2015-03-06 21:11:17 +0000

    revamp without threading and use processes because NOCHDIR is too expensive
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@25545 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- utils/dumpfid/dumpfid.c
+++ utils/dumpfid/dumpfid.c
@@ -22,12 +22,13 @@
  * %PSC_END_COPYRIGHT%
  */
 
-#include <sys/param.h>
 #include <sys/uio.h>
 #include <sys/xattr.h>
+#include <sys/wait.h>
 
 #include <err.h>
 #include <fcntl.h>
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -58,18 +59,12 @@ struct host {
 	struct psc_listentry	 h_lentry;
 };
 
-struct thr {
-	FILE			*t_fp;
-	struct host		*t_host;
-	char			 t_buf[512 * 1024];
-};
-
 #define STAT_SZ		(8 + sizeof(struct srt_stat))
 #define INOSTAT_SZ	(STAT_SZ + sizeof(struct slash_inode_od) + 8)
 #define INOXSTAT_SZ	(INOSTAT_SZ + sizeof(struct slash_inode_extras_od) + 8)
 
 struct file {
-	char			*f_fn;
+	const char		*f_fn;
 	struct psc_listentry	 f_lentry;
 	int			 f_fd;
 	uint32_t		 f_nrepls;
@@ -91,12 +86,14 @@ struct file {
 #define f_inox_od_crc	f_data.inox_crc
 };
 
-struct psc_lockedlist		 hosts = PLL_INIT(&hosts, struct host, h_lentry);
-int				 recurse;
-pthread_barrier_t		 barrier;
-struct psc_lockedlist		 excludes = PLL_INIT(&excludes, struct path, p_lentry);
-const char			*outfn;
-const char			*display_fmt =
+struct psc_lockedlist		 df_hosts = PLL_INIT(&df_hosts, struct host, h_lentry);
+int				 df_rank;
+int				 df_nprocs = 1;
+struct psc_lockedlist		 df_excludes = PLL_INIT(&df_excludes, struct path, p_lentry);
+const char			*df_outfn;
+FILE				*df_outfp;
+char				 df_buf[512 * 1024];
+const char			*df_dispfmt =
     "%f:\n"
     "  crc %d mem %C od %c\n"
     "  version %v\n"
@@ -111,12 +108,6 @@ const char			*display_fmt =
     "  replblks %B\n"
     "  xcrc %y mem %X od %x\n"
     "  bmaps %N\n%M";
-
-struct psc_listcache		 files;
-struct psc_poolmaster		 files_poolmaster;
-struct psc_poolmgr		*files_pool;
-
-const char			*progname;
 
 const char			*repl_states = "-sq+tpgx";
 
@@ -204,7 +195,7 @@ pr_repl_blks(FILE *outfp, struct file *f)
 }
 
 void
-pr_bmaps(struct thr *t, FILE *outfp, struct file *f)
+pr_bmaps(FILE *outfp, struct file *f)
 {
 	struct {
 		struct bmap_ondisk bod;
@@ -234,7 +225,6 @@ pr_bmaps(struct thr *t, FILE *outfp, struct file *f)
 		close(fd);
 		return;
 	}
-	setbuffer(fp, t->t_buf, sizeof(t->t_buf));
 
 	for (bno = 0; ; bno++) {
 		rc = fread(&bd, 1, sizeof(bd), fp);
@@ -263,33 +253,6 @@ pr_bmaps(struct thr *t, FILE *outfp, struct file *f)
 	fclose(fp);
 }
 
-int
-queue(const char *fn, __unusedx const struct stat *stb, int ftyp,
-    __unusedx int level, __unusedx void *arg)
-{
-	struct path *p;
-	struct file *f;
-
-	p = searchpaths(&excludes, fn);
-	if (p) {
-		pll_remove(&excludes, p);
-		PSCFREE(p);
-		return (PFL_FILEWALK_RC_SKIP);
-	}
-
-	if (ftyp != PFWT_F &&
-	    ftyp != PFWT_D)
-		return (0);
-
-	f = psc_pool_get(files_pool);
-	memset(f, 0, sizeof(*f));
-	f->f_fd = -1;
-	INIT_PSC_LISTENTRY(&f->f_lentry);
-	f->f_fn = pfl_strdup(fn);
-	lc_add(&files, f);
-	return (0);
-}
-
 void
 addexclude(const char *fn)
 {
@@ -298,7 +261,7 @@ addexclude(const char *fn)
 	p = PSCALLOC(sizeof(*p));
 	INIT_PSC_LISTENTRY(&p->p_lentry);
 	p->p_fn = fn;
-	pll_add(&excludes, p);
+	pll_add(&df_excludes, p);
 }
 
 int
@@ -350,87 +313,89 @@ load_data_stat(struct file *f, void *buf)
 int (*load_data)(struct file *f, void *) = load_data_stat;
 
 void
-thrmain(struct psc_thread *thr)
+df_setup(void)
 {
-	char modebuf[16];
+	if (df_outfn) {
+		char fn[PATH_MAX];
+
+		(void)FMTSTR(fn, sizeof(fn), df_outfn,
+		    FMTSTRCASE('n', "d", df_rank)
+		);
+		df_outfp = fopen(fn, "w");
+		if (df_outfp == NULL)
+			err(1, "open %s", fn);
+	} else
+		df_outfp = stdout;
+}
+
+int
+dumpfid(const char *fn, __unusedx const struct stat *stb, int ftyp,
+    /*ino_t inum, */ int level, __unusedx void *arg)
+{
+	struct file fil, *f = &fil;
 	struct slm_ino_od *ino;
 	struct srt_stat *sstb;
-	struct file *f;
-	struct thr *t;
+	struct path *p;
+	char modebuf[16];
 
-	t = thr->pscthr_private;
-	if (t->t_host) {
-#if 0
-		switch (fork()) {
-		case -1:
-			break;
-		case 0:
-			//ssh -o 'Compression yes' host
-			execve();
-			err();
-		default:
-			break;
-		}
-#endif
-	} else {
-		if (outfn) {
-			char fn[PATH_MAX];
-
-			(void)FMTSTR(fn, sizeof(fn), outfn,
-			    FMTSTRCASE('n', "s", thr->pscthr_name +
-				strcspn(thr->pscthr_name, "0123456789"))
-			);
-			t->t_fp = fopen(fn, "w");
-			if (t->t_fp == NULL)
-				err(1, "open %s", fn);
-		} else
-			t->t_fp = stdout;
+	p = searchpaths(&df_excludes, fn);
+	if (p) {
+		pll_remove(&df_excludes, p);
+		PSCFREE(p);
+		return (PFL_FILEWALK_RC_SKIP);
 	}
 
-	pthread_barrier_wait(&barrier);
-	while ((f = lc_getwait(&files))) {
-		if (!load_data(f, &f->f_data)) {
-			warn("%s", f->f_fn);
-			goto next;
-		}
-		sstb = &f->f_sstb;
-		ino = &f->f_ino;
-		f->f_nrepls = MIN(SL_MAX_REPLICAS, ino->ino_nrepls);
-		(void)PRFMTSTR(t->t_fp, display_fmt,
-		    PRFMTSTRCASEV('B', pr_repl_blks(_fp, f))
-		    PRFMTSTRCASE('b', PRIu64, sstb->sst_blocks)
-		    PRFMTSTRCASE('C', PSCPRIxCRC64, f->f_ino_mem_crc)
-		    PRFMTSTRCASE('c', PSCPRIxCRC64, f->f_ino_od_crc)
-		    PRFMTSTRCASE('d', "s",
-			f->f_ino_od_crc == f->f_ino_mem_crc ? "OK" : "BAD")
-		    PRFMTSTRCASE('F', PRIx64, sstb->sst_fg.fg_fid)
-		    PRFMTSTRCASE('f', "s", f->f_fn)
-		    PRFMTSTRCASE('G', PRIu64, sstb->sst_fg.fg_gen)
-		    PRFMTSTRCASE('g', "u", sstb->sst_gid)
-		    PRFMTSTRCASE('L', "#x", ino->ino_flags)
-		    PRFMTSTRCASEV('M', pr_bmaps(t, _fp, f))
-		    PRFMTSTRCASE('m', "s", pfl_fmt_mode(sstb->sst_mode,
-			modebuf))
-		    PRFMTSTRCASE('N', PSCPRIdOFFT,
-			(f->f_metasize - SL_BMAP_START_OFF) / BMAP_OD_SZ)
-		    PRFMTSTRCASE('n', "u", ino->ino_nrepls)
-		    PRFMTSTRCASEV('R', pr_repls(_fp, f))
-		    PRFMTSTRCASEV('T', pr_times(&_t, _fp, f))
-		    PRFMTSTRCASE('s', PRIu64, sstb->sst_size)
-		    PRFMTSTRCASE('u', "u", sstb->sst_uid)
-		    PRFMTSTRCASE('v', "u", ino->ino_version)
-		    PRFMTSTRCASE('X', PSCPRIxCRC64, f->f_inox_mem_crc)
-		    PRFMTSTRCASE('x', PSCPRIxCRC64, f->f_inox_od_crc)
-		    PRFMTSTRCASE('y', "s",
-			f->f_inox_mem_crc == f->f_inox_od_crc ? "OK" : "BAD")
-		);
+	if (ftyp != PFWT_F &&
+	    ftyp != PFWT_D)
+		return (0);
 
- next:
-		if (f->f_fd != -1)
-			close(f->f_fd);
-		PSCFREE(f->f_fn);
-		psc_pool_return(files_pool, f);
+//	if (df_rank != (int)(inum % df_nprocs))
+//		return (level < 5 ? 0 : PFL_FILEWALK_RC_SKIP);
+
+	memset(f, 0, sizeof(*f));
+	f->f_fd = -1;
+	INIT_PSC_LISTENTRY(&f->f_lentry);
+	f->f_fn = fn;
+
+	if (!load_data(f, &f->f_data)) {
+		warn("%s", f->f_fn);
+		return (0);
 	}
+	sstb = &f->f_sstb;
+	ino = &f->f_ino;
+	f->f_nrepls = MIN(SL_MAX_REPLICAS, ino->ino_nrepls);
+	(void)PRFMTSTR(df_outfp, df_dispfmt,
+	    PRFMTSTRCASEV('B', pr_repl_blks(_fp, f))
+	    PRFMTSTRCASE('b', PRIu64, sstb->sst_blocks)
+	    PRFMTSTRCASE('C', PSCPRIxCRC64, f->f_ino_mem_crc)
+	    PRFMTSTRCASE('c', PSCPRIxCRC64, f->f_ino_od_crc)
+	    PRFMTSTRCASE('d', "s",
+		f->f_ino_od_crc == f->f_ino_mem_crc ? "OK" : "BAD")
+	    PRFMTSTRCASE('F', PRIx64, sstb->sst_fg.fg_fid)
+	    PRFMTSTRCASE('f', "s", f->f_fn)
+	    PRFMTSTRCASE('G', PRIu64, sstb->sst_fg.fg_gen)
+	    PRFMTSTRCASE('g', "u", sstb->sst_gid)
+	    PRFMTSTRCASE('L', "#x", ino->ino_flags)
+	    PRFMTSTRCASEV('M', pr_bmaps(_fp, f))
+	    PRFMTSTRCASE('m', "s", pfl_fmt_mode(sstb->sst_mode,
+		modebuf))
+	    PRFMTSTRCASE('N', PSCPRIdOFFT,
+		(f->f_metasize - SL_BMAP_START_OFF) / BMAP_OD_SZ)
+	    PRFMTSTRCASE('n', "u", ino->ino_nrepls)
+	    PRFMTSTRCASEV('R', pr_repls(_fp, f))
+	    PRFMTSTRCASEV('T', pr_times(&_t, _fp, f))
+	    PRFMTSTRCASE('s', PRIu64, sstb->sst_size)
+	    PRFMTSTRCASE('u', "u", sstb->sst_uid)
+	    PRFMTSTRCASE('v', "u", ino->ino_version)
+	    PRFMTSTRCASE('X', PSCPRIxCRC64, f->f_inox_mem_crc)
+	    PRFMTSTRCASE('x', PSCPRIxCRC64, f->f_inox_od_crc)
+	    PRFMTSTRCASE('y', "s",
+		f->f_inox_mem_crc == f->f_inox_od_crc ? "OK" : "BAD")
+	);
+
+	if (f->f_fd != -1)
+		close(f->f_fd);
+	return (0);
 }
 
 void
@@ -451,17 +416,19 @@ addhost(char *s)
 		INIT_LISTENTRY(&h->h_lentry);
 		h->h_hostname = s;
 		h->h_path = path;
-		pll_add(&hosts, h);
+		pll_add(&df_hosts, h);
 	}
 }
 
 __dead void
 usage(void)
 {
+	extern const char *__progname;
+
 	fprintf(stderr,
 	    "usage: %s [-R] [-C hosts] [-F fmt] [-O file]\n"
 	    "\t[-t nthr] [-x exclude] file ...\n",
-	    progname);
+	    __progname);
 	exit(1);
 }
 
@@ -472,26 +439,23 @@ usage(void)
 int
 main(int argc, char *argv[])
 {
-	int walkflags = PFL_FILEWALKF_RELPATH, c, i, nthr = 1, need = 0;
-	struct psc_thread **thrv, *it;
-	struct host *h;
-	struct thr *t;
+	int pid, status, walkflags, c, i, need = 0;
 	char *endp;
 	FILE *fp;
 	long l;
 
 	pfl_init();
-	progname = argv[0];
+	walkflags = PFL_FILEWALKF_RELPATH | PFL_FILEWALKF_NOSTAT;
 	while ((c = getopt(argc, argv, "C:F:O:Rt:x:")) != -1) {
 		switch (c) {
 		case 'C':
 			addhost(optarg);
 			break;
 		case 'F':
-			display_fmt = optarg;
+			df_dispfmt = optarg;
 			break;
 		case 'O':
-			outfn = optarg;
+			df_outfn = optarg;
 			break;
 		case 'R':
 			walkflags |= PFL_FILEWALKF_RECURSIVE;
@@ -501,7 +465,7 @@ main(int argc, char *argv[])
 			if (l < 1 || l > 256 ||
 			    optarg == endp || *endp)
 				errx(1, "invalid -t value: %s", optarg);
-			nthr = l;
+			df_nprocs = l;
 			break;
 		case 'x':
 			addexclude(optarg);
@@ -518,7 +482,7 @@ main(int argc, char *argv[])
 	fp = fopen("/dev/null", "w");
 	if (fp == NULL)
 		err(1, "/dev/null");
-	(void)PRFMTSTR(fp, display_fmt,
+	(void)PRFMTSTR(fp, df_dispfmt,
 	    PRFMTSTRCASEV('B', need |= NEED_INOX)
 	    PRFMTSTRCASEV('b', )
 	    PRFMTSTRCASEV('C', need |= NEED_INO)
@@ -551,31 +515,44 @@ main(int argc, char *argv[])
 	else if (need & NEED_INO)
 		load_data = load_data_ino;
 
-	psc_poolmaster_init(&files_poolmaster, struct file, f_lentry, 0,
-	    nthr * 4, nthr * 4, nthr * 4, NULL, NULL, NULL, "files",
-	    NULL);
-	files_pool = psc_poolmaster_getmgr(&files_poolmaster);
-
-	pthread_barrier_init(&barrier, NULL, nthr + 1);
-	lc_init(&files, struct file, f_lentry);
-	thrv = PSCALLOC((nthr + pll_nitems(&hosts)) * sizeof(*thrv));
-	for (i = 0; i < nthr; i++) {
-		it = thrv[i] = pscthr_init(0, thrmain, NULL, sizeof(*t),
-		    "thr%d", i);
-		pscthr_setready(it);
+	for (i = 1; i < df_nprocs; i++) {
+		pid = fork();
+		switch (pid) {
+		case -1:
+			err(1, "fork");
+		case 0: /* child */
+			df_setup();
+			pfl_filewalk(*argv, walkflags, NULL, dumpfid,
+			    NULL);
+			exit(0);
+			break;
+		}
 	}
-	PLL_FOREACH(h, &hosts) {
-		it = thrv[i++] = pscthr_init(0, thrmain, NULL,
-		    sizeof(*t), "thr-%s", h->h_hostname);
-		t = it->pscthr_private;
-		t->t_host = h;
-		pscthr_setready(it);
-	}
-	pthread_barrier_wait(&barrier);
-	for (; *argv; argv++)
-		pfl_filewalk(*argv, walkflags, NULL, queue, NULL);
-	lc_kill(&files);
-	for (i = 0; i < nthr + pll_nitems(&hosts); i++)
-		pthread_join(thrv[i]->pscthr_pthread, NULL);
+	df_setup();
+	pfl_filewalk(*argv, walkflags, NULL, dumpfid, NULL);
+	while (wait(&status) == 0)
+		;
+	if (errno != ECHILD)
+		err(1, "wait");
 	exit(0);
+
+//	PLL_FOREACH(h, &hosts) {
+//		it = thrv[i++] = pscthr_init(0, thrmain, NULL,
+//		    sizeof(*t), "thr-%s", h->h_hostname);
+//		t = it->pscthr_private;
+//		t->t_host = h;
+//		pscthr_setready(it);
+#if 0
+		switch (fork()) {
+		case -1:
+			break;
+		case 0:
+			//ssh -o 'Compression yes' host
+			execve();
+			err();
+		default:
+			break;
+		}
+#endif
+//	}
 }
