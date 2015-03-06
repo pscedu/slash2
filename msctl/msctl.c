e604aad39940911740a4d3d9f9cce70034c9041b
Jared Yanovich <yanovich@psc.edu>
2015-03-06 04:50:43 +0000

    sync
    git-svn-id: svn+ssh://frodo/cluster/svn/projects/slash_nara@25533 3eda493b-6a19-0410-b2e0-ec8ea4dd8fda

--- msctl/msctl.c
+++ msctl/msctl.c
@@ -824,13 +824,12 @@ ms_biorq_prdat(__unusedx const struct psc_ctlmsghdr *mh, const void *m)
 	const struct msctlmsg_biorq *msr = m;
 
 	printf("%016"SLPRIxFID" %5d %3d %9d %6d     "
-	    "%c%c%c%c%c%c%c%c "
+	    "%c%c%c%c%c%c%c "
 	    "%3d %10s %10"PRId64" %2d %2d 0x%#lx\n",
 	    msr->msr_fid, msr->msr_bno, msr->msr_ref, msr->msr_off,
 	    msr->msr_len,
 	    msr->msr_flags & BIORQ_READ			? 'r' : '-',
 	    msr->msr_flags & BIORQ_WRITE		? 'w' : '-',
-	    msr->msr_flags & BIORQ_SCHED		? 's' : '-',
 	    msr->msr_flags & BIORQ_DIO			? 'd' : '-',
 	    msr->msr_flags & BIORQ_EXPIRE		? 'x' : '-',
 	    msr->msr_flags & BIORQ_DESTROY		? 'D' : '-',
