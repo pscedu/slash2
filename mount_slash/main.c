41e0be1ff49b06542930942b19a8ff9b573f3ed9
Jared Yanovich <slovichon@gmail.com>
2015-04-16 00:25:49 -0400

    spacing

--- mount_slash/main.c
+++ mount_slash/main.c
@@ -1488,7 +1488,7 @@ msl_readdir_finish(struct fidc_membh *d, struct dircache_page *p,
 		if (e->sstb.sst_fid == FID_ANY ||
 		    e->sstb.sst_fid == 0) {
 			DEBUG_SSTB(PLL_WARN, &e->sstb,
-			    "invalid readdir prefetch FID ent=%d"
+			    "invalid readdir prefetch FID ent=%d "
 			    "parent@%p="SLPRI_FID, i, d, fcmh_2_fid(d));
 			continue;
 		}
