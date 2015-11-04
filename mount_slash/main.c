f41419f8ac5cdc26ecb9ae2c7d565b5ee83df566
Jared Yanovich <jaredy@google.com>
2015-11-04 12:38:15 -0500

    more improved destruction handling

--- mount_slash/main.c
+++ mount_slash/main.c
@@ -3025,7 +3025,6 @@ mslfsop_destroy(__unusedx struct pscfs_req *pfr)
 	struct sl_site *s;
 	int i, j;
 
-	pfl_wkthr_killall();
 	lc_kill(&slc_bmapflushq);
 	lc_kill(&slc_bmaptimeoutq);
 	lc_kill(&slc_attrtimeoutq);
