45d06b3036d2d1ad32c602971f0cb9b637461394
Jared Yanovich <jaredy@google.com>
2015-11-06 20:39:58 -0500

    copyright

--- slashd/mdslog.h
+++ slashd/mdslog.h
@@ -1,5 +1,23 @@
 /* $Id$ */
-/* %PSC_COPYRIGHT_GPL% */
+/*
+ * %GPL_START_LICENSE%
+ * ---------------------------------------------------------------------
+ * Copyright 2009-2015, Pittsburgh Supercomputing Center
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License contained in the file
+ * `COPYING-GPL' at the top of this distribution or at
+ * https://www.gnu.org/licenses/gpl-2.0.html for more details.
+ * ---------------------------------------------------------------------
+ * %END_LICENSE%
+ */
 
 #ifndef _MDSLOG_H_
 #define _MDSLOG_H_
