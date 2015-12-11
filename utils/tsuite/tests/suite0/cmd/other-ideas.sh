15a40bc8d920261b96d66395a5f2ce6a46f6b81e
Jared Yanovich <jaredy@google.com>
2015-12-11 14:11:52 -0500

    more

--- utils/tsuite/tests/suite0/cmd/other-ideas.sh
+++ utils/tsuite/tests/suite0/cmd/other-ideas.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 # $Id$
 
-fio
-IOR
-bonnie
-postmark
-filebench
-tiobench
-dbench
-OS file system testing suites:
-	linux
-		ext4
-		btrfs
-	netbsd
-	openbsd
-	freebsd
-	zfs
-	lustre testing suite
-torture
+# fio
+# IOR
+# bonnie
+# postmark
+# filebench
+# tiobench
+# dbench
+# OS file system testing suites:
+#	linux
+#		ext4
+#		btrfs
+#	netbsd
+#	openbsd
+#	freebsd
+#	zfs
+#	lustre testing suite
+# torture
+# mysql
