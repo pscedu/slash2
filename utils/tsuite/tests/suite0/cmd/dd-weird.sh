4d46098bec79d783d71c7b6a264e1121ec9decac
Jared Yanovich <jaredy@google.com>
2015-12-28 12:49:24 -0500

    fixes

--- utils/tsuite/tests/suite0/cmd/dd-weird.sh
+++ utils/tsuite/tests/suite0/cmd/dd-weird.sh
@@ -5,5 +5,5 @@
 dd if=$RANDOM_DATA of=r000 seek=1M count=1 bs=64k
 md5sum r000
 
-dd if=$RANDOM_DATA of=r000 seek=1G count=1 bs=64k
-dd if=$RANDOM_DATA of=r000 seek=1M count=1 bs=64k
+dd if=$RANDOM_DATA of=r001 seek=1G count=1 bs=64k
+dd if=$RANDOM_DATA of=r002 seek=1M count=1 bs=64k
