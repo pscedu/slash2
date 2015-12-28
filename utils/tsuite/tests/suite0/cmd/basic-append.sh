399604aa0230477cfb415f62a11512c863a65076
Jared Yanovich <jaredy@google.com>
2015-12-28 00:45:58 -0500

    fix

--- utils/tsuite/tests/suite0/cmd/basic-append.sh
+++ utils/tsuite/tests/suite0/cmd/basic-append.sh
@@ -3,11 +3,11 @@
 [ $1 -eq 0 ] || exit 0
 
 fn=tf
-echo line1 >> $fn
+echo line1 > $fn
 echo line2 >> $fn
 echo line3 >> $fn
 echo line4 >> $fn
-nl=$(wc -l $fn | sed 's/ //g' | awk '{print $2}')
+nl=$(wc -l $fn | awk '{print $1}')
 [ x"$nl" = x"4" ]
 
 diff -q $fn - <<EOF
