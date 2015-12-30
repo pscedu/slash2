e54cb962edfc2b3ecdf03f534ed50aaddc18cce5
Jared Yanovich <jaredy@google.com>
2015-12-30 18:50:47 -0500

    fix

--- utils/tsuite/tests/suite0/cmd/tar-ssh.sh
+++ utils/tsuite/tests/suite0/cmd/tar-ssh.sh
@@ -7,10 +7,10 @@
 dep wget
 
 V=4.3
-dst=$(pwd)/linux-$V.tarssh
+dst=$(pwd)/tarssh
 
 wget -nv https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-$V.tar.xz
 tar fx linux-$V.tar.xz
 mkdir $dst
 tar fc - linux-$V | ssh localhost "tar fCx - $dst"
-diff -r linux-$V $dst
+diff -r linux-$V $dst/linux-$V
