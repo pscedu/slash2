491aa4e30b81e691aacef265f2836e0687aa9b9b
zhihui-slash2 <zhihui@psc.edu>
2016-01-06 16:36:51 -0500

    revert two previous commits

--- slashd/mds.c
+++ slashd/mds.c
@@ -1983,7 +1983,7 @@ void
 slm_setattr_core(struct fidc_membh *f, struct srt_stat *sstb,
     int to_set)
 {
-	int deref = 0, rc = 0;
+	int locked, deref = 0, rc = 0;
 	struct slm_wkdata_ptrunc *wk;
 	struct fcmh_mds_info *fmi;
 
@@ -1996,14 +1996,14 @@ slm_setattr_core(struct fidc_membh *f, struct srt_stat *sstb,
 				    sstb->sst_fid, slstrerror(rc));
 				return;
 			}
-			FCMH_LOCK(f);
 			deref = 1;
 		}
 
-		FCMH_LOCK_ENSURE(f);
+		locked = FCMH_RLOCK(f);
 		f->fcmh_flags |= FCMH_MDS_IN_PTRUNC;
 		fmi = fcmh_2_fmi(f);
 		fmi->fmi_ptrunc_size = sstb->sst_size;
+		FCMH_URLOCK(f, locked);
 
 		wk = pfl_workq_getitem(slm_ptrunc_prepare,
 		    struct slm_wkdata_ptrunc);
