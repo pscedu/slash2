878c581121cb06faf552bc4715365c344f6e9580
zhihui-slash2 <zhihui@psc.edu>
2016-01-08 09:58:39 -0500

    Add trace before we reply_read/write, this will help us detect overlapping read/write requests on the same file.

--- mount_slash/io.c
+++ mount_slash/io.c
@@ -585,6 +585,7 @@ msl_complete_fsrq(struct msl_fsrqinfo *q, size_t len,
 	struct msl_fhent *mfh;
 	struct pscfs_req *pfr;
 	struct bmpc_ioreq *r;
+	struct fidc_membh *f;
 	int i;
 
 	pfr = mfsrq_2_pfr(q);
@@ -668,6 +669,12 @@ msl_complete_fsrq(struct msl_fsrqinfo *q, size_t len,
 		}
 		pscfs_reply_write(pfr, q->mfsrq_len, abs(q->mfsrq_err));
 	}
+
+	f = mfh->mfh_fcmh;
+	DEBUG_FCMH(PLL_DIAG, f, "off=%"PRId64", size=%zu, rw=%s, rc = %d",
+	    q->mfsrq_off, q->mfsrq_len, (q->mfsrq_flags & MFSRQ_READ) ? 
+	    "read" : "write", q->mfsrq_err);
+
 	PSCFREE(oiov);
 
 	for (i = 0; i < MAX_BMAPS_REQ; i++) {
