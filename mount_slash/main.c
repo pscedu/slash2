ee3fcc15a6075e4dda56e9b3ad14ab88edda0125
zhihui-slash2 <zhihui@psc.edu>
2016-01-28 15:48:05 -0500

    Add slash2/readdir fault point

--- mount_slash/main.c
+++ mount_slash/main.c
@@ -1539,6 +1539,7 @@ msl_readdir_issue(struct pscfs_req *pfr, struct fidc_membh *d,
 	fcmh_op_start_type(d, FCMH_OPCNT_READDIR);
 
 	MSL_RMC_NEWREQ(pfr, d, csvc, SRMT_READDIR, rq, mq, mp, rc);
+	(void)pfl_fault_here_rc("slash2/readdir", &rc, EHOSTDOWN);
 	if (rc)
 		PFL_GOTOERR(out2, rc);
 
