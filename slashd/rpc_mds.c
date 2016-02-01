2409bf9d51a0e4dd2fb13cafcc283e6f7e40cd54
zhihui-slash2 <zhihui@psc.edu>
2016-02-01 14:29:51 -0500

    Remove goto err

--- slashd/rpc_mds.c
+++ slashd/rpc_mds.c
@@ -418,14 +418,13 @@ batchrq_send(struct batchrq *br)
 	iov.iov_len = br->br_len;
 	rc = slrpc_bulkclient(rq, BULK_GET_SOURCE, br->br_snd_ptl, &iov,
 	    1);
-	if (rc)
-		goto err;
 
-	rq->rq_interpret_reply = sl_batchrqsend_cb;
-	rq->rq_async_args.pointer_arg[0] = br;
-	rc = SL_NBRQSET_ADD(br->br_csvc, rq);
+	if (!rc) {
+		rq->rq_interpret_reply = sl_batchrqsend_cb;
+		rq->rq_async_args.pointer_arg[0] = br;
+		rc = SL_NBRQSET_ADD(br->br_csvc, rq);
+	}
 	if (rc) {
- err:
 		/*
 		 * If we failed, check again to see if the connection
 		 * has been reestablished since there can be delay in
