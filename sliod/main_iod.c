56b5541e824d457ecaabb1c47c6c177fd63c847b
zhihui-slash2 <zhihui@psc.edu>
2016-02-03 14:20:51 -0500

    always show start time / rev

--- sliod/main_iod.c
+++ sliod/main_iod.c
@@ -330,7 +330,7 @@ main(int argc, char *argv[])
 	OPSTAT_INCR("min-seqno");
 
 	time(&now);
-	psclog_info("IOS revision %d has started at %s", sl_stk_version,
+	psclog_max("IOS revision %d has started at %s", sl_stk_version,
 	    ctime(&now));
 
 	slictlthr_main(sfn);
