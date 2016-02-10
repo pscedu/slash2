caa5f4c497716146eb0d1c58b480f7077d4f6ac7
Jared Yanovich <jaredy@google.com>
2016-02-10 00:22:29 -0500

    typo

---
+++ utils/tsuite/create.sql
@@ -0,0 +1,24 @@
+CREATE TABLE s2ts_run (
+	id		INTEGER		PRIMARY KEY,
+	suite_name	VARCHAR(16),
+	launch_date	TIMESTAMP,
+	user		VARCHAR(32),
+	diff		MEDIUMTEXT,
+	status		INT,
+	output		MEDIUMTEXT,
+	sl2_commid	CHAR(40),
+	pfl_commid	CHAR(40)
+);
+
+CREATE INDEX user ON s2ts_run (user);
+
+CREATE TABLE s2ts_result (
+	id		INTEGER		PRIMARY KEY,
+	run_id		INT		NOT NULL,
+	task_id		INT		NOT NULL,
+	test_name	VARCHAR(32),
+	duration_ms	LONG
+);
+
+CREATE INDEX run_id ON s2ts_result (run_id);
+CREATE INDEX test_name ON s2ts_result (test_name);
