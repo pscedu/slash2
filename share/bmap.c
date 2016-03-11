9058c25d17d0aedf2b17853884faed0e5eb83d1b
zhihui-slash2 <zhihui@psc.edu>
2016-03-11 13:12:01 -0500

    We should NOT send any RPC if BMAPGETF_NORETRIEVE is set. Worse, we should not mark it as LOADED too.

--- share/bmap.c
+++ share/bmap.c
@@ -248,8 +248,11 @@ _bmap_get(const struct pfl_callerinfo *pci, struct fidc_membh *f,
 		goto loaded;
 
 	if (flags & BMAPGETF_NORETRIEVE) {
-		b->bcm_flags |= BMAPF_LOADED;
-		goto loaded;
+		if (b->bcm_flags & BMAPF_LOADED)
+			OPSTAT_INCR("bmap-already-loaded");
+		else
+			OPSTAT_INCR("bmap-notyet-loaded");
+		goto out;
 	}
 
 	b->bcm_flags |= BMAPF_LOADING;
