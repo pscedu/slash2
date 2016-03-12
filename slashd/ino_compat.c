4eaf2979c19d1a59fb4313e604102da335ba6524
zhihui-slash2 <zhihui@psc.edu>
2016-03-11 20:55:39 -0500

    fix an potential crash

--- slashd/ino_compat.c
+++ slashd/ino_compat.c
@@ -58,7 +58,7 @@ mds_inode_dump(int vfsid, struct sl_ino_compat *sic,
 	for (i = 0; ; i++) {
 		fh->fh = readh;
 		rc = bmap_getf(f, i, SL_WRITE, fl, &b);
-		if (sic)
+		if (sic && !rc)
 			b->bcm_flags |= BMAPF_LOADED;
 		fh->fh = th;
 
