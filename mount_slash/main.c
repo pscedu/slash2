10dc044b47d1c93aa1870d27d4b91322a4d1bf20
zhihui-slash2 <zhihui@psc.edu>
2016-05-08 14:17:39 -0400

    take lock to call psc_pool_try_shrink()

--- mount_slash/main.c
+++ mount_slash/main.c
@@ -3764,10 +3764,9 @@ msreapthr_main(struct psc_thread *thr)
 		curr = bmpce_pool->ppm_nfree;
 		POOL_ULOCK(bmpce_pool);
 
-		if (last && curr >= last) 
-			psc_pool_try_shrink(bmpce_pool, curr);
-
 		POOL_LOCK(bmpce_pool);
+		if (last && curr >= last)
+			psc_pool_try_shrink(bmpce_pool, curr);
 		last = bmpce_pool->ppm_nfree;
 		POOL_ULOCK(bmpce_pool);
 
