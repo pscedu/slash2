39a3b39a7be9f6d7cd4c1b0a649d469f746ec14a
zhihui-slash2 <zhihui@psc.edu>
2016-05-13 13:27:26 -0400

    help message

--- slashd/main_mds.c
+++ slashd/main_mds.c
@@ -502,7 +502,7 @@ main(int argc, char *argv[])
 		slm_mdfs_register(vfsid);
 
 	if (!zfs_nmounts)
-		errx(1, "No ZFS file system exists!");
+		errx(1, "No ZFS file system exists! Try zpool import -a.");
 
 	found = 0;
 	for (vfsid = 0; vfsid < zfs_nmounts; vfsid++) {
