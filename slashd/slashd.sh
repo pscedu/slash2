d1a374804fba9bd0efd011eb204b8114cb44bede
zhihui-slash2 <zhihui@psc.edu>
2016-05-17 11:16:32 -0400

    Double the core size limit because of the following:
    -rw-r--r-- 1 root root 54859853888 Apr  6 23:46 slashd.14475.core
    -rw-r--r-- 1 root root 54711836736 Apr 14 02:06 slashd.741.core
    -rw-r--r-- 1 root root 54688157760 Apr 10 01:11 slashd.1839.core

--- slashd/slashd.sh
+++ slashd/slashd.sh
@@ -35,7 +35,7 @@ preinit
 
 # Initialization/configuration
 ulimit -n 100000
-ulimit -c $((1024 * 1024 * 1024 * 50))
+ulimit -c $((1024 * 1024 * 1024 * 100))
 export LD_LIBRARY_PATH=/usr/local/lib
 export PSC_SYSLOG=1
 export PSC_LOG_LEVEL=${PSC_LOG_LEVEL:-notice}
