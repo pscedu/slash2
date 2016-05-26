bb81b0da9efd4f2bc7d3193c07286fe1bc0f7c76
zhihui-slash2 <zhihui@psc.edu>
2016-05-26 13:58:16 -0400

    csvc debug print

--- include/slconn.h
+++ include/slconn.h
@@ -138,10 +138,11 @@ struct slrpc_cservice {
 #define CSVC_PING_INTV		30		/* seconds */
 
 #define DEBUG_CSVC(lvl, csvc, fmt, ...)					\
-	psclog((lvl), "csvc@%p fl=%#x:%s%s%s ref:%d " fmt,		\
+	psclog((lvl), "csvc@%p fl=%#x:%s%s%s%s ref:%d " fmt,		\
 	    (csvc), (csvc)->csvc_flags,					\
 	    (csvc)->csvc_flags & CSVCF_CONNECTING	? "C" : "",	\
 	    (csvc)->csvc_flags & CSVCF_CONNECTED	? "O" : "",	\
+	    (csvc)->csvc_flags & CSVCF_WATCH		? "W" : "",	\
 	    (csvc)->csvc_flags & CSVCF_PING		? "P" : "",	\
 	    (csvc)->csvc_refcnt, ##__VA_ARGS__)
 
