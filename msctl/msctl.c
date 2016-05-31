584da0630db009428c10c4ce139ce2159e37d42e
zhihui-slash2 <zhihui@psc.edu>
2016-05-31 13:42:01 -0400

    adjust ./msctl -s biorq  output

--- msctl/msctl.c
+++ msctl/msctl.c
@@ -816,11 +816,11 @@ replst_savdat(__unusedx struct psc_ctlmsghdr *mh, const void *m)
 int
 ms_biorq_prhdr(__unusedx struct psc_ctlmsghdr *mh, __unusedx const void *m)
 {
-	printf("%-16s %5s %3s %9s %6s "
-	    "%11s %3s %10s %10s %2s %2s %4s\n",
+	printf("%16s %5s %3s %9s %10s "
+	    "%12s %3s %16s %10s %4s %2s %14s\n",
 	    "fid", "bno", "ref", "off", "len",
 	    "flags", "try", "sliod", "expire", "np", "nr", "addr");
-	return(PSC_CTL_DISPLAY_WIDTH);
+	return(PSC_CTL_DISPLAY_WIDTH+35);
 }
 
 void
@@ -828,9 +828,9 @@ ms_biorq_prdat(__unusedx const struct psc_ctlmsghdr *mh, const void *m)
 {
 	const struct msctlmsg_biorq *msr = m;
 
-	printf("%016"SLPRIxFID" %5d %3d %9d %6d "
-	    "%c%c%c%c%c%c%c%c%c%c%c "
-	    "%3d %10s %10"PRId64" %2d %2d %#lx\n",
+	printf("%016"SLPRIxFID" %5d %3d %9d %10d "
+	    "%c%c%c%c%c%c%c%c%c%c%c%c "
+	    "%3d %16s %10"PRId64" %4d %2d %#lx\n",
 	    msr->msr_fid, msr->msr_bno, msr->msr_ref, msr->msr_off,
 	    msr->msr_len,
 	    msr->msr_flags & BIORQ_READ			? 'r' : '-',
@@ -844,6 +844,7 @@ ms_biorq_prdat(__unusedx const struct psc_ctlmsghdr *mh, const void *m)
 	    msr->msr_flags & BIORQ_ONTREE		? 't' : '-',
 	    msr->msr_flags & BIORQ_READAHEAD		? 'a' : '-',
 	    msr->msr_flags & BIORQ_AIOWAKE		? 'k' : '-',
+	    msr->msr_flags & BIORQ_ABORT		? 'A' : '-',
 	    msr->msr_retries, msr->msr_last_sliod,
 	    msr->msr_expire.tv_sec, msr->msr_npages,
 	    msr->msr_nrq, msr->msr_addr);
