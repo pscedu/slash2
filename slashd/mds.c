f215f133ad820465d670c826f16aa140e0da1dc7
zhihui-slash2 <zhihui@psc.edu>
2016-06-03 15:00:34 -0400

    fix expiration check in mds_bia_odtable_startup_cb()

--- slashd/mds.c
+++ slashd/mds.c
@@ -1441,7 +1441,7 @@ mds_bia_odtable_startup_cb(void *data, struct pfl_odt_receipt *odtr,
 	 */
 	bml->bml_start = bia->bia_start;
 	bml->bml_expire = bml->bml_start + BMAP_TIMEO_MAX;
-	if (bml->bml_expire > time(NULL))
+	if (bml->bml_expire <= time(NULL))
 		OPSTAT_INCR("bmap-restart-expired");
 
 	if (bia->bia_flags & BIAF_DIO)
