7fe5a12e28afc962bec108654b84d77437c85ad9
zhihui-slash2 <zhihui@psc.edu>
2016-06-14 09:50:23 -0400

    tweak

--- slashd/inode.c
+++ slashd/inode.c
@@ -316,8 +316,7 @@ mds_inodes_odsync(int vfsid, struct fidc_membh *f,
 	if (rc == 0 && ih->inoh_ino.ino_nrepls > SL_DEF_REPLICAS)
 		rc = mds_inox_write(vfsid, ih, NULL, NULL);
 
-	DEBUG_FCMH(PLL_DEBUG, f, "updated inode logf=%p",
-	    logf);
+	DEBUG_FCMH(PLL_DEBUG, f, "updated inode logf=%p", logf);
 	INOH_URLOCK(ih, locked);
 	return (rc);
 }
