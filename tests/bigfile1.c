b151f1c123ea11d451bc2e43a8efa7a497a0d536
zhihui-slash2 <zhihui@psc.edu>
2016-06-23 20:45:27 -0400

    tweak bigfile1.c

--- tests/bigfile1.c
+++ tests/bigfile1.c
@@ -177,7 +177,7 @@ main(int argc, char *argv[])
 {
 	char *name;
 	size_t tmp;
-	int times = 1;
+	int times = 10;
 	unsigned int seed = 1234;
 	size_t i, j, c, fd, nfile;
 
@@ -213,7 +213,7 @@ main(int argc, char *argv[])
 	for (i = 0; i < nfile; i++) {
 
 		snprintf(files[i].name, 128, "%s.%02d", argv[optind], i);
-		printf("Try to allocate %12ld bytes of memory for file %s\n", 
+		printf("Try to allocate %12ld bytes of working memory for file %s\n", 
 			files[i].size, files[i].name); 
 
 		files[i].buf = malloc(files[i].size);
@@ -250,7 +250,7 @@ main(int argc, char *argv[])
 		for (i = 0; i < nfile; i++) {
 			write_file(i);
 		}
-		printf("Loop %d on %d file is done successfully.\n", j, nfile);
+		printf("Loop %d on %d files is done successfully.\n", j, nfile);
 	}
 
 	for (i = 0; i < nfile; i++) {
