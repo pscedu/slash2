668b87c842efd36293e8e9f98b98f5a691eb9832
zhihui-slash2 <zhihui@psc.edu>
2016-07-01 11:24:37 -0400

    caller already did busy/unbusy for this bmap.

--- slashd/up_sched_res.c
+++ slashd/up_sched_res.c
@@ -452,14 +452,10 @@ slm_upsch_tryptrunc(struct bmap *b, int off,
 	brepls_init(retifset, 0);
 	retifset[BREPLST_TRUNCPNDG_SCHED] = 1;
 
-	FCMH_WAIT_BUSY(f);
-	BMAP_WAIT_BUSY(b);
 	if (mds_repl_bmap_walk_all(b, NULL, retifset,
 	    REPL_WALKF_SCIRCUIT))
 		DEBUG_BMAPOD(PLL_FATAL, b,
 		    "truncate already scheduled");
-	BMAP_UNBUSY(b);
-	FCMH_UNBUSY(f);
 
 	csvc = slm_geticsvc(dst_resm, NULL, CSVCF_NONBLOCK |
 	    CSVCF_NORECON, &slm_upsch_mw);
