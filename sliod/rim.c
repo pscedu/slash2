1f9265cf06ffbe1fd04f0f54f4454f2c237db802
zhihui-slash2 <zhihui@psc.edu>
2016-07-08 09:44:33 -0400

    minor

--- sliod/rim.c
+++ sliod/rim.c
@@ -57,8 +57,7 @@
 uint64_t	sli_current_reclaim_xid;
 uint64_t	sli_current_reclaim_batchno;
 
-struct slrpc_batch_req_handler
-		sli_rim_batch_req_handlers[NSRMT];
+struct slrpc_batch_req_handler sli_rim_batch_req_handlers[NSRMT];
 
 /*
  * Handle a piece of PRECLAIM (partial reclaim) work.  If our backend
