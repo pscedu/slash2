9c9f6b59fa177fe0591021bf9a70b34c24cbfe0b
zhihui-slash2 <zhihui@psc.edu>
2016-07-21 15:50:38 -0400

    document a bug

--- slashd/rmi.c
+++ slashd/rmi.c
@@ -81,6 +81,7 @@ slm_rmi_handle_bmap_getcrcs(struct pscrpc_request *rq)
 		return (mp->rc);
 #endif
 
+	/* XXX should be look or load */
 	mp->rc = slm_fcmh_peek(&mq->fg, &f);
 	if (mp->rc) {
 		psclog_warnx("here, rc = %d", mp->rc);
