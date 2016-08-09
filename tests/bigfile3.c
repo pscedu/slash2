42d0a1c569995381f54eec95aa91e6aca1bce07a
zhihui-slash2 <zhihui@psc.edu>
2016-08-09 15:37:40 -0400

    Copyright:
    (1) /*  %GPL_LICENSE% */
    (2) make copyright

--- tests/bigfile3.c
+++ tests/bigfile3.c
@@ -1,4 +1,23 @@
 /*
+ * %GPL_START_LICENSE%
+ * ---------------------------------------------------------------------
+ * Copyright 2016, Pittsburgh Supercomputing Center
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License contained in the file
+ * `COPYING-GPL' at the top of this distribution or at
+ * https://www.gnu.org/licenses/gpl-2.0.html for more details.
+ * ---------------------------------------------------------------------
+ * %END_LICENSE%
+ */
+/*
  * 06/24/2016: bigfile3.c, write a file concurrently by multiple threads.
  */
 #include <stdio.h>
