84bc83492bc6bd1260b8f2f934cdd7fac14e632e
zhihui-slash2 <zhihui@psc.edu>
2016-09-23 11:03:22 -0400

    fix recursive busy business

--- slashd/jreplay.c
+++ slashd/jreplay.c
@@ -147,8 +147,6 @@ mds_replay_bmap(void *jent, int op)
 		 * So we have some changes in the journal, but not
 		 * in the sql table.
 		 */
-		FCMH_LOCK(b->bcm_fcmh);
-		FCMH_WAIT_BUSY(b->bcm_fcmh, 1);
 		BMAP_LOCK(b);
 		bmap_wait_locked(b, b->bcm_flags & BMAPF_REPLMODWR);
 
@@ -199,20 +197,15 @@ mds_replay_bmap(void *jent, int op)
 				break;
 			}
 
-		FCMH_UNBUSY(b->bcm_fcmh, 1);
 		break;
 	}
 
 	DEBUG_BMAPOD(PLL_DIAG, b, "replayed bmap op=%d", op);
 
-	FCMH_LOCK(b->bcm_fcmh);
-	FCMH_WAIT_BUSY(b->bcm_fcmh, 1);
-
 	BMAP_LOCK(b);
 	bmap_wait_locked(b, b->bcm_flags & BMAPF_REPLMODWR);
 	rc = mds_bmap_write(b, NULL, NULL);
 	BMAP_ULOCK(b);
-	FCMH_UNBUSY(b->bcm_fcmh, 1);
 
  out:
 	if (b)
