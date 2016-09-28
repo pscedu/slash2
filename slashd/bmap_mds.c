1dfe54e651f0eb9eede9317ef62b8401ae43464e
zhihui-slash2 <zhihui@psc.edu>
2016-09-28 15:15:49 -0400

    Initialize the GENERATION Number of a bmap properly.  Otherwise, I suspect it will push a bmap in GARBAGE state forever.

--- slashd/bmap_mds.c
+++ slashd/bmap_mds.c
@@ -397,6 +397,7 @@ mds_bmap_init(struct bmap *b)
 	pll_init(&bmi->bmi_leases, struct bmap_mds_lease,
 	    bml_bmi_lentry, &b->bcm_lock);
 
+	BHGEN_SET(b, &sl_sys_upnonce);
 	bmi->bmi_sys_prio = -1;
 	bmi->bmi_usr_prio = -1;
 }
