42b1aa9c39f4698d4af3bcc7ae83dbf3e5faa0f2
zhihui-slash2 <zhihui@psc.edu>
2016-10-07 17:09:28 -0400

    yield

--- slashd/repl_mds.c
+++ slashd/repl_mds.c
@@ -600,6 +600,9 @@ slm_repl_upd_write(struct bmap *b, int rel)
 	sprio = bmi->bmi_sys_prio;
 	uprio = bmi->bmi_usr_prio;
 
+	while (lc_nitems(&slm_db_lopri_workq))
+		usleep(1000000/2);
+
 	memset(&chg, 0, sizeof(chg));
 
 	add.nios = 0;
