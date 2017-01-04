fbbe3410ed32a93376aaff78db9b93c67cec652a
zhihui-slash2 <zhihui@psc.edu>
2017-01-04 12:02:03 -0500

    bigfile3.sh tweak

--- tests/bigfile3.sh
+++ tests/bigfile3.sh
@@ -4,22 +4,28 @@
 #
 #
 START=`date +%s%N`
+myhost=$(hostname -s)
 
-cc bigfile3.c -lpthread
+cc -o bigfile1 bigfile1.c
+cc -o bigfile2 bigfile2.c
+cc -o bigfile3 bigfile3.c -lpthread
 
-./a.out /zzh-slash2/zhihui/bigfile1.dat | tee bigfile1.log
+./bigfile1 /zzh-slash2/zhihui/$myhost.bigfile1.dat | tee $myhost.bigfile1.log
+./bigfile2 /zzh-slash2/zhihui/$myhost.bigfile2.dat | tee $myhost.bigfile1.log
 
-./a.out -s 7738 -t 6 -b 71785   -n 243656 /zzh-slash2/zhihui/bigfile2.dat | tee bigfile2.log
+./bigfile3                                   /zzh-slash2/zhihui/$myhost.bigfile3-1.dat | tee $myhost.bigfile3-1.log
 
-./a.out -s 3873 -t 7 -b 91785   -n 143659 /zzh-slash2/zhihui/bigfile3.dat | tee bigfile3.log
+./bigfile3 -s 7738 -t 6 -b 71785   -n 243656 /zzh-slash2/zhihui/$myhost.bigfile3-2.dat | tee $myhost.bigfile3-2.log
 
-./a.out -s 3805 -t 9 -b 1785    -n 443957 /zzh-slash2/zhihui/bigfile4.dat | tee bigfile4.log
+./bigfile3 -s 3873 -t 7 -b 91785   -n 143659 /zzh-slash2/zhihui/$myhost.bigfile3-3.dat | tee $myhost.bigfile3-3.log
+
+./bigfile3 -s 3805 -t 9 -b 1785    -n 443957 /zzh-slash2/zhihui/$myhost.bigfile3-4.dat | tee $myhost.bigfile3-4.log
  
-./a.out -s 9805 -t 3 -b 111785  -n 143206 /zzh-slash2/zhihui/bigfile5.dat | tee bigfile5.log
+./bigfile3 -s 9805 -t 3 -b 111785  -n 143206 /zzh-slash2/zhihui/$myhost.bigfile3-5.dat | tee $myhost.bigfile3-5.log
 
-./a.out -s 1805 -t 5 -b 81025   -n 213258 /zzh-slash2/zhihui/bigfile6.dat | tee bigfile6.log
+./bigfile3 -s 1805 -t 5 -b 81025   -n 213258 /zzh-slash2/zhihui/$myhost.bigfile3-6.dat | tee $myhost.bigfile3-6.log
 
-./a.out /zzh-slash2/zhihui/bigfile7.dat | tee bigfile7.log
+./bigfile3 -s 1234 -t 11 -b 21029  -n 110052 /zzh-slash2/zhihui/$myhost.bigfile3-7.dat | tee $myhost-bigfile3-7.log
 
 END=`date +%s%N`
 ELAPSED=`echo "scale=8; ($END - $START) / 1000000000" | bc`
