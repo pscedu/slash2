8549937313207680549e82ce1b46bfe269ebe06f
zhihui-slash2 <zhihui@psc.edu>
2017-01-04 17:29:53 -0500

    alternative path

--- tests/bigfile.sh
+++ tests/bigfile.sh
@@ -4,6 +4,14 @@
 #
 # Big file tests written for slash2 over the years.
 #
+
+if [ $# -eq 1 ]
+then
+    mypath=$1
+else
+    mypath=/zzh-slash2/zhihui
+fi
+
 START=`date +%s%N`
 myhost=$(hostname -s)
 
@@ -11,24 +19,24 @@ cc -o bigfile1 bigfile1.c
 cc -o bigfile2 bigfile2.c
 cc -o bigfile3 bigfile3.c -lpthread
 
-./bigfile1    /zzh-slash2/zhihui/$myhost.bigfile1.dat | tee $myhost.bigfile1.log
+./bigfile1    $mypath/$myhost.bigfile1.dat | tee $myhost.bigfile1.log
 
-./bigfile2    /zzh-slash2/zhihui/$myhost.bigfile2.dat | tee $myhost.bigfile2.log
-./bigfile2 -r /zzh-slash2/zhihui/$myhost.bigfile2.dat | tee $myhost.bigfile2.log
+./bigfile2    $mypath/$myhost.bigfile2.dat | tee $myhost.bigfile2.log
+./bigfile2 -r $mypath/$myhost.bigfile2.dat | tee $myhost.bigfile2.log
 
-./bigfile3                                   /zzh-slash2/zhihui/$myhost.bigfile3-1.dat | tee $myhost.bigfile3-1.log
+./bigfile3                                   $mypath/$myhost.bigfile3-1.dat | tee $myhost.bigfile3-1.log
 
-./bigfile3 -s 7738 -t 6 -b 71785   -n 243656 /zzh-slash2/zhihui/$myhost.bigfile3-2.dat | tee $myhost.bigfile3-2.log
+./bigfile3 -s 7738 -t 6 -b 71785   -n 243656 $mypath/$myhost.bigfile3-2.dat | tee $myhost.bigfile3-2.log
 
-./bigfile3 -s 3873 -t 7 -b 91785   -n 143659 /zzh-slash2/zhihui/$myhost.bigfile3-3.dat | tee $myhost.bigfile3-3.log
+./bigfile3 -s 3873 -t 7 -b 91785   -n 143659 $mypath/$myhost.bigfile3-3.dat | tee $myhost.bigfile3-3.log
 
-./bigfile3 -s 3805 -t 9 -b 1785    -n 443957 /zzh-slash2/zhihui/$myhost.bigfile3-4.dat | tee $myhost.bigfile3-4.log
+./bigfile3 -s 3805 -t 9 -b 1785    -n 443957 $mypath/$myhost.bigfile3-4.dat | tee $myhost.bigfile3-4.log
  
-./bigfile3 -s 9805 -t 3 -b 111785  -n 143206 /zzh-slash2/zhihui/$myhost.bigfile3-5.dat | tee $myhost.bigfile3-5.log
+./bigfile3 -s 9805 -t 3 -b 111785  -n 143206 $mypath/$myhost.bigfile3-5.dat | tee $myhost.bigfile3-5.log
 
-./bigfile3 -s 1805 -t 5 -b 81025   -n 213258 /zzh-slash2/zhihui/$myhost.bigfile3-6.dat | tee $myhost.bigfile3-6.log
+./bigfile3 -s 1805 -t 5 -b 81025   -n 213258 $mypath/$myhost.bigfile3-6.dat | tee $myhost.bigfile3-6.log
 
-./bigfile3 -s 1234 -t 11 -b 21029  -n 110052 /zzh-slash2/zhihui/$myhost.bigfile3-7.dat | tee $myhost-bigfile3-7.log
+./bigfile3 -s 1234 -t 11 -b 21029  -n 110052 $mypath/$myhost.bigfile3-7.dat | tee $myhost-bigfile3-7.log
 
 END=`date +%s%N`
 ELAPSED=`echo "scale=8; ($END - $START) / 1000000000" | bc`
