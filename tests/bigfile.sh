62ddf703097775f47d6e564916ad82ec8cc84f60
zhihui-slash2 <zhihui@psc.edu>
2017-01-04 19:44:31 -0500

    use even bigger file for testwq

--- tests/bigfile.sh
+++ tests/bigfile.sh
@@ -26,17 +26,19 @@ cc -o bigfile3 bigfile3.c -lpthread
 
 ./bigfile3                                   $mypath/$myhost.bigfile3-1.dat | tee $myhost.bigfile3-1.log
 
-./bigfile3 -s 7738 -t 6 -b 71785   -n 243656 $mypath/$myhost.bigfile3-2.dat | tee $myhost.bigfile3-2.log
+./bigfile3 -s  7738 -t  6 -b  71785  -n 243656 $mypath/$myhost.bigfile3-2.dat | tee $myhost.bigfile3-2.log
 
-./bigfile3 -s 3873 -t 7 -b 91785   -n 143659 $mypath/$myhost.bigfile3-3.dat | tee $myhost.bigfile3-3.log
+./bigfile3 -s  3873 -t  7 -b  91785  -n 143659 $mypath/$myhost.bigfile3-3.dat | tee $myhost.bigfile3-3.log
 
-./bigfile3 -s 3805 -t 9 -b 1785    -n 443957 $mypath/$myhost.bigfile3-4.dat | tee $myhost.bigfile3-4.log
- 
-./bigfile3 -s 9805 -t 3 -b 111785  -n 143206 $mypath/$myhost.bigfile3-5.dat | tee $myhost.bigfile3-5.log
+./bigfile3 -s  3805 -t  9 -b   1785  -n 443957 $mypath/$myhost.bigfile3-4.dat | tee $myhost.bigfile3-4.log
+  
+./bigfile3 -s  9805 -t  3 -b 111785  -n 143206 $mypath/$myhost.bigfile3-5.dat | tee $myhost.bigfile3-5.log
 
-./bigfile3 -s 1805 -t 5 -b 81025   -n 213258 $mypath/$myhost.bigfile3-6.dat | tee $myhost.bigfile3-6.log
+./bigfile3 -s  1805 -t  5 -b  81025  -n 213258 $mypath/$myhost.bigfile3-6.dat | tee $myhost.bigfile3-6.log
 
-./bigfile3 -s 1234 -t 11 -b 21029  -n 110052 $mypath/$myhost.bigfile3-7.dat | tee $myhost-bigfile3-7.log
+./bigfile3 -s  1234 -t 11 -b  21029  -n 110052 $mypath/$myhost.bigfile3-7.dat | tee $myhost-bigfile3-7.log
+
+./bigfile3 -s 91234 -t 13 -b  51029  -n 210052 $mypath/$myhost.bigfile3-7.dat | tee $myhost-bigfile3-8.log
 
 END=`date +%s%N`
 ELAPSED=`echo "scale=8; ($END - $START) / 1000000000" | bc`
