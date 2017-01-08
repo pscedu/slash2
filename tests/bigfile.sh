65d8a07ae985f95422be0a48b1824c7e65e2ee38
zhihui-slash2 <zhihui@psc.edu>
2017-01-08 10:42:46 -0500

    more case

--- tests/bigfile.sh
+++ tests/bigfile.sh
@@ -44,6 +44,8 @@ cc -o bigfile3 bigfile3.c -lpthread
 
 ./bigfile3 -s 91234 -t 13 -b  51029  -n 210052 $mypath/$myhost.bigfile3-8.dat | tee $myhost-bigfile3-8.log
 
+./bigfile3 -s 55578 -t 23 -b  31029  -n 187750 $mypath/$myhost.bigfile3-9.dat | tee $myhost-bigfile3-9.log
+
 END=`date +%s%N`
 ELAPSED=`echo "scale=8; ($END - $START) / 1000000000" | bc`
 echo "Total time $ELAPSED seconds"
