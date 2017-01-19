27ef264c2eb6bcab24f0d4e490df3cc8f3b60b08
zhihui-slash2 <zhihui@psc.edu>
2017-01-19 11:27:46 -0500

    tweak

--- tests/bigfile.sh
+++ tests/bigfile.sh
@@ -44,7 +44,7 @@ cc -o bigfile3 bigfile3.c -lpthread
 
 ./bigfile3 -s 91234 -t 13 -b  51029  -n 210052 $mypath/$myhost.bigfile3-8.dat | tee $myhost-bigfile3-8.log
 
-./bigfile3 -s 5555  -t 17 -b 114129  -n 280112 $mypath/$myhost.bigfile3-9.dat | tee $myhost-bigfile3-9.log
+./bigfile3 -s  5555 -t 17 -b 114129  -n 280112 $mypath/$myhost.bigfile3-9.dat | tee $myhost-bigfile3-9.log
 
 
 END=`date +%s%N`
