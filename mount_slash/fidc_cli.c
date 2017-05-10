371bee9b8fe03c7c7fd67a6dedecfcb44c94cc68
zhihui-slash2 <zhihui@psc.edu>
2017-05-10 15:48:13 -0400

    fix generation of a directory

--- mount_slash/fidc_cli.c
+++ mount_slash/fidc_cli.c
@@ -74,14 +74,20 @@ slc_fcmh_setattrf(struct fidc_membh *f, struct srt_stat *sstb,
 	if (fcmh_2_gen(f) == FGEN_ANY)
 		fcmh_2_gen(f) = sstb->sst_gen;
 
-	if ((FID_GET_INUM(fcmh_2_fid(f))) != SLFID_ROOT &&
+	if ((FID_GET_INUM(fcmh_2_fid(f))) != SLFID_ROOT && fcmh_isreg(f) &&
 	    fcmh_2_gen(f) > sstb->sst_gen) {
+		/*
+ 		 * We bump it locally for a directory to avoid
+ 		 * race with readdir operations.
+ 		 */
 		OPSTAT_INCR("msl.generation-backwards");
 		DEBUG_FCMH(PLL_DIAG, f, "attempt to set attr with "
 		    "gen %"PRIu64" from old gen %"PRIu64,
 		    fcmh_2_gen(f), sstb->sst_gen);
 		goto out;
 	}
+	if (fcmh_isdir(f))
+	    sstb->sst_gen = fcmh_2_gen(f);
 
 	/*
 	 * If we don't have stat attributes, how can we save our local
