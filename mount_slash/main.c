0294121e8bbcd9e2d352453b77b39f89348ed166
zhihui-slash2 <zhihui@psc.edu>
2017-05-24 16:34:44 -0400

    lower log

--- mount_slash/main.c
+++ mount_slash/main.c
@@ -1623,8 +1623,8 @@ msl_readdir_finish(struct fidc_membh *d, struct dircache_page *p,
 	if (p->dcp_dirgen != fcmh_2_gen(d)) {
 		if (!(p->dcp_flags & DIRCACHEPGF_ASYNC)) {
 			OPSTAT_INCR("msl.readdir-all-stale");
-			psclogs_warnx(SLCSS_FSOP, "Unexpected stale readdir "
-			"fid="SLPRI_FID, fcmh_2_fid(d));
+			psclogs_diag(SLCSS_FSOP, "Unexpected stale readdir "
+			    "fid="SLPRI_FID, fcmh_2_fid(d));
 		}
 		FCMH_ULOCK(d);
 		return (-EAGAIN);
