3cc33cd5de46040272173fa0fe08b0dc433238e9
zhihui-slash2 <zhihui@psc.edu>
2017-07-07 08:40:05 -0400

    fix duplication lock/unlock

--- mount_slash/main.c
+++ mount_slash/main.c
@@ -3947,9 +3947,6 @@ msreapthr_main(struct psc_thread *thr)
 
 		POOL_LOCK(bmpce_pool);
 		curr = bmpce_pool->ppm_nfree;
-		POOL_ULOCK(bmpce_pool);
-
-		POOL_LOCK(bmpce_pool);
 		if (last && curr >= last)
 			psc_pool_try_shrink(bmpce_pool, curr);
 		last = bmpce_pool->ppm_nfree;
