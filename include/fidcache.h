179baa2dc763e9a99009cdeed832f29b0714a965
zhihui-slash2 <zhihui@psc.edu>
2017-07-20 12:18:36 -0400

    Dont use FIDC_LOOKUP_LOCK in dircache

--- include/fidcache.h
+++ include/fidcache.h
@@ -218,6 +218,7 @@ struct fidc_membh {
 #define FCMH_OPCNT_READAHEAD		 9	/* IOD/CLI: readahead */
 #define FCMH_OPCNT_DIRCACHE		10	/* CLI: async dircache */
 #define FCMH_OPCNT_SYNC_AHEAD		11	/* IOD: sync ahead */
+#define FCMH_OPCNT_KEEP_LOCK		11	/* IOD: sync ahead */
 #define FCMH_OPCNT_MAXTYPE		12
 
 void	fidc_init(int);
