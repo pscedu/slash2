40ab2fe3b49966ebe2d1f19e0ff54bd025524522
zhihui-slash2 <zhihui@psc.edu>
2017-08-14 13:50:48 -0400

    throw away cachesif we don't have bmap

--- mount_slash/rcm.c
+++ mount_slash/rcm.c
@@ -226,6 +226,7 @@ msrcm_handle_releasebmap(struct pscrpc_request *rq)
  		 * long the flush will take.
  		 */
 		b->bcm_flags |= BMAPF_TOFREE; 
+		msl_bmap_cache_rls(b);
 		bmap_op_done(b);
 		fcmh_op_done(f);
 		OPSTAT_INCR("msl.bmap_reclaim");
