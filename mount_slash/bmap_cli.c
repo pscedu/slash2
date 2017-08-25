2a1638d349150284a3b69eaa9ef99bb7457e96ab
zhihui-slash2 <zhihui@psc.edu>
2017-08-25 11:20:25 -0400

    fix another place of flag handling

--- mount_slash/bmap_cli.c
+++ mount_slash/bmap_cli.c
@@ -581,7 +581,8 @@ msl_bmap_modeset_cb(struct pscrpc_request *rq,
 	if (!rc) {
 		msl_bmap_stash_lease(b, &mp->sbd, "modechange");
 		psc_assert((b->bcm_flags & BMAP_RW_MASK) == BMAPF_RD);
-		b->bcm_flags = (b->bcm_flags & ~BMAPF_RD) | BMAPF_WR;
+		b->bcm_flags &= ~BMAPF_RD;
+		b->bcm_flags |=  BMAPF_WR;
 		r = libsl_id2res(bmap_2_sbd(b)->sbd_ios);
 		if (r->res_type == SLREST_ARCHIVAL_FS) {
 			/*
