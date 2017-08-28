7b4a6db5d49aa3e7dab7f0055412b569df9aa59a
zhihui-slash2 <zhihui@psc.edu>
2017-08-28 14:21:37 -0400

    add pid in test file

--- tests/bigfile.sh
+++ tests/bigfile.sh
@@ -30,6 +30,7 @@ if [ ! -d "$mypath" ]; then
     exit 0
 fi
 
+pid=$$
 START=`date +%s%N`
 myhost=$(hostname -s)
 
@@ -39,85 +40,87 @@ cc -o bigfile3 bigfile3.c -lpthread
 
 set -o pipefail
 
-./bigfile1                                     $mypath/$myhost.bigfile1-1.dat | tee $myhost.bigfile1-1.log
+./bigfile1                                     $mypath/$myhost.bigfile1-1.$pid.dat | tee $myhost.bigfile1-1.$pid.log
 if [ $? -eq 1 ]
 then
     bail
 fi
 
-./bigfile2                                     $mypath/$myhost.bigfile2-1.dat | tee $myhost.bigfile2-1.log
+bail
+
+./bigfile2                                     $mypath/$myhost.bigfile2-1.$pid.dat | tee $myhost.bigfile2-1.$pid.log
 if [ $? -eq 1 ]
 then
     bail
 fi
 
-./bigfile2 -r                                  $mypath/$myhost.bigfile2-1.dat | tee $myhost.bigfile2-1.log
+./bigfile2 -r                                  $mypath/$myhost.bigfile2-1.$pid.dat | tee $myhost.bigfile2-1.$pid.log
 if [ $? -eq 1 ]
 then
     bail
 fi
 
-./bigfile2    -s 4499 -b 12348 -n 3993777      $mypath/$myhost.bigfile2-2.dat | tee $myhost.bigfile2-2.log
+./bigfile2    -s 4499 -b 12348 -n 3993777      $mypath/$myhost.bigfile2-2.$pid.dat | tee $myhost.bigfile2-2.$pid.log
 if [ $? -eq 1 ]
 then
     bail
 fi
 
-./bigfile2 -r -s 4499 -b 7790                  $mypath/$myhost.bigfile2-2.dat | tee $myhost.bigfile2-2.log
+./bigfile2 -r -s 4499 -b 7790                  $mypath/$myhost.bigfile2-2.$pid.dat | tee $myhost.bigfile2-2.$pid.log
 if [ $? -eq 1 ]
 then
     bail
 fi
 
-./bigfile3                                     $mypath/$myhost.bigfile3-1.dat | tee $myhost.bigfile3-1.log
+./bigfile3                                     $mypath/$myhost.bigfile3-1.$pid.dat | tee $myhost.bigfile3-1.$pid.log
 if [ $? -eq 1 ]
 then
     bail
 fi
 
-./bigfile3 -s  7738 -t  6 -b  71785  -n 243656 $mypath/$myhost.bigfile3-2.dat | tee $myhost.bigfile3-2.log
+./bigfile3 -s  7738 -t  6 -b  71785  -n 243656 $mypath/$myhost.bigfile3-2.$pid.dat | tee $myhost.bigfile3-2.$pid.log
 if [ $? -eq 1 ]
 then
     bail
 fi
 
-./bigfile3 -s  3873 -t  7 -b  91785  -n 143659 $mypath/$myhost.bigfile3-3.dat | tee $myhost.bigfile3-3.log
+./bigfile3 -s  3873 -t  7 -b  91785  -n 143659 $mypath/$myhost.bigfile3-3.$pid.dat | tee $myhost.bigfile3-3.$pid.log
 if [ $? -eq 1 ]
 then
     bail
 fi
 
-./bigfile3 -s  3805 -t  9 -b   1785  -n 443957 $mypath/$myhost.bigfile3-4.dat | tee $myhost.bigfile3-4.log
+./bigfile3 -s  3805 -t  9 -b   1785  -n 443957 $mypath/$myhost.bigfile3-4.$pid.dat | tee $myhost.bigfile3-4.$pid.log
 if [ $? -eq 1 ]
 then
     bail
 fi
   
-./bigfile3 -s  9805 -t  3 -b 111785  -n 143206 $mypath/$myhost.bigfile3-5.dat | tee $myhost.bigfile3-5.log
+./bigfile3 -s  9805 -t  3 -b 111785  -n 143206 $mypath/$myhost.bigfile3-5.$pid.dat | tee $myhost.bigfile3-5.$pid.log
 if [ $? -eq 1 ]
 then
     bail
 fi
 
-./bigfile3 -s  1805 -t  5 -b  81025  -n 213258 $mypath/$myhost.bigfile3-6.dat | tee $myhost.bigfile3-6.log
+./bigfile3 -s  1805 -t  5 -b  81025  -n 213258 $mypath/$myhost.bigfile3-6.$pid.dat | tee $myhost.bigfile3-6.$pid.log
 if [ $? -eq 1 ]
 then
     bail
 fi
 
-./bigfile3 -s  1234 -t 25 -b  21029  -n  10052 $mypath/$myhost.bigfile3-7.dat | tee $myhost.bigfile3-7.log
+./bigfile3 -s  1234 -t 25 -b  21029  -n  10052 $mypath/$myhost.bigfile3-7.$pid.dat | tee $myhost.bigfile3-7.$pid.log
 if [ $? -eq 1 ]
 then
     bail
 fi
 
-./bigfile3 -s 91234 -t 13 -b  51029  -n 210052 $mypath/$myhost.bigfile3-8.dat | tee $myhost.bigfile3-8.log
+./bigfile3 -s 91234 -t 13 -b  51029  -n 210052 $mypath/$myhost.bigfile3-8.$pid.dat | tee $myhost.bigfile3-8.$pid.log
 if [ $? -eq 1 ]
 then
     bail
 fi
 
-./bigfile3 -s  5555 -t 17 -b 114129  -n  80112 $mypath/$myhost.bigfile3-9.dat | tee $myhost.bigfile3-9.log
+./bigfile3 -s  5555 -t 17 -b 114129  -n  80112 $mypath/$myhost.bigfile3-9.$pid.dat | tee $myhost.bigfile3-9.$pid.log
 if [ $? -eq 1 ]
 then
     bail
