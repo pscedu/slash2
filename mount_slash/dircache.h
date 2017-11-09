9fd3a9f59ebff4a26a0bd8787d753f21e0e4c0dd
zhihui-slash2 <zhihui@psc.edu>
2017-11-09 09:20:34 -0500

    comments

--- mount_slash/dircache.h
+++ mount_slash/dircache.h
@@ -67,7 +67,7 @@ struct fidc_membh;
 #define DIRCACHE_NPAGES		64		/* initial number of pages in pool */
 #define DIRCACHE_NAMECACHE	2048		/* initial number of name cache enties in pool */
 
-#define DIRCACHEPG_SOFT_TIMEO	30		/* expiration regardless if read */
+#define DIRCACHEPG_SOFT_TIMEO	30		/* expiration regardless if read or not */
 
 /*
  * This consitutes a block of 'struct dirent' members (dircache_ent)
--- mount_slash/main.c
+++ mount_slash/main.c
@@ -1759,6 +1759,10 @@ msl_readdir_cb(struct pscrpc_request *rq, struct pscrpc_async_args *av)
  out:
 	DIRCACHE_WRLOCK(d);
 
+	/*
+ 	 * XXX Looks like we never free a page inside a callback. If we
+ 	 * use reference count properly, we should be able to do so.
+ 	 */
 	p->dcp_rc = rc;
 	p->dcp_refcnt--;
 	async = p->dcp_flags & DIRCACHEPGF_ASYNC;
