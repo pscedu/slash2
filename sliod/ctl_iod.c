f91e03d776922d6d51243734f57db3ed7d7abea6
zhihui-slash2 <zhihui@psc.edu>
2018-01-23 14:54:57 -0500

    iod: add rpc_version

--- sliod/ctl_iod.c
+++ sliod/ctl_iod.c
@@ -631,8 +631,12 @@ slictlthr_spawn(const char *fn)
 
 	psc_ctlparam_register_simple("sys.uptime",
 	    slctlparam_uptime_get, NULL);
+
 	psc_ctlparam_register_simple("sys.version",
 	    slctlparam_version_get, NULL);
+	psc_ctlparam_register_simple("sys.rpc_version",
+	    slctlparam_rpc_version_get, NULL);
+
 	psc_ctlparam_register_var("sys.datadir", PFLCTL_PARAMT_STR, 0,
 	    (char *)sl_datadir);
 
