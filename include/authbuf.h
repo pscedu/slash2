17ac2474fe9271be8bafbed881b05e55a7506424
zhihui-slash2 <zhihui@psc.edu>
2018-03-16 13:33:47 -0400

    copyright

--- include/authbuf.h
+++ include/authbuf.h
@@ -2,8 +2,7 @@
 /*
  * %GPL_START_LICENSE%
  * ---------------------------------------------------------------------
- * Copyright 2015, Google, Inc.
- * Copyright (c) 2008-2015, Pittsburgh Supercomputing Center (PSC).
+ * Copyright 2008-2018, Pittsburgh Supercomputing Center
  * All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
