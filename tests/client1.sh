923123d5b81c702aa1697e9ee37f5c94a5c2a159
zhihui-slash2 <zhihui@psc.edu>
2018-06-19 11:07:45 -0400

    Add client1.sh and client2.sh for test

---
+++ tests/client1.sh
@@ -0,0 +1,9 @@
+#
+# 06/19/2018
+#
+# client1.sh: to be used with client2.sh
+#
+while true
+do
+  pwd; hostname -f ; date; date > test
+done
---
+++ tests/client2.sh
@@ -0,0 +1,4 @@
+#
+# 06/19/2018: client2.sh, used with client1.sh
+#
+for i in `seq 1 1200000`; do echo "`date` : `cat test`"; sleep 1; done
