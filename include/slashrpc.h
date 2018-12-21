cff6ddbc9d39dff101cd6a3fd003880167d54e80
Jared Yanovich <jaredy@google.com>
2018-12-21 15:22:31 -0500

    precedence

--- include/slashrpc.h
+++ include/slashrpc.h
@@ -348,7 +348,7 @@ struct srt_stat {
 		    sizeof(*(dst)) - _n);				\
 	} while (0)
 
-#define BW_UNITSZ		1024*1024		/* denomination of bw */
+#define BW_UNITSZ		(1024 * 1024)	/* denomination of bw */
 
 struct srt_bwqueued {
 	 int32_t		 sbq_ingress;
